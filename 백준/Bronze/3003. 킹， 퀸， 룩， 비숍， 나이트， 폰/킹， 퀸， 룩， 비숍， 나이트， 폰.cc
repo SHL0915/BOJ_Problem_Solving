2bb54043cce7633e0def08d8c97b0c504114f9ea
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-09-14 08:33:57 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int N;
+	cin >> N;
+	cout << 1 - N << " ";
+	cin >> N;
+	cout << 1 - N << " ";
+	cin >> N;
+	cout << 2 - N << " ";
+	cin >> N;
+	cout << 2 - N << " ";
+	cin >> N;
+	cout << 2 - N << " ";
+	cin >> N;
+	cout << 8 - N << endl;
+	return 0;
+}
