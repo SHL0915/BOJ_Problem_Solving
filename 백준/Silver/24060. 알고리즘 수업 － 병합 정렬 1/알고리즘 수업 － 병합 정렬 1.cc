217bdcfa0e7d1d849170b96a5a314ed111822b82
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-09-14 09:35:02 +0900

    [Silver IV] Title: 알고리즘 수업 - 병합 정렬 1, Time: 288 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.cc
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.cc
@@ -1,72 +1,69 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int cnt = 0;
-int num = -1;
+const int MAX_N = 500005;
 
-void MergeSort(int* Arr, int* temp, int left, int right_end, int K);
-void Merge(int* Arr, int* temp, int left, int right, int right_end, int K);
+int N, K, cnt = 0;
+int Arr[MAX_N];
+int temp[MAX_N];
 
-int main(void) {
-	int N, K;
-	cin >> N >> K;
-	int* Arr = (int*)malloc(sizeof(int) * N);
-	int* temp = (int*)malloc(sizeof(int) * N);
+void MergeSort(int left, int right_end);
+void Merge(int left, int right, int right_end);
+
+int main(void) {	
+	cin >> N >> K;	
 	for (int i = 0; i < N; i++)
 		cin >> Arr[i];
-	MergeSort(Arr, temp, 0, N - 1, K);
-	cout << num;
+	MergeSort(0, N - 1);
+	cout << "-1";
 	return 0;
 }
 
-void MergeSort(int* Arr, int* temp, int left, int right_end, int K) {
-	int mid = (left + right_end) / 2;
+void MergeSort(int left, int right_end) {	
 	if (left < right_end) {
-		MergeSort(Arr, temp, left, mid, K);
-		MergeSort(Arr, temp, mid + 1, right_end, K);
-		Merge(Arr, temp, left, mid + 1, right_end, K);
+		int mid = (left + right_end) / 2;
+		MergeSort(left, mid);
+		MergeSort(mid + 1, right_end);
+		Merge(left, mid + 1, right_end);
 	}
 	return;
 }
 
-void Merge(int* Arr, int* temp, int left, int right, int right_end, int K) {
+void Merge(int left, int right, int right_end) {
 	int i, j, pos;
 	i = left;
 	j = right;
 	pos = left;
 	while (i < right && j <= right_end) {
 		if (Arr[i] <= Arr[j]) {
-			temp[pos] = Arr[i];
-			i++;
-			pos++;
-			cnt++;			
-			if (cnt == K)
-				num = Arr[i - 1];
+			temp[pos++] = Arr[i++];						
+			if (++cnt == K) {
+				cout << Arr[i - 1];
+				exit(0);
+			}
 		}
 		else {
-			temp[pos] = Arr[j];
-			j++;
-			pos++;
-			cnt++;			
-			if (cnt == K)
-				num = Arr[j - 1];
+			temp[pos++] = Arr[j++];
+			if (++cnt == K) {
+				cout << Arr[j - 1];
+				exit(0);
+			}
 		}
 	}
 	while (i < right) {
-		temp[pos] = Arr[i];
-		i++;
-		pos++;
-		cnt++;		
-		if (cnt == K)
-			num = Arr[i - 1];
+		temp[pos++] = Arr[i++];
+		if (++cnt == K) {
+			cout << Arr[i - 1];
+			exit(0);
+		}
 	}
 	while (j <= right_end) {
-		temp[pos] = Arr[j];
-		j++;
-		pos++;
-		cnt++;		
-		if (cnt == K)
-			num = Arr[j - 1];
+		temp[pos++] = Arr[j++];
+		if (++cnt == K) {
+			cout << Arr[j - 1];
+			exit(0);
+		}
 	}
 	for (i = left; i <= right_end; i++)
 		Arr[i] = temp[i];
