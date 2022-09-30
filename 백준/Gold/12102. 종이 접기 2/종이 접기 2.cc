7da87884008cf542b51080e6bc1075baf39d0a17
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-09-30 17:40:33 +0900

    [Gold III] Title: 종이 접기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/12102. 종이 접기 2/종이 접기 2.cc
+++ 백준/Gold/12102. 종이 접기 2/종이 접기 2.cc
@@ -3,80 +3,79 @@
 #include <algorithm>
 using namespace std;
 
-int N, M, ans = -101;
+int N, M, ans = - 101;
+int paper[12][12];
+vector <int> row, column;
 
-void BackTracking(int arr[6][6], int height, int width, int row, int column);
+void BackTracking(int len, vector<int> prev_state, bool dir);
 
 int main(void) {
 	cin >> N >> M;
-	int org[6][6];
-	for (int i = 0; i < N; i++) {		
+	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			cin >> org[i][j];
+			cin >> paper[i][j];
+			ans = max(ans, paper[i][j]);
+		}
+	}		
+	vector <int> org(12); // 가장 초기의 state
+	for (int i = 0; i < N; i++) 
+		org[i] = (1 << i);
+	BackTracking(N, org, true);	
+	for (int i = 0; i < M; i++)
+		org[i] = (1 << i);
+	BackTracking(M, org, false);
+
+	column.erase(unique(column.begin(), column.end()), column.end());
+	row.erase(unique(row.begin(), row.end()), row.end());
+	
+	for (int i = 0; i < column.size(); i++) { // 세로를 먼저 접는다
+		vector <int> temp(12); // 각 가로줄에 대한 정보를 저장
+		for (int j = 0; j < N; j++) {
+			if (column[i] & (1 << j)) { // 접힘의 결과로 두 행이 서로 겹친다면
+				for (int k = 0; k < M; k++) { // 모든 가로에 대하여 값을 더해준다
+					temp[k] += paper[j][k];
+				}
+			}				
+		}
+		for (int j = 0; j < row.size(); j++) { // 접힌 세로에 대해서 가로를 접는다
+			int result = 0;
+			for (int k = 0; k < M; k++) { // 어떠한 가로열에 대해서,
+				if (row[j] & (1 << k)) { // 접힘의 결과로 두 열이 서로 겹친다면
+					result += temp[k]; // 최종 접힘의 결과는 그 가로열을 모두 더해준 것이다
+				}
+			}
+			ans = max(ans, result);
 		}
 	}
-	BackTracking(org, N, M, 0, 0);
-	cout << ans;	
+	cout << ans;
 	return 0;
 }
 
-void BackTracking(int arr[6][6], int prev_height, int prev_width, int row, int column) {
-	int height = max(prev_height - row, row);
-	int width = max(prev_width - column, column);
-	int temp[6][6];
-	if (row == 0) { // 가로로 접은 경우
-		if (width > column) {
-			for (int i = 0; i < height; i++) {
-				for (int j = 0; j < width; j++) {
-					if (j < column) temp[i][j] = arr[i][column + j] + arr[i][column - j - 1];
-					else temp[i][j] = arr[i][column + j];					
-				}
+void BackTracking(int len, vector<int> prev_state, bool dir) {
+	if (dir == true) {
+		for (int i = 0; i < len; i++)
+			column.push_back(prev_state[i]); // 겹쳐진 종이의 값만 중요하므로 각 칸의 상태만 전달
+	}
+	else {
+		for (int i = 0; i < len; i++)
+			row.push_back(prev_state[i]);
+	}	
+	for (int i = 1; i < len; i++) { // 접을 위치를 선택, 접지 않는 경우는 이미 고려했으므로 1부터 시작
+		int newlen = max(i, len - i); // 접힌 뒤 총 길이
+		vector <int> now(newlen); 
+		if (newlen > i) {
+			for (int j = 0; j < newlen; j++) {
+				if(j < i) now[j] = prev_state[i - j - 1] | prev_state[i + j];
+				else now[j] = prev_state[i + j];
 			}
 		}
 		else {
-			for (int i = 0; i < height; i++) {
-				for (int j = 0; j < width; j++) {
-					if (j < prev_width - column) temp[i][width - 1 - j] = arr[i][width - 1 - j] + arr[i][width + j];
-					else temp[i][width - 1 - j] = arr[i][width - 1 - j];					
-				}
+			for (int j = 0; j < newlen; j++) {
+				if (j < len - i) now[j] = prev_state[newlen - j - 1] | prev_state[newlen + j];
+				else now[j] = prev_state[newlen - 1 - j];
 			}
 		}
-	} // 세로로 접은 경우
-	else {
-		if (height > row) {
-			for (int i = 0; i < height; i++) {
-				for (int j = 0; j < width; j++) {
-					if (i < row) temp[i][j] = arr[i + row][j] + arr[row - 1 - i][j];
-					else temp[i][j] = arr[i + row][j];					
-				}
-			}
-		}
-		else {
-			for (int i = 0; i < height; i++) {
-				for (int j = 0; j < width; j++) {
-					if (i < prev_height - row) temp[height - 1 - i][j] = arr[height - 1 - i][j] + arr[height + i][j];
-					else temp[height - 1 - i][j] = arr[height - 1 - i][j];					
-				}
-			}
-		}
+		BackTracking(newlen, now, dir);
 	}
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++)
-			ans = max(ans, temp[i][j]);
-	}
-	if (height == 1 && width == 1) {		
-		return;
-	}
-	else {
-		for (int i = 0; i < height; i++) {
-			if (i == 0) {
-				for (int j = 1; j < width; j++) 
-					BackTracking(temp, height, width, 0, j);				
-			}
-			else {
-				BackTracking(temp, height, width, i, 0);
-			}
-		}		
-		return;
-	}
+	return;
 }
