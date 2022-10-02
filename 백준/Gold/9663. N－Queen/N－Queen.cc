700ef9bc5855039e2be8f18c0e83fbf26aa92331
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-10-02 23:21:55 +0900

    [Gold IV] Title: N-Queen, Time: 1596 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -2,10 +2,11 @@
 using namespace std;
 
 int N, num = 0;
-int chessboard[14][14] = { 0 };
+int row[14];
+int rightUpper[28];
+int leftUpper[28];
 
 void N_Queen(int k);
-bool CanPutQueen(int x, int y);
 
 int main(void) {
 	cin >> N;
@@ -16,41 +17,20 @@ int main(void) {
 
 void N_Queen(int k) {
 	if (k == N) {
-		num++;		
+		num++;
 		return;
 	}
 	else {
 		for (int i = 0; i < N; i++) {
-			if (CanPutQueen(i, k) == true) {
-				chessboard[k][i] = 1;
+			if (row[i] == 0 && rightUpper[i + k] == 0 && leftUpper[N - 1 + k - i] == 0) {
+				row[i] = 1;
+				rightUpper[i + k] = 1;
+				leftUpper[N - 1 + k - i] = 1;
 				N_Queen(k + 1);
-				chessboard[k][i] = 0;
+				row[i] = 0;
+				rightUpper[i + k] = 0;
+				leftUpper[N - 1 + k - i] = 0;
 			}
 		}
 	}
-}
-
-bool CanPutQueen(int x, int y) {
-	int i;
-	for (i = 0; i < y; i++) {
-		if (chessboard[i][x] == 1)
-			return false;
-	}
-	i = 0;
-	while (1) {
-		if (x - i < 0 || y - i < 0)
-			break;
-		if (chessboard[y - i][x - i] == 1)
-			return false;
-		i++;
-	}
-	i = 0;
-	while (1) {
-		if (x + i >= N || y - i < 0)
-			break;
-		if (chessboard[y - i][x + i] == 1)
-			return false;
-		i++;
-	}
-	return true;
 }
