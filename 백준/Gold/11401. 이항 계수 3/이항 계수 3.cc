e2934cc904ff0a6c8ab488cc4cda981116a2897d
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-10-07 09:56:30 +0900

    [Gold I] Title: 이항 계수 3, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
@@ -4,8 +4,7 @@ using namespace std;
 const long long mod = 1000000007;
 
 int N, K;
-long long nFact, bFact, ans;
-long long exponential[31];
+long long nFact, bFact, ans, exponential;
 
 int main(void) {
 	cin >> N >> K;
@@ -13,14 +12,14 @@ int main(void) {
 	for (int i = 1; i <= N; i++) nFact = (nFact * i) % mod;
 	bFact = 1;
 	for (int i = 1; i <= K; i++) bFact = (bFact * i) % mod;
-	for (int i = 1; i <= N - K; i++) bFact = (bFact * i) % mod;
-	exponential[1] = bFact;
-	for (int i = 2; i <= 30; i++) exponential[i] = (exponential[i - 1] * exponential[i - 1]) % mod;
+	for (int i = 1; i <= N - K; i++) bFact = (bFact * i) % mod;	
+	exponential = bFact;
 	long long result = 1;
 	int bit = 1;
 	for (int i = 1; i <= 30; i++) {
 		if (bit & (mod - 2))
-			result = (result * exponential[i]) % mod;
+			result = (result * exponential) % mod;
+		exponential = (exponential * exponential) % mod;
 		bit <<= 1;
 	}
 	ans = (nFact * result) % mod;
