f3a0a52f01af4686ee5c1a32e014c3cda339cb8b
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-10-07 09:32:30 +0900

    [Gold I] Title: 이항 계수 3, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.cc
@@ -2,18 +2,19 @@
 using namespace std;
 
 const long long mod = 1000000007;
-int N, K;
-long long ans;
-long long factorial[4000001];
-long long ifactorial[4000001];
-long long exponential[31];
 
+int N, K;
+long long nFact, bFact, ans;
+long long exponential[31];
 
 int main(void) {
 	cin >> N >> K;
-	factorial[0] = 1;	
-	for (int i = 1; i <= N; i++) factorial[i] = (factorial[i - 1] * i) % mod;
-	exponential[1] = factorial[N];
+	nFact = 1;
+	for (int i = 1; i <= N; i++) nFact = (nFact * i) % mod;
+	bFact = 1;
+	for (int i = 1; i <= K; i++) bFact = (bFact * i) % mod;
+	for (int i = 1; i <= N - K; i++) bFact = (bFact * i) % mod;
+	exponential[1] = bFact;
 	for (int i = 2; i <= 30; i++) exponential[i] = (exponential[i - 1] * exponential[i - 1]) % mod;
 	long long result = 1;
 	int bit = 1;
@@ -22,9 +23,7 @@ int main(void) {
 			result = (result * exponential[i]) % mod;
 		bit <<= 1;
 	}
-	ifactorial[N] = result;
-	for (int i = N - 1; i >= 0; i--) ifactorial[i] = (ifactorial[i + 1] * (i + 1)) % mod;
-	ans = ((factorial[N] * ifactorial[K]) % mod * ifactorial[N - K]) % mod;
+	ans = (nFact * result) % mod;
 	cout << ans;
 	return 0;
 }
