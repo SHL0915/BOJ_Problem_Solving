4e0dde0a47e6a2245e4357544061081f97d6a943
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-10-07 14:48:28 +0900

    [Gold II] Title: N포커, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Gold/16565. N포커/N포커.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+const int mod = 10007;
+
+int N;
+long long combination[53][53];
+long long memo[53];
+
+long long comb(int N, int K);
+long long DP(int N);
+
+int main(void) {
+	cin >> N;	
+	cout << DP(N);		
+	return 0;
+}
+
+long long comb(int N, int K) {
+	if (combination[N][K] != 0) return combination[N][K];
+	else if (K == 0 || K == N) combination[N][K] = 1;
+	else combination[N][K] = (comb(N - 1, K - 1) + comb(N - 1, K)) % mod;
+	return combination[N][K];
+}
+
+long long DP(int N) {
+	if (N < 4) return 0;
+	else if (memo[N] != 0) return memo[N];
+	else if (N == 4) memo[N] = 13;
+	else {
+		for (int i = 1; i <= N / 4; i++) {
+			if (i % 2 == 1) {
+				memo[N] += (comb(13, i) % mod * ((comb(52 - i * 4, N - i * 4) % mod) % mod)) % mod;
+				memo[N] = memo[N] % mod;
+			}
+			else {
+				memo[N] -= (comb(13, i) % mod * ((comb(52 - i * 4, N - i * 4) % mod) % mod)) % mod;
+				memo[N] = (memo[N] + mod) % mod;
+			}
+		}
+	}	
+	return memo[N];
+}
