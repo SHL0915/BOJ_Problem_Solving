189c8c15594d55eeafea372f08500af213f8d627
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-10-11 14:16:25 +0900

    [Gold IV] Title: 단어 수학, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.cc
+++ 백준/Gold/1339. 단어 수학/단어 수학.cc
@@ -1,60 +1,41 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-int N, alphIndex, ans;
-string words[10];
+int N, num = 9, ans;
+string word[10];
 int Alphabet[26];
-int AlphNum[10];
-int mark[10];
-
-int calculate();
-void BackTracking(int k);
-
+int cnt[26];
 
 int main(void) {
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> words[i];
-		for (int j = 0; j < words[i].length(); j++) {
-			if (Alphabet[words[i][j] - 'A'] == 0) {
-				AlphNum[alphIndex++] = words[i][j] - 'A';
-				Alphabet[words[i][j] - 'A'] = 1;
-			}
+	for (int i = 0; i < N; i++) cin >> word[i];
+	for (int i = 0; i < 26; i++) Alphabet[i] = -1;
+	int temp = 10000000;
+	for (int i = 0; i < 8; i++) {		
+		for (int j = 0; j < N; j++) {
+			if (word[j].length() < 8 - i) continue;	
+			cnt[word[j][word[j].length() - (8 - i)] - 'A'] += temp;
 		}
+		temp /= 10;
 	}
-	BackTracking(0);
+	while (1) {
+		int index = 0, sum = 0;
+		for (int i = 0; i < 26; i++) {			
+			if (cnt[index] < cnt[i]) index = i;
+			sum += cnt[i];
+		}
+		if (sum == 0) break;
+		Alphabet[index] = num--;
+		cnt[index] = 0;
+	}
+	temp = 10000000;
+	for (int i = 0; i < 8; i++) {			
+		for (int j = 0; j < N; j++) {
+			if (word[j].length() < 8 - i) continue;
+			ans += temp * Alphabet[word[j][word[j].length() - (8 - i)] - 'A'];
+		}
+		temp /= 10;
+	}
 	cout << ans;
 	return 0;
-}
-
-int calculate() {
-	int sum = 0;
-	for (int i = 0; i < N; i++) {
-		int num = 0;
-		int temp = 1;
-		for (int j = 0; j < words[i].length(); j++) {
-			num += Alphabet[words[i][words[i].length() - 1 - j] - 'A'] * temp;
-			temp *= 10;
-		}
-		sum += num;
-	}
-	return sum;
-}
-
-void BackTracking(int k) {
-	if (k == alphIndex) {
-		ans = max(ans, calculate());
-		return;
-	}
-	else {
-		for (int i = 0; i < 10; i++) {
-			if (mark[i] == 0) {
-				mark[i] = 1;
-				Alphabet[AlphNum[k]] = i;
-				BackTracking(k + 1);
-				mark[i] = 0;
-			}
-		}
-	}
 }
