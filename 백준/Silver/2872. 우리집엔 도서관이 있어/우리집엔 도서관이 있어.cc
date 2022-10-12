c067eb9a01ed5def7a0169c8a95d465792d01954
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-10-13 00:09:12 +0900

    [Silver II] Title: 우리집엔 도서관이 있어, Time: 32 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.cc
+++ 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.cc
@@ -1,55 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-int N, ans;
-int input[300000];
-pair <int, int> books[300000];
-int mark[300001];
-
-bool cmp(pair <int, int> A, pair <int, int> B) {
-	return A.first < B.first;
-}
+int N, idx, temp, cnt;
+int books[300000];
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> N;
 	for (int i = 0; i < N; i++) {
-		cin >> input[i];
-		books[i] = { input[i], i };
+		cin >> books[i];
+		if (books[i] == N)
+			idx = i;
 	}
-	sort(books, books + N, cmp);
-	for (int i = N; i >= 1; i--) {
-		if (mark[i] != 0) {
-			for (int j = 0; j < books[i + 1].second; j++) {
-				if (mark[input[j]] == 0) {
-					mark[input[j]] = 1;
-					ans++;
-				}
-			}
-			break;
-		}
-		if (i == N) {
-			mark[input[books[i - 1].second]] = 1;
-			for (int j = books[i - 1].second; j < N; j++) {
-				if (mark[input[j]] == 0) {
-					mark[input[j]] = 1;
-					ans++;
-				}
-			}
-		}
-		else {
-			mark[input[books[i - 1].second]] = 1;
-			for (int j = books[i - 1].second; j < books[i].second; j++) {
-				if (mark[input[j]] == 0) {
-					mark[input[j]] = 1;
-					ans++;
-				}
-			}
+	temp = N;
+	for (int i = idx; i >= 0; i--) {
+		if (books[i] == temp) {
+			cnt++;
+			temp--;
 		}
 	}
-	cout << ans;
+	cout << N - cnt;
 	return 0;
 }
