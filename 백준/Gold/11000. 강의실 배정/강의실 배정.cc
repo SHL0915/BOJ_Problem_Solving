5e472a73fc58b14763771a52fc9d5dfc3cdfde08
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-10-13 09:03:21 +0900

    [Gold V] Title: 강의실 배정, Time: 200 ms, Memory: 5108 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int N, cnt;
+vector <pair <int, int>> classes;
+
+bool cmp(pair <int, int> A, pair <int, int> B) {
+	if (A.first == B.first) return A.second < B.second;
+	return A.first < B.first;
+}
+
+struct lower {
+	bool operator() (int A, int B) {
+		return A > B;
+	}
+};
+
+int main(void) {
+	priority_queue <int, vector <int>, lower> pq;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int S, T;
+		cin >> S >> T;
+		classes.push_back({ S,T });
+	}
+	sort(classes.begin(), classes.end(), cmp);
+	for (int i = 0; i < N; i++) {
+		if (pq.size() == 0) pq.push(classes[i].second);
+		else {
+			if (classes[i].first >= pq.top()) {
+				pq.pop();
+				pq.push(classes[i].second);
+			}
+			else pq.push(classes[i].second);
+		}
+	}
+	cout << pq.size();
+	return 0;
+}
