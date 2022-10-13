4c41fa3dc6283fe653e2d4c4a260eac836f6fadf
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-10-13 09:22:44 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 36 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N, ans;
+
+struct lower {
+	bool operator() (int A, int B) {
+		return A > B;
+	}
+};
+
+int main(void) {
+	cin >> N;
+	priority_queue <int, vector<int>, lower> pq;
+	for (int i = 0; i < N; i++) {
+		int input;
+		cin >> input;
+		pq.push(input);
+	}
+	while (pq.size() != 1) {
+		int temp = pq.top();
+		pq.pop();
+		temp += pq.top();
+		ans += temp;
+		pq.pop();
+		pq.push(temp);
+	}
+	cout << ans;
+	return 0;
+}
