aa4fa3aee716dbea55d1a187bce6404fda311071
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-10-21 10:15:17 +0900

    [Silver IV] Title: 평행선, Time: 56 ms, Memory: 5128 KB -BaekjoonHub

--- 백준/Silver/2358. 평행선/평행선.cc
+++ 백준/Silver/2358. 평행선/평행선.cc
@@ -1,32 +1,33 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <map>
 using namespace std;
 
 int N, ans;
+int arr[100000][2];
 vector <int> x_val, y_val;
-map <int, int> dots_x;
-map <int, int> dots_y;
+int x_pos[100000];
+int y_pos[100000];
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		int x, y;
-		cin >> x >> y;
-		dots_x[x]++;
-		dots_y[y]++;
-		x_val.push_back(x);
-		y_val.push_back(y);
+	for (int i = 0; i < N; i++) {	
+		cin >> arr[i][0] >> arr[i][1];
+		x_val.push_back(arr[i][0]);
+		y_val.push_back(arr[i][1]);
 	}
 	sort(x_val.begin(), x_val.end());
 	sort(y_val.begin(), y_val.end());
 	x_val.erase(unique(x_val.begin(), x_val.end()), x_val.end());
 	y_val.erase(unique(y_val.begin(), y_val.end()), y_val.end());
-	for (int i = 0; i < x_val.size(); i++) if (dots_x[x_val[i]] >= 2) ans++;
-	for (int i = 0; i < y_val.size(); i++) if (dots_y[y_val[i]] >= 2) ans++;
+	for (int i = 0; i < N; i++) {
+		x_pos[lower_bound(x_val.begin(), x_val.end(), arr[i][0]) - x_val.begin()]++;
+		y_pos[lower_bound(y_val.begin(), y_val.end(), arr[i][1]) - y_val.begin()]++;
+	}	
+	for (int i = 0; i < x_val.size(); i++) if (x_pos[i] >= 2) ans++;
+	for (int i = 0; i < y_val.size(); i++) if (y_pos[i] >= 2) ans++;
 	cout << ans;
 	return 0;
 }
