64adb74afce6a156feaf314050c22484c72d3396
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-10-24 15:08:25 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 36 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+long long input, ans;
+vector <pair<long long, long long>> s;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	while (1) {
+		cin >> N;
+		if (N == 0) break;
+		ans = 0;
+		s.clear();
+		for (int i = 0; i < N; i++) {
+			cin >> input;
+			if (s.size() == 0) s.push_back({ input, i });
+			else if (input > s.back().first) s.push_back({ input, i });
+			else if (input == s.back().first) continue;
+			else {
+				while (1) {
+					pair <long long, long long> temp = s.back();
+					ans = max(ans, temp.first * (i - temp.second));
+					s.pop_back();
+					if (s.size() == 0) {
+						s.push_back({ input,0 });
+						break;
+					}
+					if (input > s.back().first) {
+						s.push_back({ input, temp.second });
+						break;
+					}
+					else if (input == s.back().first) break;
+				}
+			}
+		}
+		while (s.size()) {
+			pair <long long, long long> temp = s.back();
+			s.pop_back();
+			ans = max(ans, temp.first * (N - temp.second));
+		}
+		cout << ans << '\n';
+	}
+	return 0;
+}
