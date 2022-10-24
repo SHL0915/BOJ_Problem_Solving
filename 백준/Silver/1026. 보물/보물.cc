c7597354b93e9cf28f2a56888c0dda72b7dcf1de
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-10-24 16:53:21 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.cc
+++ 백준/Silver/1026. 보물/보물.cc
@@ -2,20 +2,19 @@
 #include <algorithm>
 using namespace std;
 
-int N, sum;
+int N, ans;
 int A[50];
 int B[50];
 
 int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	cin >> N;
-	for (int i = 0; i < N; i++) 
-		cin >> A[i];
-	for (int i = 0; i < N; i++)
-		cin >> B[i];
+	for (int i = 0; i < N; i++) cin >> A[i];
+	for (int i = 0; i < N; i++) cin >> B[i];
 	sort(A, A + N);
 	sort(B, B + N);
-	for (int i = 0; i < N; i++)
-		sum += A[i] * B[N - 1 - i];
-	cout << sum;
+	for (int i = 0; i < N; i++) ans += A[i] * B[N - 1 - i];
+	cout << ans;
 	return 0;
 }
