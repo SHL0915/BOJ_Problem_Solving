2191a380ca070556b35441f39a37aecbec33c469
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-02 12:01:38 +0900

    [Gold I] Title: Daruma Otoshi, Time: 244 ms, Memory: 3000 KB -BaekjoonHub

---
+++ 백준/Gold/13283. Daruma Otoshi/Daruma Otoshi.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int weight[500];
+int table[500][500];
+
+int absol(int A) {
+	if (A >= 0) return A;
+	else return -1 * A;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	while (1) {
+		cin >> N;
+		if (N == 0) break;		
+		for (int i = 0; i < N; i++) cin >> weight[i];
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j <= N; j++) table[i][j] = 0;
+		}		
+		for (int i = 1; i <= N; i++) {
+			if (i == 2) {
+				for (int j = 0; j < N - 1; j++) {
+					if (absol(weight[j] - weight[j + 1]) <= 1) table[j][j + 1] = 2;
+				}
+			}
+			else {
+				for (int j = 0; j < N - i + 1; j++) {
+					for (int k = 0; k < i - 1; k++) table[j][j + i - 1] = max(table[j][j + i - 1], table[j][j + k] + table[j + k + 1][j + i - 1]);
+					if (((i % 2) == 0) && (absol(weight[j] - weight[j + i - 1]) <= 1) && (table[j + 1][j + i - 2] == i - 2)) table[j][j + i - 1] = max(table[j][j + i - 1], i);
+				}
+			}
+		}
+		cout << table[0][N - 1] << '\n';
+	}
+	return 0;
+}
