cd7de6365108b5d56b886e5ce69e4868995189b8
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-07 00:09:56 +0900

    [Silver IV] Title: 행운의 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2840. 행운의 바퀴/행운의 바퀴.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, K, ptr;
+vector <char> arr;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N >> K;
+	arr.resize(N);
+	for (int i = 0; i < N; i++) arr[i] = '?';
+	for (int i = 0; i < K; i++) {
+		int change;
+		char c;
+		cin >> change >> c;
+		ptr += change;
+		ptr %= N;
+		if (arr[ptr] == '?') arr[ptr] = c;		
+		else if (arr[ptr] != c) {
+			cout << "!";
+			return 0;
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			if (arr[i] != '?' && arr[i] == arr[j]) {
+				cout << '!';
+				return 0;
+			}
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		cout << arr[ptr];
+		ptr--;
+		if (ptr < 0) ptr = N - 1;
+	}
+	return 0;
+}
