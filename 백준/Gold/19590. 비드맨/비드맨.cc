db61df922fb718fa5917ac3b833575b5f15ce442
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-07 14:38:36 +0900

    [Gold II] Title: 비드맨, Time: 16 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/19590. 비드맨/비드맨.cc
+++ 백준/Gold/19590. 비드맨/비드맨.cc
@@ -6,20 +6,17 @@ int N;
 long long arr[100000];
 long long sum;
 
-bool cmp(long long A, long long B) {
-	return A > B;
-}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> N;
 	for (int i = 0; i < N; i++) cin >> arr[i];
-	sort(arr, arr + N, cmp);
-	for (int i = 1; i < N; i++) sum += arr[i];
-	if (arr[0] > sum) cout << arr[0] - sum;
+	sort(arr, arr + N);
+	for (int i = 0; i < N - 1; i++) sum += arr[i];
+	if (arr[N - 1] > sum) cout << arr[N - 1] - sum;
 	else {
-		if ((arr[0] + sum) % 2 == 0) cout << 0;
+		if ((arr[N - 1] + sum) % 2 == 0) cout << 0;
 		else cout << 1;
 	}
 	return 0;
