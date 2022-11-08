1f13366ceb10911faba88d3b9ab91a806437eee6
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-08 11:32:38 +0900

    [Gold V] Title: 귀찮은 해강이, Time: 52 ms, Memory: 8368 KB -BaekjoonHub

--- 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.cc
+++ 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.cc
@@ -6,7 +6,7 @@ int N, M, cnt, ans, input, curr, before;
 vector <int> graph[100001];
 int mark[100001];
 
-void BFS(int vertex);
+void DFS(int vertex);
 
 int main(void) {
 	ios::sync_with_stdio(false);
@@ -21,7 +21,7 @@ int main(void) {
 	for (int i = 1; i <= N; i++) {
 		if (mark[i] == 0) {
 			cnt++;
-			BFS(i);
+			DFS(i);
 		}
 	}
 	for (int i = 0; i < N; i++) {
@@ -34,9 +34,9 @@ int main(void) {
 	return 0;
 }
 
-void BFS(int vertex) {
+void DFS(int vertex) {
 	if (mark[vertex] != 0) return;
 	mark[vertex] = cnt;
-	for (int i = 0; i < graph[vertex].size(); i++) BFS(graph[vertex][i]);
+	for (int i = 0; i < graph[vertex].size(); i++) DFS(graph[vertex][i]);
 	return;
 }
