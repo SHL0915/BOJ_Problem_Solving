8a59d79f33f640ba8e6044fccd7bc599ab39af8c
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-11 13:17:13 +0900

    [Platinum III] Title: 釘 (Nails), Time: 296 ms, Memory: 99716 KB -BaekjoonHub

---
+++ 백준/Platinum/5541. 釘 （Nails）/釘 （Nails）.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M, cnt;
+int table[5001][5001];
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		int A, B, C;
+		cin >> A >> B >> C;
+		table[A][B] = max(table[B][A], C + 1);
+	}
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			int val;
+			val = max(table[i - 1][j] - 1, table[i - 1][j - 1] - 1);
+			val = max(val, table[i][j]);
+			if (val != 0) cnt++;
+			table[i][j] = val;
+		}
+	}
+	cout << cnt;
+	return 0;
+}
