e7f8852e29fdf240b0905c27691084687883aa64
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-13 15:09:00 +0900

    [Platinum II] Title: 가장 가까운 두 점, Time: 72 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Platinum/2261. 가장 가까운 두 점/가장 가까운 두 점.cc
+++ 백준/Platinum/2261. 가장 가까운 두 점/가장 가까운 두 점.cc
@@ -5,56 +5,51 @@ using namespace std;
 
 const int INF = 1234567890;
 
-int N;
+int N, ans = INF;
 vector <pair<int, int>> arr;
 
-bool cmp(pair<int, int> A, pair<int, int> B) {
-	return A.first < B.first;
-}
-
 int DNC(int left, int right);
-int sqrDis(int A, int B) {
-	return (arr[A].first - arr[B].first) * (arr[A].first - arr[B].first) + (arr[A].second - arr[B].second) * (arr[A].second - arr[B].second);
+int sqrDis(pair<int, int> A, pair<int, int> B) {
+	return (A.first - B.first) * (A.first - B.first) + (A.second - B.second) * (A.second - B.second);
 }
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> N;
-	arr.resize(N);
 	for (int i = 0; i < N; i++) {
 		int x, y;
 		cin >> x >> y;
-		arr[i] = { x,y };
+		arr.push_back({ x,y });
 	}
-	sort(arr.begin(), arr.end(), cmp);
-	cout << DNC(0, N - 1);
+	sort(arr.begin(), arr.end());
+	ans = min(ans, DNC(0, N - 1));
+	cout << ans;
 	return 0;
 }
 
 int DNC(int left, int right) {
 	int mid = (left + right) / 2;
-	if (right - left == 2) {
-		int A = min(sqrDis(left, left + 1), sqrDis(left + 1, right));
-		A = min(A, sqrDis(left, right));
-		return A;	
-	}
-	if (right - left == 1) return sqrDis(left, right);
-	if (right == left) return INF;
+	if (left == right) return INF;
+	if (right - left == 1) return sqrDis(arr[left], arr[right]);
 	else {
 		int A = min(DNC(left, mid), DNC(mid + 1, right));
 		vector <pair<int, int>> temp;
-		int line = (arr[mid].first + arr[mid + 1].first) / 2;
 		for (int i = left; i <= right; i++) {
-			if ((line - arr[i].first) * (line - arr[i].first) < A) temp.push_back({ arr[i].second, arr[i].first });
+			if ((arr[mid].first - arr[i].first) * (arr[mid].first - arr[i].first) < A) temp.push_back({arr[i].second, arr[i].first});
 		}
-		sort(temp.begin(), temp.end(), cmp);
+		if (temp.size() == 0) return A;
+		sort(temp.begin(), temp.end());
 		for (int i = 0; i < temp.size(); i++) {
 			for (int j = i + 1; j < temp.size(); j++) {
-				int diffX = temp[i].second - temp[j].second;
-				int diffY = temp[i].first - temp[j].first;
-				if (diffY * diffY >= A) break;
-				A = min(A, diffX * diffX + diffY * diffY);
+				int d = (temp[i].first - temp[j].first) * (temp[i].first - temp[j].first);
+				if (d > A) break;
+				int d2 = (temp[i].second - temp[j].second) * (temp[i].second - temp[j].second);
+				if (d2 > A) continue;
+				else {
+					int dist = sqrDis(temp[i], temp[j]);
+					A = min(A, dist);
+				}
 			}
 		}
 		return A;
