1b1096982beb939fa1d6b7f7b972b684dd790771
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-13 15:39:02 +0900

    [Platinum IV] Title: 멀티탭 스케줄링 2, Time: 640 ms, Memory: 350240 KB -BaekjoonHub

--- 백준/Platinum/20136. 멀티탭 스케줄링 2/멀티탭 스케줄링 2.cc
+++ 백준/Platinum/20136. 멀티탭 스케줄링 2/멀티탭 스케줄링 2.cc
@@ -1,22 +1,19 @@
 #include <iostream>
-#include <algorithm>
 #include <queue>
-#include <vector>
 using namespace std;
 
 const int INF = 1234567890;
 
 int N, K, cnt, ans;
-vector <int> arr;
-deque <int> tool[500001];
+int arr[500001];
 int mark[500001];
-priority_queue <pair<int, int>> multitap;
+deque <int> tool[500001];
+priority_queue<pair<int, int>> pq;
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> N >> K;
-	arr.resize(K);
 	for (int i = 0; i < K; i++) {
 		cin >> arr[i];
 		tool[arr[i]].push_back(i);
@@ -25,18 +22,18 @@ int main(void) {
 	for (int i = 0; i < K; i++) {
 		tool[arr[i]].pop_front();
 		if (mark[arr[i]] == 1) {
-			multitap.push({ tool[arr[i]].front(), arr[i] });
+			pq.push({ tool[arr[i]].front(), arr[i] });
 			continue;
 		}
 		if (cnt < N) {
-			multitap.push({ tool[arr[i]].front(), arr[i] });
+			pq.push({ tool[arr[i]].front(), arr[i] });
 			mark[arr[i]] = 1;
 			cnt++;
 		}
 		else {
-			mark[multitap.top().second] = 0;
-			multitap.pop();
-			multitap.push({ tool[arr[i]].front(), arr[i] });
+			mark[pq.top().second] = 0;
+			pq.pop();
+			pq.push({ tool[arr[i]].front(), arr[i] });
 			mark[arr[i]] = 1;
 			ans++;
 		}
