ab612b22734119944edfd569ade6f0ac2d1708c1
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-15 20:02:44 +0900

    [Gold II] Title: 트리 정리하기, Time: 4 ms, Memory: 3648 KB -BaekjoonHub

--- 백준/Gold/23844. 트리 정리하기/트리 정리하기.cc
+++ 백준/Gold/23844. 트리 정리하기/트리 정리하기.cc
@@ -3,17 +3,11 @@
 #include <vector>
 using namespace std;
 
-int N, K, max_lev;
+int N, K, max_lev, ans;
 vector <int> tree[10001];
-int table[10001];
-int par[10001];
 vector <int> level[10001];
 
 void DFS(int node, int parent, int lev);
-
-bool cmp(int A, int B) {
-	return table[A] > table[B];
-}
 
 int main(void) {
 	ios::sync_with_stdio(false);
@@ -26,35 +20,16 @@ int main(void) {
 		tree[B].push_back(A);
 	}
 	DFS(1, -1, 0);
-	for (int i = max_lev; i > 0; i--) {
-		if (level[i].size() > K) {
-			sort(level[i].begin(), level[i].end(), cmp);
-			while (level[i].size() > K) {
-				int temp = level[i].back();
-				while (1) {
-					if (temp == -1) break;
-					table[par[temp]] -= table[level[i].back()];
-					temp = par[temp];
-				}
-				level[i].pop_back();
-			}
-		}
-	}
-	cout << table[1];
+	for (int i = max_lev; i >= 0; i--) ans += min(K, (int)level[i].size());
+	cout << ans;
 	return 0;
 }
 
 void DFS(int node, int parent, int lev) {
-	if (table[node] != 0) return;
-	table[node] = 1;
 	max_lev = max(max_lev, lev);
-	par[node] = parent;
 	level[lev].push_back(node);
 	for (int i = 0; i < tree[node].size(); i++) {
-		if (tree[node][i] != parent) {
-			DFS(tree[node][i], node, lev + 1);
-			table[node] += table[tree[node][i]];
-		}
+		if (tree[node][i] != parent) DFS(tree[node][i], node, lev + 1);		
 	}
 	return;
 }
