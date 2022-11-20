2d464d427fbf7843ed283e52200863f9d450a69a
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-20 14:05:54 +0900

    [Platinum III] Title: 회사 문화 2, Time: 76 ms, Memory: 19924 KB -BaekjoonHub

---
+++ 백준/Platinum/14268. 회사 문화 2/회사 문화 2.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M, cnt;
+long long arr[100001];
+int s[100001];
+int e[100001];
+vector <long long> tree, lazy;
+vector <int> graph[100001];
+
+void DFS(int node);
+void update_lazy(int node, int start, int end);
+void update(int node, int start, int end, int left, int right, long long diff);
+long long query(int node, int start, int end, int left, int right);
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N >> M;
+	int input;
+	cin >> input;
+	for (int i = 2; i <= N; i++) {
+		cin >> input;
+		graph[input].push_back(i);
+	}
+	DFS(1);
+	tree.resize(4 * N);
+	lazy.resize(4 * N);	
+	for (int i = 0; i < M; i++) {
+		long long A, B, C;
+		cin >> A >> B;
+		if (A == 1) {
+			cin >> C;
+			update(1, 1, N, s[B], e[B], C);
+		}
+		else cout << query(1, 1, N, s[B], s[B]) << '\n';
+	}
+	return 0;
+}
+
+void DFS(int node) {	
+	cnt++;
+	s[node] = cnt;
+	for (int i = 0; i < graph[node].size(); i++) DFS(graph[node][i]);
+	e[node] = cnt;
+}
+
+void update_lazy(int node, int start, int end) {
+	if (lazy[node] != 0) {
+		tree[node] += lazy[node] * (end - start + 1);
+		if (start != end) {
+			lazy[node * 2] += lazy[node];
+			lazy[node * 2 + 1] += lazy[node];
+		}
+		lazy[node] = 0;
+	}
+	return;
+}
+
+void update(int node, int start, int end, int left, int right, long long diff) {
+	update_lazy(node, start, end);
+	if (left > end || right < start) return;
+	if (left <= start && end <= right) {
+		lazy[node] += diff;
+		update_lazy(node, start, end);
+		return;
+	}
+	int mid = (start + end) / 2;
+	update(node * 2, start, mid, left, right, diff);
+	update(node * 2 + 1, mid + 1, end, left, right, diff);
+	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	return;
+}
+
+long long query(int node, int start, int end, int left, int right) {
+	update_lazy(node, start, end);
+	if (left > end || right < start) return 0;
+	if (left <= start && end <= right) return tree[node];
+	int mid = (start + end) / 2;
+	return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
+}
