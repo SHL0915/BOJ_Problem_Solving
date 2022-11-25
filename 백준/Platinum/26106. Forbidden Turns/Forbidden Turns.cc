53e5571e32de1e44c8bd90c7dd75a2f79fa7e997
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-25 14:52:48 +0900

    [Platinum V] Title: Forbidden Turns, Time: 688 ms, Memory: 45788 KB -BaekjoonHub

--- 백준/Platinum/26106. Forbidden Turns/Forbidden Turns.cc
+++ 백준/Platinum/26106. Forbidden Turns/Forbidden Turns.cc
@@ -11,11 +11,11 @@ struct cmp {
 };
 
 int M, N, K, S, E;
-long long ans = INF;
 vector <pair<long long, int>> graph[30000];
-vector <int> compressed[30000];
-long long dist[30000][10];
+vector <int> path[30000];
+long long dist[30000];
 set <piii> forbidden;
+set <pair<int, int>> edge;
 
 void Dijkstra(int start);
 
@@ -29,52 +29,45 @@ int main(void) {
 		long long c;
 		cin >> x >> y >> c;
 		graph[x].push_back({ c,y });
-		compressed[y].push_back(x);
+		edge.insert({ x,y });
 	}
-	for (int i = 0; i < N; i++) sort(compressed[i].begin(), compressed[i].end());	
 	for (int i = 0; i < K; i++) {
 		int x, y, z;
 		cin >> x >> y >> z;
 		forbidden.insert({ {x,y},z });
 	}
 	Dijkstra(S);
-	for (int i = 0; i < 10; i++) ans = min(ans, dist[E][i]);
 	if (S == E) cout << 0;
-	else if (ans == INF) cout << -1;
-	else cout << ans;
+	else if (dist[E] == INF) cout << -1;
+	else cout << dist[E];
 	return 0;
 }
 
 void Dijkstra(int start) {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < 10; j++) dist[i][j] = INF;
-	}
+	for (int i = 0; i < N; i++) dist[i] = INF;
 	priority_queue<plii, vector<plii>, cmp> pq;
 	for (int i = 0; i < graph[start].size(); i++) {
-		int next = graph[start][i].second;
-		int now_idx = lower_bound(compressed[next].begin(), compressed[next].end(), start) - compressed[next].begin();
+		int next = graph[start][i].second;		
 		long long new_val = graph[start][i].first;
-		if (dist[next][now_idx] > new_val) {
-			dist[next][now_idx] = new_val;
-			pq.push({ new_val, {next, now_idx} });
+		if (dist[next] > new_val) {
+			dist[next] = new_val;
+			edge.erase({ start, next });
+			pq.push({ new_val, {next, start} });
 		}
 	}	
 	while (pq.size()) {
-		int now = pq.top().second.first;
-		int prev = compressed[now][pq.top().second.second];
-		int prev_idx = pq.top().second.second;
+		int now = pq.top().second.first;	
+		int prev = pq.top().second.second;
 		long long val = pq.top().first;
 		pq.pop();
-		if (dist[now][prev_idx] < val) continue;
 		for (int i = 0; i < graph[now].size(); i++) {
 			int next = graph[now][i].second;
-			if (forbidden.count({ {prev, now},next }) != 0) continue;			
-			int now_idx = lower_bound(compressed[next].begin(), compressed[next].end(), now) - compressed[next].begin();
+			if (edge.count({ now, next }) == 0) continue;
+			if (forbidden.count({ {prev, now},next }) != 0) continue;
+			edge.erase({ now,next });
 			long long new_val = val + graph[now][i].first;
-			if (dist[next][now_idx] > new_val) {
-				dist[next][now_idx] = new_val;
-				pq.push({ new_val, {next, now_idx} });
-			}
+			dist[next] = min(dist[next], new_val);
+			pq.push({ new_val, {next, now} });
 		}
 	}
 }
