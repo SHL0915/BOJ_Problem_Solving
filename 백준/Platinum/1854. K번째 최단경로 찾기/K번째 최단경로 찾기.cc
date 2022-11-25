fbdc9fd9e3ae246375d88d1bc9e99a66f7cf7d82
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-25 15:26:56 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 112 ms, Memory: 10080 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.cc
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.cc
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const ll INF = 1000000000000;
+typedef pair<long long, int> pli;
+const long long INF = 0x3f3f3f3f3f3f3f3fLL;
 
 struct cmp {
-	bool operator() (pair<ll, int> A, pair<ll, int> B) {
+	bool operator() (pli A, pli B) {
 		return A.first > B.first;
 	}
 };
 
 int N, M, K;
-vector <pair<int, int>> graph[1001];
-priority_queue <ll> dist[1001];
+vector <pli> graph[1001];
+priority_queue <long long> dist[1001];
 
 void Dijkstra(int start);
 
@@ -20,9 +20,10 @@ int main(void) {
 	cin.tie(0);
 	cin >> N >> M >> K;
 	for (int i = 0; i < M; i++) {
-		int A, B, C;
+		int A, B;
+		long long C;
 		cin >> A >> B >> C;
-		graph[A].push_back({ B,C });
+		graph[A].push_back({ C,B });
 	}
 	Dijkstra(1);
 	for (int i = 1; i <= N; i++) {
@@ -33,27 +34,22 @@ int main(void) {
 }
 
 void Dijkstra(int start) {
-	priority_queue < pair<ll, int>, vector<pair<ll, int>>, cmp> pq;
-	pq.push({ 0, start });
+	priority_queue <pli, vector<pli>, cmp> pq;
 	dist[start].push(0);
+	pq.push({ 0,start });
 	while (pq.size()) {
 		int now = pq.top().second;
-		ll val = pq.top().first;
+		long long val = pq.top().first;
 		pq.pop();
 		for (int i = 0; i < graph[now].size(); i++) {
-			int next = graph[now][i].first;
-			ll next_val = graph[now][i].second + val;
+			int next = graph[now][i].second;
+			long long new_val = val + graph[now][i].first;
 			if (dist[next].size() == K) {
-				if (dist[next].top() > next_val) {
-					dist[next].pop();
-					dist[next].push(next_val);
-					pq.push({ next_val, next });
-				}
+				if (dist[next].top() > new_val) dist[next].pop();
+				else continue;
 			}
-			else {
-				dist[next].push(next_val);
-				pq.push({ next_val, next });
-			}
+			dist[next].push(new_val);
+			pq.push({ new_val, next });
 		}
 	}
 }
