63c8049b353a2dceb6638a200da598710fc601fd
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-28 21:11:05 +0900

    [Platinum V] Title: 더 어려운 스케줄링, Time: 24 ms, Memory: 4404 KB -BaekjoonHub

--- 백준/Platinum/26094. 더 어려운 스케줄링/더 어려운 스케줄링.cc
+++ 백준/Platinum/26094. 더 어려운 스케줄링/더 어려운 스케줄링.cc
@@ -1,15 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct cmp {
-	bool operator() (int A, int B) {
-		return A > B;
-	}
-};
-
-int N, Q, state;
-priority_queue <int> upper;
-priority_queue <int, vector<int>, cmp> lower;
+int N, Q, state, output;
 deque <int> arr[2];
 set <int> s;
 
@@ -23,50 +15,32 @@ int main(void) {
 		if (A == 0) {
 			cin >> B;
 			arr[state].push_back(B);
-			s.insert(B);
 		}
 		else if (A == 1) {
 			for (int i = 0; i < 2; i++) {
 				while (arr[i].size()) {
-					upper.push(arr[i].back());
-					lower.push(arr[i].back());
+					s.insert(arr[i].back());
 					arr[i].pop_back();
 				}
 			}
 			state = 0;
 		}
 		else if (A == 2) state ^= 1;
-		else {
-			while (lower.size()) {
-				if (s.count(lower.top()) == 0) lower.pop();
-				else break;
-			}
-			while (upper.size()) {
-				if (s.count(upper.top()) == 0) upper.pop();
-				else break;
-			}
+		else {			
 			if (arr[state].size() != 0) {
-				cout << arr[state].back() << '\n';
-				s.erase(arr[state].back());
+				output = arr[state].back();
 				arr[state].pop_back();				
 			}
-			else {
-				if (state == 0 && lower.size() != 0) {
-					cout << lower.top() << '\n'; 
-					s.erase(lower.top());
-					lower.pop();
-				}
-				else if(state == 1 && upper.size() != 0) {
-					cout << upper.top() << '\n';
-					s.erase(upper.top());
-					upper.pop();
-				}
-				else {
-					cout << arr[state ^ 1][0] << '\n';
-					s.erase(arr[state ^ 1][0]);
-					arr[state ^ 1].pop_front();
-				}
+			else if(s.size()) {
+				if (state == 0) output = *s.begin();
+				else output = *s.rbegin();
+				s.erase(output);
 			}
+			else {
+				output = arr[state ^ 1].front();
+				arr[state ^ 1].pop_front();
+			}
+			cout << output << '\n';
 		}
 	}
 	return 0;
