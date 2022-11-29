1b4ba04af7ae42d4465efa030d060fa8ea538348
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-29 22:41:35 +0900

    [Gold II] Title: 코딩은 예쁘게, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.cc
+++ 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.cc
@@ -1,63 +1,17 @@
-//#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-long long N, ans;
-long long arr[100000];
+const int mxn = 100010;
+int N, a[mxn], x;
 
-void recur(int s, int e);
+int main() {
+    cin >> N;
+    for (int i = 1; i <= N; i++)cin >> a[i];
+    for (int i = 1; i <= N; i++)cin >> x, a[i] -= x;
+    int res = 0;
+    for (int i = 0; i <= N; i++)
+        res += abs(a[i] - a[i + 1]);
+    cout << res / 2;
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	//freopen("input.txt", "r", stdin);
-	cin >> N;
-	for (int i = 0; i < N; i++) cin >> arr[i];
-	for (int i = 0; i < N; i++) {
-		int t;
-		cin >> t;
-		arr[i] -= t;
-	}
-	recur(0, N - 1);
-	cout << ans;
-	return 0;
-}
-
-void recur(int s, int e) {
-	while (s <= e) {
-		if (arr[s] == 0) s++;
-		else if (arr[e] == 0) e--;
-		else if (arr[s] > 0 && arr[e] > 0) {
-			int temp = min(arr[s], arr[e]);
-			ans += temp;
-			for (int i = s; i <= e; i++) arr[i] -= temp;
-		}
-		else if (arr[s] < 0 && arr[e] < 0) {
-			int temp = max(arr[s], arr[e]);
-			ans -= temp;
-			for (int i = s; i <= e; i++) arr[i] -= temp;
-		}
-		else {
-			if (arr[s] > 0) {
-				for (int i = s; i <= e; i++) {
-					if (arr[i] < 0) {
-						recur(s, i - 1);
-						s = i;
-						break;
-					}
-				}
-			}
-			else {
-				for (int i = s; i <= e; i++) {
-					if (arr[i] > 0) {
-						recur(s, i - 1);
-						s = i;
-						break;
-					}
-				}
-			}
-		}
-	}
-	return;
+    return 0;
 }
