1b6894d5bb5324379db5f8dabf480027636f1815
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-30 22:15:49 +0900

    [Gold II] Title: 컵라면, Time: 60 ms, Memory: 4368 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.cc
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -1,31 +1,48 @@
-#include <iostream>
-#include <algorithm>
-#include <queue>;
+#include <bits/stdc++.h>
 using namespace std;
 
 int N, ans;
-vector <int> arr[200001];
+pair<int, int> arr[200000];
+int parent[200001];
+
+int Find(int node);
+void Union(int A, int B);
+
+bool cmp(pair<int, int> A, pair<int, int> B) {
+	return A.first > B.first;
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> N;
-	priority_queue <int, vector <int>> pq;
+	for (int i = 0; i <= N; i++) parent[i] = i;
 	for (int i = 0; i < N; i++) {
-		int deadline, ramyun;
-		cin >> deadline >> ramyun;
-		arr[deadline].push_back(ramyun);
+		int A, B;
+		cin >> A >> B;
+		arr[i] = { B, A };
 	}
-	for (int i = N; i >= 1; i--) {
-		while (arr[i].size()) {
-			pq.push(arr[i].back());
-			arr[i].pop_back();
-		}
-		if (pq.size()) {
-			ans += pq.top();
-			pq.pop();
-		}
+	sort(arr, arr + N, cmp);
+	for (int i = 0; i < N; i++) {
+		int capacity = Find(arr[i].second);
+		if (capacity == 0) continue;
+		ans += arr[i].first;
+		Union(capacity, capacity - 1);
 	}
 	cout << ans;
 	return 0;
+}
+
+int Find(int node) {
+	if (parent[node] == node) return parent[node];
+	else return parent[node] = Find(parent[node]);
+}
+
+void Union(int A, int B) {
+	A = Find(A);
+	B = Find(B);
+	if (A == B) return;
+	if (A > B) parent[A] = B;
+	else parent[B] = A;
+	return;
 }
