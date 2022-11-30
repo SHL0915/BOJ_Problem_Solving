0aed67ebe07d6aa73791c1ced5970562543fd792
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-30 21:53:08 +0900

    [Gold III] Title: 순회강연, Time: 4 ms, Memory: 2140 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -1,33 +1,47 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
-// priority_queue를 사용한 풀이
-
 int N, ans;
-vector <int> lecture[10001];
+pair<int, int> arr[10000];
+int parent[10001];
+
+bool cmp(pair<int, int> A, pair<int, int > B) {
+	return A.second > B.second;
+}
+
+int Find(int node);
+void Union(int A, int B);
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> N;
-	priority_queue <int, vector <int>> pq;
 	for (int i = 0; i < N; i++) {
-		int p, d;
-		cin >> p >> d;
-		lecture[d].push_back(p);
+		int D, P;
+		cin >> P >> D;
+		arr[i] = { D,P };
 	}
-	for (int i = 10000; i >= 1; i--) {
-		while (lecture[i].size()) {
-			pq.push(lecture[i].back());
-			lecture[i].pop_back();
-		}
-		if (pq.size()) {
-			ans += pq.top();
-			pq.pop();
-		}
+	for (int i = 0; i <= 10000; i++) parent[i] = i;
+	sort(arr, arr + N, cmp);
+	for (int i = 0; i < N; i++) {
+		if (Find(arr[i].first) == 0) continue;
+		ans += arr[i].second;
+		Union(Find(arr[i].first), Find(arr[i].first) - 1);
 	}
 	cout << ans;
 	return 0;
+}
+
+int Find(int node) {
+	if (parent[node] == node) return parent[node];
+	else return parent[node] = Find(parent[node]);
+}
+
+void Union(int A, int B) {
+	A = Find(A);
+	B = Find(B);
+	if (A == B) return;
+	if (A > B) parent[A] = B;
+	else parent[B] = A;
+	return;
 }
