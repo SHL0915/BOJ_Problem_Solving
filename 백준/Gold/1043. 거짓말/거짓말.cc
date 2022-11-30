c10720b0205e0f63667e6bbcb41e2e1ba8d029ac
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-30 21:21:02 +0900

    [Gold IV] Title: 거짓말, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.cc
+++ 백준/Gold/1043. 거짓말/거짓말.cc
@@ -1,60 +1,48 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int N, M, K, flag, cnt;
-vector <int> graph[51];
-vector <int> truth;
-int party[50][50];
-int mark[51];
+int N, M, K, cnt;
+int parent[51];
+vector <int> arr[51];
 
-void DFS1(int vertex);
-void DFS2(int vertex);
+int Find(int node);
+void Union(int A, int B);
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N >> M >> K;
+	cin >> N >> M;
+	cin >> K;
+	for (int i = 0; i <= N; i++) parent[i] = i;
 	for (int i = 0; i < K; i++) {
-		int input;
-		cin >> input;
-		truth.push_back(input);
+		int A;
+		cin >> A;
+		Union(0, A);		
 	}
 	for (int i = 0; i < M; i++) {
-		cin >> K;
-		for (int j = 0; j < K; j++) {
-			cin >> party[i][j];
-			if (j == 0) continue;
-			else {
-				graph[party[i][j - 1]].push_back(party[i][j]);
-				graph[party[i][j]].push_back(party[i][j - 1]);
-			}
-		}		
+		int A;
+		cin >> A;
+		arr[i].resize(A);
+		for (int j = 0; j < A; j++) {
+			cin >> arr[i][j];
+			Union(arr[i][0], arr[i][j]);
+		}
 	}
-	for (int i = 0; i < truth.size(); i++) DFS1(truth[i]);
-	for (int i = 0; i < M; i++) {
-		flag = 0;
-		cnt++;
-		DFS2(party[i][0]);
-	}
+	for (int i = 0; i < M; i++) if (Find(arr[i][0]) != 0) cnt++;
 	cout << cnt;
 	return 0;
 }
 
-void DFS1(int vertex) {
-	if (mark[vertex] != 0) return;
-	mark[vertex] = 2;
-	for (int i = 0; i < graph[vertex].size(); i++) DFS1(graph[vertex][i]);
-	return;
+int Find(int node) {
+	if (parent[node] == node) return parent[node];
+	else return parent[node] = Find(parent[node]);
 }
 
-void DFS2(int vertex) {
-	if (mark[vertex] == 2 && flag == 0) {
-		cnt--;
-		flag = 1;
-	}
-	if (mark[vertex] != 0) return;
-	mark[vertex] = 1;
-	for (int i = 0; i < graph[vertex].size(); i++) DFS2(graph[vertex][i]);
+void Union(int A, int B) {
+	A = Find(A);
+	B = Find(B);
+	if (A == B) return;
+	if (A > B) parent[A] = B;
+	else parent[B] = A;
 	return;
 }
