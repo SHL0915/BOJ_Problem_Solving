359282c66e894d084c61f7825031127dd87a7f6d
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-11-30 22:25:31 +0900

    [Gold II] Title: 공항, Time: 12 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.cc
+++ 백준/Gold/10775. 공항/공항.cc
@@ -1,27 +1,42 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int G, P, ans;
-vector <int> validGate;
+int G, P;
+int arr[100000];
+int parent[100001];
+
+int Find(int node);
+void Union(int A, int B);
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> G >> P;
-	for (int i = 1; i <= G; i++) validGate.push_back(i);
+	for (int i = 0; i <= G; i++) parent[i] = i;
 	for (int i = 0; i < P; i++) {
-		int g;
-		cin >> g;
-		if (validGate.size() == 0) break;
-		int idx = upper_bound(validGate.begin(), validGate.end(), g) - validGate.begin();
-		if (idx == 0) break;
-		else {
-			validGate.erase(validGate.begin() + idx - 1);
-			ans++;
+		int A;
+		cin >> A;
+		int capacity = Find(A);
+		if (capacity == 0) {
+			cout << i;
+			return 0;
 		}
+		Union(capacity, capacity - 1);
 	}
-	cout << ans;
+	cout << P;
 	return 0;
+}
+
+int Find(int node) {
+	if (parent[node] == node) return parent[node];
+	else return parent[node] = Find(parent[node]);
+}
+
+void Union(int A, int B) {
+	A = Find(A);
+	B = Find(B);
+	if (A == B) return;
+	if (A > B) parent[A] = B;
+	else parent[B] = A;
+	return;
 }
