d3f226bce27a9b9d87776c3528dfbcb4dc3010cf
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-12-06 23:07:05 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 324 ms, Memory: 174056 KB -BaekjoonHub

---
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 0x3f3f3f3f;
+
+int N, ans = 1234567890;
+int arr[21][21];
+int table[(1 << 21)][21];
+
+int DP(int state, int now);
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) cin >> arr[i][j];
+	}
+	memset(table, INF, sizeof(table));
+	cout << DP(0, 0);
+	return 0;
+}
+
+int DP(int state, int now) {
+	if (state == (1 << N) - 1) return 0;
+	if (table[state][now] != INF) return table[state][now];
+	for (int i = 0; i < N; i++) {
+		if (state & (1 << i)) continue;
+		table[state][now] = min(table[state][now], DP(state ^ (1 << i), now + 1) + arr[now][i]);
+	}
+	return table[state][now];
+}
