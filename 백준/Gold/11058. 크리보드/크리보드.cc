13321229880eb4c5216dde6865a5c61b7e8770fc
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-12-07 13:58:09 +0900

    [Gold V] Title: 크리보드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11058. 크리보드/크리보드.cc
+++ 백준/Gold/11058. 크리보드/크리보드.cc
@@ -1,35 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<int, pair<long long, long long>> pill;
 
 int N;
-long long ans;
-map <pair<int,long long>, long long> mark;
-queue <pill> q;
-
-void BFS();
+long long table[101];
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> N;
-	q.push({ 1, {1,0} });
-	while (q.size()) BFS();
-	cout << ans;
+	table[1] = 1;
+	for (int i = 2; i <= N; i++) {
+		table[i] = max(table[i], table[i - 1] + 1);
+		for (int j = i - 3; j >= 0; j--) table[i] = max(table[i], table[j] * (i - j - 1));
+	}
+	cout << table[N];
 	return 0;
-}
-
-void BFS() {
-	pill f = q.front();
-	q.pop();
-	int now = f.first;
-	long long cnt = f.second.first;
-	long long copied = f.second.second;
-	if (now > N) return;
-	if (mark[{now, cnt}] > copied) return;
-	mark[{now, cnt}] = copied;
-	ans = max(ans, cnt);
-	if (copied == 0) q.push({ now + 1, {cnt + 1, copied} });
-	else q.push({ now + 1, {cnt + copied, copied} });
-	q.push({ now + 3, {2 * cnt, cnt} });
 }
