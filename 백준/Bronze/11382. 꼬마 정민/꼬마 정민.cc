fd810dfb9a814e331c9da66acf1689436b5bf011
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-12-16 14:05:58 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	long long A, B, C;
+	cin >> A >> B >> C;
+	cout << A + B + C;
+	return 0;
+}
