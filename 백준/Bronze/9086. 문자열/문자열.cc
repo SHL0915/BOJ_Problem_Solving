0f8dfd78319b6a3b9b3b7fa0528e2ae9375d9119
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-12-16 14:10:52 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		string s;
+		cin >> s;
+		cout << s.front() << s.back() << '\n';
+	}
+	return 0;
+}
