e8fadff86d606186208d8d6d421b4ca0980cc803
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-12-18 15:42:17 +0900

    [Gold III] Title: 소수의 배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17436. 소수의 배수/소수의 배수.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long N, M, ans;
+long long arr[10];
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) cin >> arr[i];
+	for (int i = 1; i < (1 << N); i++) {
+		int cnt = popcount((unsigned int)i);
+		long long now = 1, bit = 1, idx = 0;
+		while (bit <= i) {
+			if (bit & i) now *= arr[idx];
+			bit <<= 1;
+			idx++;
+		}
+		if (cnt % 2) ans += (M / now);
+		else ans -= (M / now);
+	}
+	cout << ans;
+	return 0;
+}
