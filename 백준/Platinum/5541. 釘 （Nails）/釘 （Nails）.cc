700afa21e678d4673a26e10e64cdac31a936fccf
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-12-22 20:40:00 +0900

    [Platinum III] Title: 釘 (Nails), Time: 260 ms, Memory: 99716 KB -BaekjoonHub

--- 백준/Platinum/5541. 釘 （Nails）/釘 （Nails）.cc
+++ 백준/Platinum/5541. 釘 （Nails）/釘 （Nails）.cc
@@ -12,7 +12,7 @@ int main(void) {
 	for (int i = 0; i < M; i++) {
 		int A, B, C;
 		cin >> A >> B >> C;
-		table[A][B] = max(table[A][B], C + 1);
+		table[A][B] = C + 1;
 	}
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
