450d839f2b8e3b96e30ff94e8489b1648fcee267
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2022-12-28 20:31:10 +0900

    [Platinum V] Title: 팰린드롬 보행, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Platinum/12950. 팰린드롬 보행/팰린드롬 보행.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+
+int N, M;
+vector <int> graph[20][26];
+ll table[20][20];
+int adj[20][20];
+
+ll DP(int start, int end);
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		char c;
+		cin >> a >> b >> c;
+		graph[a][c - 'a'].push_back(b);
+		graph[b][c - 'a'].push_back(a);
+		adj[a][b] = 1;
+		adj[b][a] = 1;
+	}
+	memset(table, -1, sizeof(table));
+	if (DP(0, 1) == INF) cout << -1;
+	else cout << DP(0, 1);
+	return 0;
+}
+
+ll DP(int start, int end) {
+	if (start == end) return 0;
+	if (adj[start][end] == 1) return 1;
+	ll& ret = table[start][end];
+	if (ret != -1) return ret;
+	ret = INF;
+	for (int i = 0; i < 26; i++) {
+		for (int j = 0; j < graph[start][i].size(); j++) {
+			for (int k = 0; k < graph[end][i].size(); k++) {
+				int s_next = graph[start][i][j];
+				int e_next = graph[end][i][k];
+				ret = min(ret, 2 + DP(s_next, e_next));
+			}			
+		}
+	}
+	return ret;
+}
