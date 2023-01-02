b2a09e04ddaf91c847cb77c84960b437405ed5c4
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-02 09:04:27 +0900

    [Silver I] Title: 1, 2, 3 더하기 7, Time: 20 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Silver/15992. 1， 2， 3 더하기 7/1， 2， 3 더하기 7.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+const long long mod = 1000000009;
+
+int t, N, M;
+long long table[1001][1001];
+
+long long DP(int now, int k);
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> t;
+	memset(table, -1, sizeof(table));
+	while (t--) {
+		cin >> N >> M;
+		cout << DP(N, M) << '\n';
+	}
+	return 0;
+}
+
+long long DP(int now, int k) {
+	if (now < 0) return 0;
+	if (now == 0) return (k == 0);
+	long long& ret = table[now][k];
+	if (ret != -1) return ret;
+	ret = 0;
+	ret += DP(now - 1, k - 1);
+	ret += DP(now - 2, k - 1);
+	ret += DP(now - 3, k - 1);
+	ret %= mod;
+	return ret;
+}
