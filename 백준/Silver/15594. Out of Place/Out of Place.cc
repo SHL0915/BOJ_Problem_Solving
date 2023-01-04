de97e5a37710e289abf65db9cf9cdd7156607f6f
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-04 17:26:54 +0900

    [Silver IV] Title: Out of Place, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/15594. Out of Place/Out of Place.cc
+++ 백준/Silver/15594. Out of Place/Out of Place.cc
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include<stdio.h>
+#include<algorithm>
 using namespace std;
 int a,b,c[100],d[100],i,j;
 int main(void){scanf("%d",&a);for(i=0;i<a;i++){scanf("%d",&c[i]);d[i]=c[i];}sort(d,d+a);for(i=0;i<a;i++){if(c[i]==d[i])continue;for(j=i+1;j<a;j++){if(c[j]==d[i]&&c[j]!=d[j]){swap(c[i],c[j]);b++;}}}printf("%d",b);return 0;}
