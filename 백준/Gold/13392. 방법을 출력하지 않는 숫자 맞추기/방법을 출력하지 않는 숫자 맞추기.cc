0670b3666affe716ba8801c706daf695cc18198d
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-05 09:37:41 +0900

    [Gold I] Title: 방법을 출력하지 않는 숫자 맞추기, Time: 28 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/방법을 출력하지 않는 숫자 맞추기.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std; 
+const long long INF = 0x3f3f3f3f3f3f3f3fLL;
+const int mod = 10;
+
+int N;
+string A, B;
+long long table[10001][11][11];
+int num[10001];
+
+long long DP(int pos, int left, int right);
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N;
+	cin >> A >> B;
+	for (int i = 0; i < A.size(); i++) num[i] = A[i] - '0';
+	memset(table, -1, sizeof(table));
+	cout << DP(0, 0, 0);
+	return 0;
+}
+
+long long DP(int pos, int left, int right) {
+	if (pos == N) return 0;
+	long long& ret = table[pos][left][right];
+	if (ret != -1) return ret;
+	ret = INF;
+	int now_num = (num[pos] - right + left + mod) % mod;
+	if (now_num == B[pos] - '0') ret = min(ret, DP(pos + 1, left, 0));
+	ret = min(ret, 1 + DP(pos, (left + 1) % mod, right));
+	ret = min(ret, 1 + DP(pos, left, (right + 1) % mod));
+	return ret;	
+}
