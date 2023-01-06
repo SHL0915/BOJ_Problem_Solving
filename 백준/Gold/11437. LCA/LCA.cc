d959fa6d8c7fc5b9543c838d6e5108292099afec
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-07 01:02:59 +0900

    [Gold III] Title: LCA, Time: 28 ms, Memory: 11876 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.cc
+++ 백준/Gold/11437. LCA/LCA.cc
@@ -2,11 +2,11 @@
 using namespace std;
 
 int N, M;
+vector <int> tree[50001];
 int parent[50001][20];
 int level[50001];
-vector <int> tree[50001];
 
-void findLev(int node, int par, int lv);
+void pre(int node, int par, int lv);
 int LCA(int A, int B);
 
 int main(void) {
@@ -14,44 +14,39 @@ int main(void) {
 	cin.tie(0);
 	cin >> N;
 	for (int i = 0; i < N - 1; i++) {
-		int A, B;
-		cin >> A >> B;
-		tree[A].push_back(B);
-		tree[B].push_back(A);
+		int a, b;
+		cin >> a >> b;
+		tree[a].push_back(b);
+		tree[b].push_back(a);
 	}
-	findLev(1, 0, 1);
+	pre(1, 1, 1);
 	cin >> M;
 	for (int i = 0; i < M; i++) {
-		int A, B;
-		cin >> A >> B;
-		cout << LCA(A, B) << '\n';
+		int a, b;
+		cin >> a >> b;
+		cout << LCA(a, b) << '\n';
 	}
 	return 0;
 }
 
-void findLev(int node, int par, int lv) {
+void pre(int node, int par, int lv) {
 	level[node] = lv;
 	parent[node][0] = par;
 	for (int i = 1; i < 20; i++) parent[node][i] = parent[parent[node][i - 1]][i - 1];
 	for (int i = 0; i < tree[node].size(); i++) {
-		if (tree[node][i] == par) continue;
-		findLev(tree[node][i], node, lv + 1);
+		int next = tree[node][i];
+		if (next == par) continue;
+		pre(next, node, lv + 1);
 	}
 	return;
 }
 
 int LCA(int A, int B) {
 	if (A == 1 || B == 1) return 1;
-	if (level[A] < level[B]) {
-		int temp = A;
-		A = B;
-		B = temp;
-	}
-	if (level[A] != level[B]) {
+	if (level[A] < level[B]) swap(A, B);
+	if (level[A] != level[B] ) {
 		for (int i = 19; i >= 0; i--) {
-			if (level[parent[A][i]] >= level[B]) {
-				A = parent[A][i];
-			}
+			if (level[parent[A][i]] >= level[B]) A = parent[A][i];			
 		}
 	}
 	int ret = A;
