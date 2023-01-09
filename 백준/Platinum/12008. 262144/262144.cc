a5e6a4b61bcd0e3b03e02b8fa9ca3bb16b4a3ea7
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-09 14:40:35 +0900

    [Platinum II] Title: 262144, Time: 452 ms, Memory: 106468 KB -BaekjoonHub

---
+++ 백준/Platinum/12008. 262144/262144.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, ans;
+int arr[262145];
+int table[262145][101];
+
+int DP(int pos, int k);
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> arr[i];
+    memset(table, -1, sizeof(table));
+    for (int i = 100; i >= 0; i--) {
+        for (int j = 0; j < N; j++) {
+            if (DP(j, i) != 0) ans = max(ans, i);
+        }
+    }
+    cout << ans;
+    return 0;
+}
+
+int DP(int pos, int k) {
+    if (k == arr[pos]) return pos + 1;
+    if (pos == N) return 0;
+    int& ret = table[pos][k];
+    if (ret != -1) return ret;
+    ret = 0;
+    int temp = DP(pos, k - 1);
+    if (temp > pos) ret = DP(temp, k - 1);    
+    return ret;
+}
