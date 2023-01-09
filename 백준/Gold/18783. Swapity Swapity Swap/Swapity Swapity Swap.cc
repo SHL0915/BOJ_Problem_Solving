07a40988c85859275622e47b830bbf27fccaf7ca
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-09 15:12:07 +0900

    [Gold II] Title: Swapity Swapity Swap, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Gold/18783. Swapity Swapity Swap/Swapity Swapity Swap.cc
+++ 백준/Gold/18783. Swapity Swapity Swap/Swapity Swapity Swap.cc
@@ -2,28 +2,27 @@
 using namespace std;
 
 int N, M, K;
-int table[100001][31];
-int ans[100001];
+int table[31][100001];
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> N >> M >> K;
-	for (int i = 1; i <= N; i++) table[i][0] = i;	
+	for (int i = 1; i <= N; i++) table[0][i] = i;	
 	for (int i = 0; i < M; i++) {
 		int L, R;
 		cin >> L >> R;
-		for (int j = L; j <= (R + L) / 2; j++) swap(table[j][0], table[R + L - j][0]);
+		for (int j = L; j <= (R + L) / 2; j++) swap(table[0][j], table[0][R + L - j]);
 	}
 	for (int i = 1; i <= 30; i++) {
-		for (int j = 1; j <= N; j++) table[j][i] = table[table[j][i - 1]][i - 1];
+		for (int j = 1; j <= N; j++) table[i][j] = table[i - 1][table[i - 1][j]];
 	}
 	for (int i = 1; i <= N; i++) {
 		int now = i;
 		long long bit = 1;
 		int pos = 0;
 		while (bit <= K) {
-			if (bit & K) now = table[now][pos];
+			if (bit & K) now = table[pos][now];
 			pos++;
 			bit <<= 1;
 		}
