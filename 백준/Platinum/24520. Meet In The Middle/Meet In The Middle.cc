8961194101aad9ca1f35bec38f552adaa90fa47d
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-09 10:52:54 +0900

    [Platinum III] Title: Meet In The Middle, Time: 268 ms, Memory: 38656 KB -BaekjoonHub

--- 백준/Platinum/24520. Meet In The Middle/Meet In The Middle.cc
+++ 백준/Platinum/24520. Meet In The Middle/Meet In The Middle.cc
@@ -2,16 +2,15 @@
 using namespace std;
 
 int N, K;
-vector <pair<int, long long>> tree[100001];
-map <pair<int, int>, long long> d;
+vector <pair<int, int>> tree[100001];
 int level[100001];
 int parent[100001][21];
-long long dist[100001][21];
+long long table[100001][21];
 
-void pre(int node, int par, int lv);
-int LCA(int A, int B);
-int find_middle(int A, int B, int lca);
-long long find_distance(int A, int k);
+void pre(int node, int par, int lv, int val);
+int query(int a, int b);
+int LCA(int a, int b);
+long long dist(int a, int b);
 
 int main(void) {
 	ios::sync_with_stdio(false);
@@ -22,94 +21,92 @@ int main(void) {
 		cin >> u >> v >> w;
 		tree[u].push_back({ v,w });
 		tree[v].push_back({ u,w });
-		d[{u, v}] = w;
-		d[{v, u}] = w;
 	}
-	d[{1, 0}] = 0;
-	pre(1, 0, 1);
+	pre(1, 1, 1, 0);
 	for (int i = 0; i < K; i++) {
 		int u, v;
 		cin >> u >> v;
-		cout << find_middle(u, v, LCA(u, v)) << '\n';
+		cout << query(u, v) << '\n';
 	}
 	return 0;
 }
 
-void pre(int node, int par, int lv) {
+void pre(int node, int par, int lv, int val) {
 	level[node] = lv;
 	parent[node][0] = par;
-	dist[node][0] = d[{node, par}];
+	table[node][0] = val;
 	for (int i = 1; i <= 20; i++) {
 		parent[node][i] = parent[parent[node][i - 1]][i - 1];
-		dist[node][i] = dist[node][i - 1] + dist[parent[node][i - 1]][i - 1];
+		table[node][i] = table[node][i - 1] + table[parent[node][i - 1]][i - 1];
 	}
 	for (int i = 0; i < tree[node].size(); i++) {
 		if (tree[node][i].first == par) continue;
-		pre(tree[node][i].first, node, lv + 1);
+		pre(tree[node][i].first, node, lv + 1, tree[node][i].second);
+	}
+	return;
+}
+
+int query(int a, int b) {
+	int lca = LCA(a, b);
+	long long distA = dist(lca, a);
+	long long distB = dist(lca, b);
+	long long find = distA + distB;
+	if (find % 2) return -1;
+	find /= 2;
+	if (find >= distA) {
+		find -= distA;
+		find = distB - find;
+		for (int i = 20; i >= 0; i--) {
+			if (table[b][i] <= find) {
+				find -= table[b][i];
+				b = parent[b][i];
+			}
+		}
+		if (find) return -1;
+		else return b;
+	}
+	else {
+		find -= distB;
+		find = distA - find;
+		for (int i = 20; i >= 0; i--) {
+			if (table[a][i] <= find) {
+				find -= table[a][i];
+				a = parent[a][i];
+			}
+		}
+		if (find) return -1;
+		else return a;
 	}
 }
 
-int LCA(int A, int B) {
-	if (A == 1 || B == 1) return 1;
-	if (level[A] < level[B]) {
-		int temp = A;
-		A = B;
-		B = temp;
-	}
-	if (level[A] != level[B]) {
+int LCA(int a, int b) {
+	if (a == 1 || b == 1) return 1;
+	if (level[a] < level[b]) swap(a, b);
+	if (level[a] != level[b]) {
 		for (int i = 20; i >= 0; i--) {
-			if (level[parent[A][i]] >= level[B]) A = parent[A][i];
+			if (level[parent[a][i]] >= level[b]) a = parent[a][i];
 		}
 	}
-	int ret = A;
-	if (A != B) {
+	int ret = a;
+	if (a != b) {
 		for (int i = 20; i >= 0; i--) {
-			if (parent[A][i] != parent[B][i]) {
-				A = parent[A][i];
-				B = parent[B][i];
+			if (parent[a][i] != parent[b][i]) {
+				a = parent[a][i];
+				b = parent[b][i];
 			}
-			ret = parent[A][i];
+			ret = parent[a][i];
 		}
 	}
 	return ret;
 }
 
-int find_middle(int A, int B, int lca) {
-	long long dist_A = find_distance(A, level[A] - level[lca]);
-	long long dist_B = find_distance(B, level[B] - level[lca]);
-	if (dist_A == dist_B) return lca;
-	if (dist_A < dist_B) {
-		long long temp = A;
-		A = B;
-		B = temp;
-		temp = dist_A;
-		dist_A = dist_B;
-		dist_B = temp;
-	}	
-	if ((dist_A - dist_B) % 2 == 1) return -1;
-	long long len = dist_A - (dist_A - dist_B) / 2;
-	long long temp = 0;
-	for (int i = 20; i >= 0; i--) {
-		if (temp + dist[A][i] <= len) {
-			temp += dist[A][i];
-			A = parent[A][i];
-		}
-	}
-	if (temp == len) return A;
-	else return -1;
-}
-
-long long find_distance(int A, int k) {
+long long dist(int a, int b) {
 	long long ret = 0;
-	int bit = 1;
-	int cnt = 0;
-	while (bit <= k) {
-		if (bit & k) {
-			ret += dist[A][cnt];
-			A = parent[A][cnt];
+	for (int i = 20; i >= 0; i--) {
+		if (level[parent[b][i]] >= level[a]) {
+			ret += table[b][i];
+			b = parent[b][i];
 		}
-		cnt++;
-		bit <<= 1;
 	}
 	return ret;
 }
