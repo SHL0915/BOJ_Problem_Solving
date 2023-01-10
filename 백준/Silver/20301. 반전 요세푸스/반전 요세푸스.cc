7428161568160650a38694e2ee2868546df05f86
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-10 21:10:30 +0900

    [Silver III] Title: 반전 요세푸스, Time: 72 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.cc
+++ 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.cc
@@ -1,125 +1,35 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef struct ListNode{
-	struct ListNode* next, *prev;
-	int data;
-}ListNode;
-
-typedef struct {
-	ListNode* front, * rear;
-	int size;
-}Deque;
-
-Deque* createDeque(void) {
-	Deque* D = new Deque;
-	if (!D)
-		return nullptr;
-	D->front = nullptr;
-	D->rear = nullptr;
-	D->size = 0;
-	return D;
-}
-
-void PushFront(Deque * D, int data) {
-	ListNode* newNode = new ListNode;
-	if (!newNode)
-		return;
-	newNode->data = data;
-	newNode->prev = nullptr;
-	if (D->size == 0) {	
-		newNode->next = nullptr;
-		D->rear = newNode;
-	}
-	else {
-		D->front->prev = newNode;
-		newNode->next = D->front;
-	}
-	D->front = newNode;
-	D->size++;
-	return;
-}
-
-void PushRear(Deque* D, int data) {
-	ListNode* newNode = new ListNode;
-	if (!newNode)
-		return;
-	newNode->data = data;
-	newNode->next = nullptr;
-	if (D->size == 0) {
-		D->front = newNode;
-		newNode->prev = nullptr;
-	}
-	else {
-		D->rear->next = newNode;
-		newNode->prev = D->rear;
-	}
-	D->rear = newNode;
-	D->size++;
-	return;
-}
-
-int PopFront(Deque* D) {
-	if (D->size == 0)
-		return -1;
-	ListNode* temp = D->front;
-	int data = temp->data;
-	if (D->size == 1) {
-		D->front = nullptr;
-		D->rear = nullptr;
-	}
-	else {
-		D->front = D->front->next;
-		D->front->prev = nullptr;
-	}
-	D->size--;
-	delete temp;
-	return data;
-}
-
-int PopRear(Deque* D) {
-	if (D->size == 0)
-		return -1;
-	ListNode* temp = D->rear;
-	int data = temp->data;
-	if (D->size == 1) {
-		D->front = nullptr;
-		D->rear = nullptr;
-	}
-	else {
-		D->rear = D->rear->prev;
-		D->rear->next = nullptr;
-	}
-	D->size--;
-	delete temp;
-	return data;
-}
+int N, K, M, mode, cnt;
+deque <int> dq;
 
 int main(void) {
-	Deque* D = createDeque();
-	int N, K, M, reverse = 0, m = 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	cin >> N >> K >> M;
-	for (int i = 1; i <= N; i++)
-		PushRear(D, i);	
-	while (1) {
-		if (D->size == 0)
-			break;
-		if (reverse == 0) { // 순방향
-			for (int i = 0; i < K - 1; i++) 
-				PushRear(D, PopFront(D));
-			cout << PopFront(D) << endl;
+	for (int i = 1; i <= N; i++) dq.push_back(i);
+	while (dq.size()) {
+		for (int i = 0; i < K - 1; i++) {
+			if (mode) {
+				dq.push_front(dq.back());
+				dq.pop_back();
+			}
+			else {
+				dq.push_back(dq.front());
+				dq.pop_front();
+			}
 		}
-		else { // 역방향
-			for (int i = 0; i < K - 1; i++)
-				PushFront(D, PopRear(D));
-			cout << PopRear(D) << endl;
+		if (mode) {
+			cout << dq.back() << '\n';
+			dq.pop_back();
 		}
-		m ++;
-		if (m == M) {
-			reverse += 1;
-			reverse = reverse % 2;
-			m = 0;
+		else {
+			cout << dq.front() << '\n';
+			dq.pop_front();
 		}
-	}
+		cnt++;
+		if (cnt % M == 0) mode ^= 1;
+	}	
 	return 0;
 }
