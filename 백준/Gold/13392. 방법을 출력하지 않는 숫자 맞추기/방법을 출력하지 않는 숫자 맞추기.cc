715498df4ca8e43c7b76843e86075b063a1308f9
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-11 09:56:36 +0900

    [Gold I] Title: 방법을 출력하지 않는 숫자 맞추기, Time: 0 ms, Memory: 3064 KB -BaekjoonHub

--- 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/방법을 출력하지 않는 숫자 맞추기.cc
+++ 백준/Gold/13392. 방법을 출력하지 않는 숫자 맞추기/방법을 출력하지 않는 숫자 맞추기.cc
@@ -1,36 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-const long long INF = 0x3f3f3f3f3f3f3f3fLL;
+const int INF = 0x3f3f3f3f;
 const int mod = 10;
 
 int N;
 string A, B;
-long long table[10001][11][11];
-int num[10001];
+int table[10001][10];
 
-long long DP(int pos, int left, int right);
+int DP(int pos, int left);
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N;
-	cin >> A >> B;
-	for (int i = 0; i < A.size(); i++) num[i] = A[i] - '0';
+	cin >> N >> A >> B;
 	memset(table, -1, sizeof(table));
-	cout << DP(0, 0, 0);
+	cout << DP(0, 0);
 	return 0;
 }
 
-long long DP(int pos, int left, int right) {
+int DP(int pos, int left) {
 	if (pos == N) return 0;
-	long long& ret = table[pos][left][right];
+	int& ret = table[pos][left];
 	if (ret != -1) return ret;
 	ret = INF;
-	int now_num = (num[pos] - right + left + mod) % mod;
-	if (now_num == B[pos] - '0') ret = min(ret, DP(pos + 1, left, 0));
-	else {
-		ret = min(ret, 1 + DP(pos, (left + 1) % mod, right));
-		ret = min(ret, 1 + DP(pos, left, (right + 1) % mod));
-	}
+	int now = (A[pos] - '0' + left) % mod;
+	int rcost = ((now - (B[pos] - '0')) + mod) % mod;
+	int lcost = ((B[pos] - '0' - now) + mod) % mod;
+	ret = min(ret, rcost + DP(pos + 1, left));
+	ret = min(ret, lcost + DP(pos + 1, (left + lcost) % mod));
 	return ret;
 }
