24f6314544fb9635338560501cfea192b7de6333
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-11 11:35:38 +0900

    [Gold III] Title: 이진 트리, Time: 164 ms, Memory: 18404 KB -BaekjoonHub

--- 백준/Gold/13325. 이진 트리/이진 트리.cc
+++ 백준/Gold/13325. 이진 트리/이진 트리.cc
@@ -1,41 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int k;
-long long ans;
-long long pow_2[22];
-long long tree[10000000];
-long long sum[10000000];
-long long path[10000000];
-
-long long DFS(int node);
-void add(int node);
+int k, ans;
+int tree[1 << 22];
 
 int main(void) {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
-	pow_2[0] = 1;
-	for (int i = 1; i <= 21; i++) pow_2[i] = pow_2[i - 1] * 2;
+	cin.tie(0);	
 	cin >> k;
-	for (int i = 2; i <= pow_2[k + 1] - 1; i++) cin >> tree[i];
-	DFS(1);
-	add(1);
-	for (int i = 2; i <= pow_2[k + 1] - 1; i++) ans += tree[i];
+	for (int i = 2; i < (1 << (k + 1)); i++) {
+		cin >> tree[i];
+		ans += tree[i];
+	}
+	for (int i = k; i > 0; i--) {
+		int l = (1 << i), h = (1 << (i + 1));
+		for (int j = l; j < h; j += 2) {
+			ans += tree[j] < tree[j + 1] ? tree[j + 1] - tree[j] : tree[j] - tree[j + 1];
+			tree[j / 2] += max(tree[j], tree[j + 1]);
+		}
+	}	
 	cout << ans;
 	return 0;
-}
-
-long long DFS(int node) {
-	if (node >= pow_2[k + 1]) return 0;	
-	path[node] = max(DFS(node * 2) + tree[node * 2], DFS(node * 2 + 1) + tree[node * 2 + 1]);
-	return path[node];
-}
-
-void add(int node) {
-	if (node * 2 >= pow_2[k + 1]) return;
-	tree[node * 2] = path[node] - path[node * 2];
-	tree[node * 2 + 1] = path[node] - path[node * 2 + 1];
-	add(node * 2);
-	add(node * 2 + 1);
-	return;
 }
