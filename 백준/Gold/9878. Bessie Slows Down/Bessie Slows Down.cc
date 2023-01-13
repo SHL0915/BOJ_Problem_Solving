2c9886f59bad7f96c4fb0bec28f2a55da23001a7
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-13 11:45:35 +0900

    [Gold V] Title: Bessie Slows Down, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/9878. Bessie Slows Down/Bessie Slows Down.cc
+++ 백준/Gold/9878. Bessie Slows Down/Bessie Slows Down.cc
@@ -1,65 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-void fast_io() {
-  cin.tie(0)->sync_with_stdio(0);
-}
-
-int main() {
-  fast_io();
-
-  priority_queue<int, vector<int>, greater<int>> eventT, eventD;
-  int e;
-  cin >> e;
-  while (e--) {
-    string op;
-    int val;
-    cin >> op >> val;
-    if (op == "T") eventT.push(val);
-    if (op == "D") eventD.push(val);
-  }
-  eventD.push(1000);
-
-  int inv_speed = 1;
-  double ctime = 0, cdist = 0, speed = 1;
-  while (!eventT.empty() && !eventD.empty()) {
-    int ndist = eventD.top();
-    int ntime = eventT.top();
-
-    if (speed * (ntime - ctime) <= ndist - cdist) {
-      cdist += speed * (ntime - ctime);
-      ctime = ntime;
-      eventT.pop();
-    }
-    else {
-      ctime += (ndist - cdist) / speed;
-      cdist = ndist;
-      eventD.pop();
-    }
-
-    inv_speed++;
-    speed = 1.0 / inv_speed;
-  }
-
-  while (!eventT.empty()) {
-    int ntime = eventT.top();
-    cdist += speed * (ntime - ctime);
-    ctime = ntime;
-    eventT.pop();
-
-    inv_speed++;
-    speed = 1.0 / inv_speed;
-  }
-
-  while (!eventD.empty()) {
-    int ndist = eventD.top();
-    ctime += (ndist - cdist) / speed;
-    cdist = ndist;
-    eventD.pop();
-
-    inv_speed++;
-    speed = 1.0 / inv_speed;
-  }
-
-  cout << (int)round(ctime) << '\n';
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	priority_queue <int, vector<int>, greater<>> T, D;
+	int N, sp = 1;
+	double now_t = 0, now_d = 0, now_s = 1;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		char t; int a;
+		cin >> t >> a;
+		if (t == 'T') T.push(a);
+		else D.push(a);
+	}
+	D.push(1000);
+	while (D.size() && T.size()) {
+		int t = T.top();
+		int d = D.top();
+		if (now_s * (t - now_t) <= d - now_d) {
+			now_d += now_s * (t - now_t);
+			now_t = t;
+			T.pop();
+		}
+		else {
+			now_t += (d - now_d) / now_s;
+			now_d = d;
+			D.pop();
+		}
+		sp++;
+		now_s = (1.0 / sp);
+	}
+	while (T.size()) {
+		int t = T.top();
+		T.pop();
+		now_d += now_s * (t - now_t);
+		now_t = t;
+		sp++;
+		now_s = (1.0 / sp);
+	}
+	while (D.size()) {
+		int d = D.top();
+		D.pop();
+		now_t += (d - now_d) / now_s;
+		now_d = d;
+		sp++;
+		now_s = (1.0 / sp);
+	}
+	cout << (int)round(now_t) << '\n';
+	return 0;
 }
