2fc8f54333aa905c9e8ea94c4d13a5a361bd39dc
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-16 11:34:32 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
@@ -4,36 +4,22 @@ using ll = long long;
 using pii = pair<int, int>;
 
 ll N, ans;
-int table[1000001];
-vector <ll> primeNum;
 
 int main(void) {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
-	for (ll i = 2; i <= 1000000; i++) {
-		if (table[i] == 0) {
-			primeNum.push_back(i);
-			for (ll j = i * i; j <= 1000000; j += i) table[j] = 1;
-		}
-	}
+	cin.tie(0);	
 	cin >> N;
 	ans = N;
-	for (int i = 0; i < primeNum.size(); i++) {
-		ll now = primeNum[i];
-		if (now * now > N) break;
-		if (N % now == 0) {
-			ans /= now;
-			ans *= (now - 1);
+	for (ll i = 2; i * i <= N; i++) {
+		if (N % i == 0) {
+			ans = ans / i * (i - 1);
 			while (1) {
-				if (N % now) break;
-				N /= now;
+				if (N % i) break;
+				N /= i;
 			}
 		}
 	}
-	if (N != 1) {
-		ans /= N;
-		ans *= (N - 1);
-	}
+	if (N != 1) ans = ans / N * (N - 1);	
 	cout << ans;
 	return 0;
 }
