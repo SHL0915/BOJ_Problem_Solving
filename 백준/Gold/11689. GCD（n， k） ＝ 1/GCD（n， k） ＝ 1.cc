10dc02029b0a3f0af55e6dcd52a812b0fadb9387
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-16 11:31:09 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 8 ms, Memory: 7592 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.cc
@@ -1,38 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
-long long N, temp;
-int table[10000000];
-vector <int> primeNum;
-vector <long long> p;
+ll N, ans;
+int table[1000001];
+vector <ll> primeNum;
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	for (long long i = 2; i < 10000000; i++) {
+	for (ll i = 2; i <= 1000000; i++) {
 		if (table[i] == 0) {
 			primeNum.push_back(i);
-			for (long long j = i * i; j < 10000000; j += i) table[j] = 1;
+			for (ll j = i * i; j <= 1000000; j += i) table[j] = 1;
 		}
 	}
 	cin >> N;
-	temp = N;
+	ans = N;
 	for (int i = 0; i < primeNum.size(); i++) {
-		int now = primeNum[i];
-		if (now * now > temp) break;
-		if (temp % now == 0) {
-			p.push_back(now);
+		ll now = primeNum[i];
+		if (now * now > N) break;
+		if (N % now == 0) {
+			ans /= now;
+			ans *= (now - 1);
 			while (1) {
-				if (temp % now == 0) temp /= now;
-				else break;
+				if (N % now) break;
+				N /= now;
 			}
 		}
 	}
-	if (temp != 1) p.push_back(temp);
-	long long ans = N;
-	for (int i = 0; i < p.size(); i++) {
-		ans /= p[i];
-		ans *= (p[i] - 1);
+	if (N != 1) {
+		ans /= N;
+		ans *= (N - 1);
 	}
 	cout << ans;
 	return 0;
