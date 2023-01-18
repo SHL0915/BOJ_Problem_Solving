8a326550014bf82aaf0b04d1f6dad1bb86774f24
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-18 10:14:50 +0900

    [Platinum V] Title: 사탕상자, Time: 204 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Platinum/2243. 사탕상자/사탕상자.cc
+++ 백준/Platinum/2243. 사탕상자/사탕상자.cc
@@ -1,64 +1,67 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int SZ = 1000000;
 
-const long long MAX = 1000001LL;
+int A, B, C;
+vector <int> tree;
 
-int N;
-vector <long long> tree;
+void update(int node, int start, int end, int idx, int val);
+int query(int node, int start, int end, int left, int right);
+int BinarySearch(int left, int right, int val);
 
-void update(int node, int start, int end, int idx, long long diff);
-long long query(int node, int start, int end, int left, int right);
-int Binary_search(int left, int right, long long data);
+void solve() {
+	cin >> A;
+	if (A == 1) {
+		cin >> B;
+		int val = BinarySearch(1, 1000000, B);
+		update(1, 1, SZ, val, -1);
+		cout << val << '\n';
+	}
+	else {
+		cin >> B >> C;
+		update(1, 1, SZ, B, C);
+	}
+	return;
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	//freopen("input.txt", "r", stdin);
-	cin >> N;
-	tree.resize(4 * MAX);
-	for (int i = 0; i < N; i++) {
-		long long A, B, C;
-		cin >> A;
-		if (A == 1) {
-			cin >> B;
-			int flavor = Binary_search(1, MAX, B);
-			update(1, 1, MAX, flavor, -1);
-			cout << flavor << '\n';
-		}
-		else {
-			cin >> B >> C;
-			update(1, 1, MAX, B, C);
-		}
-	}
+	tree.resize(4 * SZ);
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
-void update(int node, int start, int end, int idx, long long diff) {
+void update(int node, int start, int end, int idx, int val) {
 	if (idx > end || idx < start) return;
 	if (start == end) {
-		tree[node] += diff;
+		tree[node] += val;
 		return;
 	}
 	int mid = (start + end) / 2;
-	update(node * 2, start, mid, idx, diff);
-	update(node * 2 + 1, mid + 1, end, idx, diff);
+	update(node * 2, start, mid, idx, val);
+	update(node * 2 + 1, mid + 1, end, idx, val);
 	tree[node] = tree[node * 2] + tree[node * 2 + 1];
 	return;
 }
 
-long long query(int node, int start, int end, int left, int right) {
+int query(int node, int start, int end, int left, int right) {
 	if (left > end || right < start) return 0;
 	if (left <= start && end <= right) return tree[node];
 	int mid = (start + end) / 2;
-	return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
+	return (query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right));
 }
 
-int Binary_search(int left, int right, long long data) {
-	int mid = (left + right) / 2;
+int BinarySearch(int left, int right, int val) {
 	if (left < right) {
-		if (query(1, 1, MAX, left, mid) < data) return Binary_search(mid + 1, right, data - query(1, 1, MAX, left, mid));
-		else return Binary_search(left, mid, data);
+		int mid = (left + right) / 2;
+		int q = query(1, 1, SZ, 1, mid);
+		if (q >= val) return BinarySearch(left, mid, val);
+		else return BinarySearch(mid + 1, right, val);
 	}
-	else return mid;
+	else return left;
 }
