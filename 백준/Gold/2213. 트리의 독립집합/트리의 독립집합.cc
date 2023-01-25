181f7ded468bed54f6ab8c1e8de3063f5231191a
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-25 14:16:30 +0900

    [Gold I] Title: 트리의 독립집합, Time: 8 ms, Memory: 3636 KB -BaekjoonHub

--- 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.cc
+++ 백준/Gold/2213. 트리의 독립집합/트리의 독립집합.cc
@@ -1,69 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
-int N, ans;
-int cost[10001];
-vector <int> tree[10001];
+int N;
+int arr[10001];
+vector <int> graph[10001], v;
 int table[10001][2];
-vector <int> path[10001][2];
+vector <pii> ans[10001][2];
 
-int DFS(int node, int par, int state);
+int DP(int node, int mode, int par);
+void DFS(int node, int mode);
+
+void solve() {
+	cin >> N;
+	for (int i = 1; i <= N; i++) cin >> arr[i];
+	for (int i = 0; i < N - 1; i++) {
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+	memset(table, -1, sizeof(table));
+	int a = DP(1, 0, -1);
+	int b = DP(1, 1, -1);
+	if (a > b) {
+		cout << a << '\n';
+		DFS(1, 0);
+	}
+	else {
+		cout << b << '\n';
+		int node = 1, mode = 1;
+		DFS(1, 1);
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++) cout << v[i] << " ";
+	return;
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N;
-	memset(table, -1, sizeof(table));
-	for (int i = 1; i <= N; i++) cin >> cost[i];
-	for (int i = 0; i < N - 1; i++) {
-		int A, B;
-		cin >> A >> B;
-		tree[A].push_back(B);
-		tree[B].push_back(A);
-	}
-	int A = DFS(1, 0, 0);
-	int B = DFS(1, 0, 1);
-	if (A > B) {
-		ans = A;
-		for (int j = 0; j < path[1][0].size(); j++) path[0][0].push_back(path[1][0][j]);
-	}
-	else {
-		ans = B;
-		for (int j = 0; j < path[1][1].size(); j++) path[0][0].push_back(path[1][1][j]);
-	}
-	sort(path[0][0].begin(), path[0][0].end());
-	path[0][0].erase(unique(path[0][0].begin(), path[0][0].end()), path[0][0].end());
-	cout << ans << '\n';
-	for (int i = 0; i < path[0][0].size(); i++) cout << path[0][0][i] << " ";
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
-int DFS(int node, int par, int state) {
-	if (table[node][state] != -1) return table[node][state];
-	if (state == 0) {
-		table[node][state] = 0;
-		for (int i = 0; i < tree[node].size(); i++) {
-			if (tree[node][i] == par) continue;
-			int A = DFS(tree[node][i], node, 0);
-			int B = DFS(tree[node][i], node, 1);
-			if (A > B) {
-				table[node][state] += A;
-				for (int j = 0; j < path[tree[node][i]][0].size(); j++) path[node][state].push_back(path[tree[node][i]][0][j]);
-			}
-			else {
-				table[node][state] += B;
-				for (int j = 0; j < path[tree[node][i]][1].size(); j++) path[node][state].push_back(path[tree[node][i]][1][j]);
-			}			
+int DP(int node, int mode, int par) {
+	int& ret = table[node][mode];
+	if (ret != -1) return ret;	
+	if (mode) {
+		ret = arr[node];
+		for (int i = 0; i < graph[node].size(); i++) {
+			int next = graph[node][i];
+			if (next == par) continue;
+			ret += DP(next, 0, node);
+			ans[node][mode].push_back({ next,0 });
 		}
 	}
 	else {
-		table[node][state] = cost[node];
-		path[node][state].push_back(node);
-		for (int i = 0; i < tree[node].size(); i++) {
-			if (tree[node][i] == par) continue;
-			table[node][state] += DFS(tree[node][i], node, 0);
-			for (int j = 0; j < path[tree[node][i]][0].size(); j++) path[node][state].push_back(path[tree[node][i]][0][j]);			
+		ret = 0;
+		for (int i = 0; i < graph[node].size(); i++) {
+			int next = graph[node][i];
+			if (next == par) continue;
+			if (DP(next, 0, node) > DP(next, 1, node)) {
+				ret += DP(next, 0, node);
+				ans[node][mode].push_back({ next,0 });
+			}
+			else {
+				ret += DP(next, 1, node);
+				ans[node][mode].push_back({ next,1 });
+			}			
 		}
-	}	
-	return table[node][state];
+	}
+	return ret;
+}
+
+void DFS(int node, int mode) {
+	if (mode == 1) v.push_back(node);
+	for (int i = 0; i < ans[node][mode].size(); i++) {
+		DFS(ans[node][mode][i].first, ans[node][mode][i].second);
+	}
+	return;
 }
