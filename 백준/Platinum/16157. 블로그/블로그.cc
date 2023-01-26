39d1a976970ad8c3cb87f3b5858f4ffb975d2fd8
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-26 11:19:47 +0900

    [Platinum IV] Title: 블로그, Time: 0 ms, Memory: 2144 KB -BaekjoonHub

--- 백준/Platinum/16157. 블로그/블로그.cc
+++ 백준/Platinum/16157. 블로그/블로그.cc
@@ -1,36 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 const int INF = 0x3f3f3f3f;
 
 int N;
 string S;
-int arr[101];
-int table[101][101][4];
+int table[101][101][3];
 
 int DP(int left, int right, int color);
+
+void solve() {
+	cin >> N >> S;
+	for (int i = 0; i < S.length(); i++) {
+		if (S[i] == 'R') S[i] = '0';
+		else if (S[i] == 'G') S[i] = '1';
+		else S[i] ='2';
+	}
+	memset(table, -1, sizeof(table));
+	cout << 1 + DP(0, N - 1, S[0] - '0');
+	return;
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N >> S;
-	for (int i = 0; i < N; i++) {
-		if (S[i] == 'R') arr[i] = 1;
-		else if (S[i] == 'G') arr[i] = 2;
-		else arr[i] = 3;
-	}
-	memset(table, -1, sizeof(table));	
-	cout << DP(0, N - 1, 0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
 int DP(int left, int right, int color) {
-	if (left > right) return 0;
-	if (left == right) return (arr[left] != color);
+	if (left >= N) return 0;
+	if (left == right) return (S[left] - '0' != color);
+	if (S[left] - '0' == color) return DP(left + 1, right, color);
 	int& ret = table[left][right][color];
 	if (ret != -1) return ret;
 	ret = INF;
-	int a = (arr[left] != color);
-	ret = min(ret, a + DP(left + 1, right, color));	
-	for (int i = left; i <= right; i++) ret = min(ret, 1 + DP(left, i, arr[left]) + DP(i + 1, right, color));
+	for (int i = left; i <= right; i++) {
+		if (S[i] == S[left]) ret = min(ret, 1 + DP(left, i, S[left] - '0') + DP(i + 1, right, color));
+	}
 	return ret;
 }
