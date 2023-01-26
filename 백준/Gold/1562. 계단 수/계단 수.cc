2dfe516f4f33b4c43fc0b2be08b9f532b4105dd6
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-26 09:38:52 +0900

    [Gold I] Title: 계단 수, Time: 0 ms, Memory: 6060 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.cc
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -1,32 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-const long long mod = 1000000000;
+using ll = long long;
+using pii = pair<int, int>;
+const int mod = 1000000000;
 
 int N;
-long long ans;
-long long table[1 << 11][10][100];
+int table[(1 << 10)][101][10];
 
-long long DP(int state, int num, int pos);
+int DP(int state, int pos, int now);
+
+void solve() {
+	cin >> N;
+	memset(table, -1, sizeof(table));
+	int ans = 0;
+	for (int i = 1; i < 10; i++) ans = (ans + DP((1 << i), 1, i)) % mod;
+	cout << ans;
+	return;
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N;
-	memset(table, -1, sizeof(table));
-	for (int i = 1; i < 10; i++) ans = (ans + DP((1 << i), i, 1)) % mod;
-	cout << ans << '\n';
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
-long long DP(int state, int num, int pos) {
-	if (pos == N) {	
-		if (state == (1 << 10) - 1) return 1;		
-		else return 0;
-	}
-	long long& ret = table[state][num][pos];
+int DP(int state, int pos, int now) {
+	if (pos == N) return (state == (1 << 10) - 1);
+	int& ret = table[state][pos][now];
 	if (ret != -1) return ret;
 	ret = 0;
-	if (num > 0) ret = (ret + DP((state | (1 << (num - 1))), num - 1, pos + 1)) % mod;
-	if (num < 9) ret = (ret + DP((state | (1 << (num + 1))), num + 1, pos + 1)) % mod;
+	if (now > 0) ret += DP(state | (1 << (now - 1)), pos + 1, now - 1);
+	if (now < 9) ret += DP(state | (1 << (now + 1)), pos + 1, now + 1);
+	ret %= mod;
 	return ret;
 }
