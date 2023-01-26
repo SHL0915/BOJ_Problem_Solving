f2ca6add0dac8ed2a8201fadadaa05fe7b7ddad0
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-26 22:07:53 +0900

    [Silver I] Title: 회의실 배정, Time: 28 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,32 +1,38 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
-int N, ans, ptr;
-vector <pair <int, int>> meeting;
+int N;
+pii arr[100001];
 
-bool cmp(pair <int, int> A, pair <int, int> B) {
-	if (A.second == B.second) return A.first < B.first;
-	else return A.second < B.second;
+bool cmp(pii A, pii B);
+
+void solve() {
+	cin >> N;
+	for (int i = 0; i < N; i++) cin >> arr[i].first >> arr[i].second;
+	sort(arr, arr + N, cmp);
+	int now = 0, ans = 0;
+	for (int i = 0; i < N; i++) {
+		if (arr[i].first >= now) {
+			now = arr[i].second;
+			ans++;
+		}
+	}
+	cout << ans;
+	return;
 }
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		int start, end;
-		cin >> start >> end;
-		meeting.push_back({ start, end });
-	}
-	sort(meeting.begin(), meeting.end(), cmp);
-	for (int i = 0; i < N; i++) {
-		if (meeting[i].first >= ptr) {
-			ptr = meeting[i].second;
-			ans++;
-		}
-	}
-	cout << ans;
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
+}
+
+bool cmp(pii A, pii B) {
+	if (A.second == B.second) return A.first < B.first;
+	return A.second < B.second;
 }
