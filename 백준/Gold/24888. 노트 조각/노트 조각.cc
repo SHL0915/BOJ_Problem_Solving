fc3db932b3bccd9fa35cda9feb08dcba947c8053
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-26 15:11:37 +0900

    [Gold I] Title: 노트 조각, Time: 232 ms, Memory: 39476 KB -BaekjoonHub

--- 백준/Gold/24888. 노트 조각/노트 조각.cc
+++ 백준/Gold/24888. 노트 조각/노트 조각.cc
@@ -1,90 +1,96 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const ll INF = 1000000000000000;
+using ll = long long;
+using pii = pair<ll, ll>;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 
 struct cmp {
-	bool operator() (pair<ll, int> A, pair<ll, int> B){
-		return A.first > B.first;
+	bool operator() (pair<pii, int> A, pair<pii, int> B) {
+		if (A.first.first == B.first.first) return A.first.second < B.first.second;
+		return A.first.first > B.first.first;
 	}
 };
 
 int N, M, cnt;
-vector <pair<int, ll>> graph[200001];
-int paper[200001];
-ll dist[200001][2];
-vector <int> path[200001];
-deque <int> ans;
+vector <pii> graph[200001];
+ll arr[200001];
+pii dist[200001];
+vector <int> path[200001], ans;
 
 void Dijkstra(int start);
-void FindPath(int node);
+void DFS(int node);
+int compare(pii A, pii B);
+
+void solve() {
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		ll a, b, c;
+		cin >> a >> b >> c;
+		graph[a].push_back({ b,c });
+		graph[b].push_back({ a,c });
+	}
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i];
+		cnt += arr[i];
+	}
+	Dijkstra(1);
+	if (dist[N].second < cnt) {
+		cout << -1;
+		return;
+	}
+	DFS(N);
+	cout << ans.size() << '\n';
+	for (int i = ans.size() - 1; i >= 0; i--) cout << ans[i] << " ";
+	return;
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N >> M;
-	for (int i = 0; i < M; i++) {
-		int u, v;
-		ll w;
-		cin >> u >> v >> w;
-		graph[u].push_back({ v,w });
-		graph[v].push_back({ u,w });
-	}
-	for (int i = 1; i <= N; i++) {
-		cin >> paper[i];
-		if (paper[i] == 1) cnt++;
-	}
-	Dijkstra(1);
-	if (dist[N][1] < cnt) {
-		cout << -1;
-		return 0;
-	}
-	FindPath(N);
-	cout << ans.size() << '\n';
-	for (int i = 0; i < ans.size(); i++) cout << ans[i] << " ";
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
 void Dijkstra(int start) {
-	for (int i = 1; i <= N; i++) dist[i][0] = INF;
-	priority_queue<pair<ll, int>, vector<pair<ll, int>>, cmp> pq;
-	pq.push({ 0, start });
-	if (paper[start] == 1) dist[start][1] = 1;
-	dist[start][0] = 0;
+	priority_queue <pair<pii, int>, vector<pair<pii, int>>, cmp> pq;
+	for (int i = 1; i <= N; i++) dist[i].first = INF;
+	dist[start] = { 0, arr[start] };
+	pq.push({ {0, arr[start]}, start });
 	while (pq.size()) {
+		pii val = pq.top().first;
 		int now = pq.top().second;
-		ll val = pq.top().first;
 		pq.pop();
-		if (dist[now][0] < val) continue;
+		if (compare(dist[now], val) == 1) continue;
 		for (int i = 0; i < graph[now].size(); i++) {
 			int next = graph[now][i].first;
-			ll next_val = val + graph[now][i].second;
-			if (dist[next][0] > next_val) {
+			pii next_val = { val.first + graph[now][i].second, val.second + arr[next] };
+			if (compare(dist[next], next_val) == -1) {
 				path[next].clear();
 				path[next].push_back(now);
-				dist[next][0] = next_val;
-				dist[next][1] = dist[now][1] + paper[next];
+				dist[next] = next_val;
 				pq.push({ next_val, next });
 			}
-			else if (dist[next][0] == next_val) {
-				dist[next][1] = max(dist[next][1], dist[now][1] + paper[next]);
-				path[next].push_back(now);
-			}
+			else if (compare(dist[next], next_val) == 0) path[next].push_back(now);
 		}
 	}
 }
 
-void FindPath(int node) {
-	ans.push_front(node);
-	if (node == 1) return;	
+void DFS(int node) {
+	ans.push_back(node);
+	if (path[node].size()) DFS(path[node][0]);
+	return;
+}
+
+int compare(pii A, pii B) {
+	if (A.first == B.first && A.second == B.second) return 0;
+	else if (A.first == B.first) {
+		if (A.second > B.second) return 1;
+		else return -1;
+	}
 	else {
-		ll max_num = 0;
-		for (int i = 0; i < path[node].size(); i++) max_num = max(max_num, dist[path[node][i]][1]);
-		for (int i = 0; i < path[node].size(); i++) {
-			if (dist[path[node][i]][1] == max_num) {
-				FindPath(path[node][i]);
-				break;
-			}
-		}
+		if (A.first < B.first) return 1;
+		else return -1;
 	}
 }
