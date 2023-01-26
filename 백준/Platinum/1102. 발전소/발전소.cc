778df14653b3a17a0f4ee0690534d627c75551cb
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-26 09:26:34 +0900

    [Platinum V] Title: 발전소, Time: 20 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Platinum/1102. 발전소/발전소.cc
+++ 백준/Platinum/1102. 발전소/발전소.cc
@@ -1,54 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int INF = 0x3f3f3f3f;
 
-const int INF = 1234567890;
-
-int N, P, ans;
+int N, P, ans = INF;
+int arr[20][20];
+int table[(1 << 20)];
 string S;
-int cost[16][16];
-int table[(1 << 16)];
 
-int strtoi(string s);
-int DP(int state);
+int DP(int state, int cnt);
+int strtoint(string s);
+
+void solve() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) cin >> arr[i][j];
+	}
+	cin >> S;
+	cin >> P;
+	memset(table, -1, sizeof(table));
+	int state = strtoint(S);
+	int cnt = 0;
+	for (int i = 0; i < S.length(); i++) if (S[i] == 'Y') cnt++;
+	int ans = DP(state, cnt);
+	if (ans == INF) cout << -1;
+	else cout << DP(state, cnt);
+	return;
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) cin >> cost[i][j];
-	}
-	memset(table, -1, sizeof(table));
-	cin >> S;
-	cin >> P;
-	ans = DP(strtoi(S));
-	if (P == 0) cout << 0;
-	else if (ans == INF) cout << -1;
-	else cout << ans;
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
-int strtoi(string s) {
-	int ret = 0;
-	int bit = 1;
-	for (int i = 0; i < s.length(); i++) {
-		if (s[i] == 'Y') ret += bit;		
-		bit <<= 1;
-	}
-	return ret;
-}
-
-int DP(int state) {
-	if (table[state] != -1) return table[state];
-	int cnt = popcount((unsigned int)state);
+int DP(int state, int cnt) {
 	if (cnt >= P) return 0;
-	table[state] = INF;
+	int& ret = table[state];
+	if (ret != -1) return ret;
+	ret = INF;
 	for (int i = 0; i < N; i++) {
 		if (!(state & (1 << i))) continue;
 		for (int j = 0; j < N; j++) {
 			if (state & (1 << j)) continue;
-			table[state] = min(table[state], cost[i][j] + DP(state | (1 << j)));
+			ret = min(ret, arr[i][j] + DP(state | (1 << j), cnt + 1));
 		}
 	}
-	return table[state];
+	return ret;
+}
+
+int strtoint(string s) {
+	int ret = 0;
+	for (int i = s.length() - 1; i >= 0; i--) {
+		ret *= 2;
+		if (s[i] == 'Y') ret += 1;
+	}
+	return ret;
 }
