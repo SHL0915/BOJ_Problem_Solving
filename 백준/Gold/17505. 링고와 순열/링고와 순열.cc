88de425ff5a09f23c9291d1541831c63b822e79a
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-27 09:45:38 +0900

    [Gold V] Title: 링고와 순열, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17505. 링고와 순열/링고와 순열.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+ll N, K;
+
+void solve() {
+	cin >> N >> K;
+	ll now = 1;
+	for (int i = N - 1; i >= 0; i--) {
+		if (K < i) {
+			for (int j = 0; j < i - K; j++) cout << now++ << " ";
+			cout << i + 1 << " ";
+			for (; now <= i; now++) cout << now << " ";
+			break;
+		}
+		K -= i;
+		cout << i + 1 << " ";
+	}
+	return;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
