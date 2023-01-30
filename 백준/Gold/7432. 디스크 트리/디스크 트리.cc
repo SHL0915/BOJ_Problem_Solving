8ecb8771e6f1633a94c90db7e185406032518b5f
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-01-30 11:53:00 +0900

    [Gold II] Title: 디스크 트리, Time: 16 ms, Memory: 4800 KB -BaekjoonHub

--- 백준/Gold/7432. 디스크 트리/디스크 트리.cc
+++ 백준/Gold/7432. 디스크 트리/디스크 트리.cc
@@ -3,33 +3,27 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 
-struct Trie {
-	int level;
-	bool end;
+struct Trie {	
 	map <string, Trie*> ch;
 
-	Trie(int lv) {
-		level = lv;
-		end = false;
-	}
 	~Trie() {
 		for (auto i = ch.begin(); i != ch.end(); i++) delete (*i).second;
 	}
 
 	void insert(vector <string>& v, int idx) {
-		if (idx == v.size()) end = true;
+		if (idx == v.size()) return;
 		else {
 			string now = v[idx];
-			if (ch.count(now) == 0) ch[now] = new Trie(level + 1);
+			if (ch.count(now) == 0) ch[now] = new Trie();
 			ch[now]->insert(v, idx + 1);
 		}
 		return;
 	}
-	void func() {		
+	void func(int level) {		
 		for (auto i = ch.begin(); i != ch.end(); i++) {
 			for (int j = 0; j < level; j++) cout << " ";
 			cout << (*i).first << '\n';
-			(*i).second->func();
+			(*i).second->func(level + 1);
 		}
 		return;
 	}
@@ -39,7 +33,7 @@ int N;
 
 void solve() {
 	cin >> N;
-	Trie* root = new Trie(0);
+	Trie* root = new Trie();
 	for (int i = 0; i < N; i++) {
 		string s, temp = "";
 		vector <string> v;
@@ -54,7 +48,7 @@ void solve() {
 		v.push_back(temp);
 		root->insert(v, 0);
 	}
-	root->func();
+	root->func(0);
 	delete root;
 	return;
 }
