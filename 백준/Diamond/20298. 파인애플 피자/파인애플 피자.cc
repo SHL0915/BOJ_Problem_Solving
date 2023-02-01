ee21464bfc499fc98b3c00f284fce0efa7646d71
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-01 12:06:22 +0900

    [Diamond III] Title: 파인애플 피자, Time: 248 ms, Memory: 8348 KB -BaekjoonHub

--- 백준/Diamond/20298. 파인애플 피자/파인애플 피자.cc
+++ 백준/Diamond/20298. 파인애플 피자/파인애플 피자.cc
@@ -1,46 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using pii = pair<ll, ll>;
+using pii = pair<int, int>;
 
-ll N, K;
-ll arr[200001];
-ll cmp[100001];
-pii temp[100001];
-ll pi[100001];
-vector <ll> arr_val, cmp_val, ans, tree;
+int N, K;
+int arr[200001], B[100001], pi[100001];
+pii cmp[100001];
+vector <int> a, b, ans, tree;
 
-void update(int node, int start, int end, int idx, ll val);
-ll query(int node, int start, int end, int left, int right);
-void makePi();
+void update(int node, int start, int end, int idx, int val);
+int query(int node, int start, int end, int left, int right);
 void kmp();
+void makePi();
 bool check(int i, int j, int type);
 
 void solve() {
 	cin >> N >> K;
 	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
-		arr_val.push_back(arr[i]);
+		a.push_back(arr[i]);
 	}
-	sort(arr_val.begin(), arr_val.end());
-	arr_val.erase(unique(arr_val.begin(), arr_val.end()), arr_val.end());
-	for (int i = 0; i < N; i++) arr[i] = lower_bound(arr_val.begin(), arr_val.end(), arr[i]) - arr_val.begin() + 1;
-	for (int i = 0; i < N; i++) arr[N + i] = arr[i];
-
 	for (int i = 0; i < K; i++) {
-		cin >> cmp[i];
-		cmp_val.push_back(cmp[i]);
-	}
-	sort(cmp_val.begin(), cmp_val.end());
-	cmp_val.erase(unique(cmp_val.begin(), cmp_val.end()), cmp_val.end());
-	for (int i = 0; i < K; i++) cmp[i] = lower_bound(cmp_val.begin(), cmp_val.end(), cmp[i]) - cmp_val.begin() + 1;
-	tree.resize(4 * K, 0);
-	for (int i = 0; i < K; i++) {
-		update(1, 1, K, cmp[i], 1);
-		temp[i] = { query(1,1,K,1,cmp[i] - 1), query(1,1,K,cmp[i] + 1,K) };
+		cin >> B[i];
+		b.push_back(B[i]);
 	}
 	
-	makePi();
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	a.erase(unique(a.begin(), a.end()), a.end());
+	b.erase(unique(b.begin(), b.end()), b.end());
+
+	for (int i = 0; i < N; i++) {
+		arr[i] = lower_bound(a.begin(), a.end(), arr[i]) - a.begin() + 1;
+		arr[N + i] = arr[i];
+	}
+	for (int i = 0; i < K; i++) B[i] = lower_bound(b.begin(), b.end(), B[i]) - b.begin() + 1;
+	
 	kmp();
 
 	cout << ans.size();
@@ -56,7 +51,7 @@ int main(void) {
 	return 0;
 }
 
-void update(int node, int start, int end, int idx, ll val) {
+void update(int node, int start, int end, int idx, int val) {
 	if (idx > end || idx < start) return;
 	if (start == end) {
 		tree[node] += val;
@@ -69,39 +64,18 @@ void update(int node, int start, int end, int idx, ll val) {
 	return;
 }
 
-ll query(int node, int start, int end, int left, int right) {
+int query(int node, int start, int end, int left, int right) {
 	if (left > end || right < start) return 0;
 	if (left <= start && end <= right) return tree[node];
 	int mid = (start + end) / 2;
 	return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
 }
 
-void makePi() {
-	tree.clear();
-	tree.resize(4 * K, 0);
-	ll n = K, pos = 1, k = 0;
-	while (pos + k < n) {
-		if (check(pos + k, k, 0)) {
-			update(1, 1, K, cmp[pos + k], 1);
-			k++;
-			pi[pos + k - 1] = k;
-		}
-		else {
-			if (k == 0) pos++;
-			else {
-				for (int i = pos; i < pos + k - pi[k - 1]; i++) update(1, 1, K, cmp[i], -1);
-				pos += (k - pi[k - 1]);
-				k = pi[k - 1];
-			}
-		}
-	}
-	return;
-}
-
 void kmp() {
+	makePi();
 	tree.clear();
 	tree.resize(4 * N, 0);
-	ll n = 2 * N, m = K, pos = 0, k = 0;
+	int m = K, pos = 0, k = 0;
 	while (pos < N) {
 		if (k < m && check(pos + k, k, 1)) {
 			update(1, 1, N, arr[pos + k], 1);
@@ -109,12 +83,38 @@ void kmp() {
 			if (k == m) ans.push_back(pos);
 		}
 		else {
-			if (k == 0) pos++;			
+			if (k == 0) pos++;
 			else {
-				for (int i = pos; i < pos + k - pi[k - 1]; i++) {
-					if (i >= N) break;
-					update(1, 1, N, arr[i], -1);
-				}
+				for (int i = pos; i < pos + k - pi[k - 1]; i++) update(1, 1, N, arr[i], -1);
+				pos += k - pi[k - 1];
+				k = pi[k - 1];
+			}
+		}
+	}
+	return;
+}
+
+void makePi() {
+	tree.clear();
+	tree.resize(4 * K, 0);
+	for (int i = 0; i < K; i++) {
+		update(1, 1, K, B[i], 1);
+		cmp[i] = { query(1,1,K,1,B[i] - 1), query(1,1,K,B[i] + 1, K) };
+	}
+	tree.clear();
+	tree.resize(4 * K, 0);
+	
+	int n = K, pos = 1, k = 0;
+	while (pos + k < n) {
+		if (check(pos + k, k, 0)) {
+			update(1, 1, K, B[pos + k], 1);
+			k++;
+			pi[pos + k - 1] = k;
+		}
+		else {
+			if (k == 0) pos++;
+			else {
+				for (int i = pos; i < pos + k - pi[k - 1]; i++) update(1, 1, K, B[i], -1);
 				pos += k - pi[k - 1];
 				k = pi[k - 1];
 			}
@@ -124,15 +124,15 @@ void kmp() {
 }
 
 bool check(int i, int j, int type) {
-	bool a, b;
+	bool l, r;
 	if (type) {
-		a = (query(1, 1, N, 1, arr[i] - 1) == temp[j].first);
-		b = (query(1, 1, N, arr[i] + 1, N) == temp[j].second);
+		l = (query(1, 1, N, 1, arr[i] - 1) == cmp[j].first);
+		r = (query(1, 1, N, arr[i] + 1, N) == cmp[j].second);
 	}
 	else {
-		a = (query(1, 1, K, 1, cmp[i] - 1) == temp[j].first);
-		b = (query(1, 1, K, cmp[i] + 1, K) == temp[j].second);
+		l = (query(1, 1, K, 1, B[i] - 1) == cmp[j].first);
+		r = (query(1, 1, K, B[i] + 1, K) == cmp[j].second);
 	}
-	if (a == true && b == true) return true;
+	if (l == true && r == true) return true;
 	else return false;
 }
