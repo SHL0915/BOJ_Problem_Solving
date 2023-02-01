a318729a15628b0cc717a84f65125c72ee19e22f
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-01 14:54:15 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -1,37 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<ll, ll> pll;
+using ll = long long;
+using pii = pair<ll, ll>;
 
-pair<pll, pll> L[2];
+pii A, B, C, D;
 
-int func(pair<pll, pll> L1, pair<pll, pll> L2);
-int CCW(pll A, pll B, pll C);
+ll CCW(pii a, pii b, pii c);
+pii sub(pii a, pii b);
+
+void solve() {
+	cin >> A.first >> A.second;
+	cin >> B.first >> B.second;
+	cin >> C.first >> C.second;
+	cin >> D.first >> D.second;
+	int ccw1 = CCW(A, B, C) * CCW(A, B, D);
+	int ccw2 = CCW(C, D, A) * CCW(C, D, B);
+	if (ccw1 == 0 && ccw2 == 0) {
+		if (A > B) swap(A, B);
+		if (C > D) swap(C, D);
+		if (A <= D && C <= B) cout << 1;
+		else cout << 0;
+	}
+	else if (ccw1 <= 0 && ccw2 <= 0) cout << 1;
+	else cout << 0;
+	return;
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	for (int i = 0; i < 2; i++) cin >> L[i].first.first >> L[i].first.second >> L[i].second.first >> L[i].second.second;
-	cout << func(L[0], L[1]);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
-int func(pair<pll, pll> L1, pair<pll, pll> L2) {
-	pll p1 = L1.first, p2 = L1.second, p3 = L2.first, p4 = L2.second;
-	int ccw1 = CCW(p1, p2, p3) * CCW(p1, p2, p4);
-	int ccw2 = CCW(p3, p4, p1) * CCW(p3, p4, p2);
-	if (ccw1 == 0 && ccw2 == 0) {
-		if (p1 > p2) swap(p1, p2);
-		if (p3 > p4) swap(p3, p4);
-		return (p1 <= p4 && p3 <= p2);
-	}
-	else return (ccw1 <= 0 && ccw2 <= 0);
+ll CCW(pii a, pii b, pii c) {
+	pii u = sub(b, a);
+	pii v = sub(c, b);
+	ll ret = u.first * v.second - u.second * v.first;
+	if (ret) ret /= abs(ret);
+	return ret;
 }
 
-int CCW(pll A, pll B, pll C) {
-	ll a = A.first * B.second + B.first * C.second + C.first * A.second;
-	ll b = B.first * A.second + C.first * B.second + A.first * C.second;
-	if (a > b) return 1;
-	else if (a == b) return 0;
-	else return -1;
+pii sub(pii a, pii b) {
+	return { a.first - b.first, a.second - b.second };
 }
