ae288743ff39f46ce0896b754b9f58a057522036
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-02 15:48:11 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 96 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -1,38 +1,44 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
 int N, M, R, cnt = 1;
 vector <int> graph[100001];
-queue <int> q;
 int mark[100001];
+queue <int> q;
 
 void BFS();
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void solve() {
 	cin >> N >> M >> R;
 	for (int i = 0; i < M; i++) {
-		int u, v;
-		cin >> u >> v;
-		graph[u].push_back(v);
-		graph[v].push_back(u);
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
 	}
 	for (int i = 1; i <= N; i++) sort(graph[i].begin(), graph[i].end());
 	q.push(R);
 	while (q.size()) BFS();
 	for (int i = 1; i <= N; i++) cout << mark[i] << '\n';
+	return;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
 void BFS() {
-	int vertex = q.front();
+	int node = q.front();
 	q.pop();
-	if (mark[vertex] != 0) return;
-	mark[vertex] = cnt++;
-	for (int i = 0; i < graph[vertex].size(); i++) q.push(graph[vertex][i]);
+	if (mark[node]) return;
+	mark[node] = cnt++;
+	for (int i = 0; i < graph[node].size(); i++) q.push(graph[node][i]);
 	return;
 }
