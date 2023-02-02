927a5d0d58116da87a5ec2fd84b8e91b6601b9d2
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-02 15:16:51 +0900

    [Silver I] Title: 그림, Time: 28 ms, Memory: 11672 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,44 +1,54 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
 
-int N, M, num, cnt, ans;
-int paper[500][500];
-int mark[500][500];
+int N, M, ans, cnt, num;
+int arr[501][501];
+int mark[501][501];
 
 void DFS(int x, int y);
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void solve() {
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> paper[i][j];
-			if (paper[i][j] == 0) mark[i][j] = 1;
-		}
+		for (int j = 0; j < M; j++) cin >> arr[i][j];
 	}
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			if (mark[i][j] == 0) {
-				cnt = 0;
+			if (arr[i][j] == 1 && mark[i][j] == 0) {
 				num++;
+				cnt = 0;
 				DFS(j, i);
 				ans = max(ans, cnt);
 			}
 		}
 	}
 	cout << num << '\n' << ans;
+	return;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
 void DFS(int x, int y) {
-	if (mark[y][x] != 0) return;
+	if (x < 0 || x >= M || y < 0 || y >= N) return;
+	if (arr[y][x] == 0) return;
+	if (mark[y][x]) return;
 	mark[y][x] = 1;
 	cnt++;
-	if (x > 0) DFS(x - 1, y);
-	if (x < M - 1) DFS(x + 1, y);
-	if (y > 0) DFS(x, y - 1);
-	if (y < N - 1) DFS(x, y + 1);
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		DFS(nx, ny);
+	}
 	return;
 }
