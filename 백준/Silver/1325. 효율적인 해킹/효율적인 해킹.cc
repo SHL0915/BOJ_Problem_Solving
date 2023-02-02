369638ff1287d2b07a4e0a172e48c4a0f9d41b89
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-02 16:16:22 +0900

    [Silver I] Title: 효율적인 해킹, Time: 3248 ms, Memory: 3488 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -1,36 +1,55 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
-int N, M, ans, idx;
+int N, M, cnt;
 vector <int> graph[10001];
 int mark[10001];
-int cnt[10001];
+pii ans[10001];
 
-void DFS(int vertex, int start);
+void DFS(int node);
+bool cmp(pii a, pii b);
+
+void solve() {
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+		graph[b].push_back(a);
+	}
+	for (int i = 1; i <= N; i++) {
+		memset(mark, 0, sizeof(mark));
+		cnt = 0;
+		DFS(i);
+		ans[i] = { cnt, i };
+	}
+	sort(ans + 1, ans + N + 1, cmp);
+	for (int i = 1; i <= N; i++) {
+		if (ans[i].first == ans[1].first) cout << ans[i].second << ' ';
+		else break;
+	}
+	return;
+}
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N >> M;
-	for (int i = 0; i < M; i++) {
-		int A, B;
-		cin >> A >> B;
-		graph[B].push_back(A);
-	}
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) mark[j] = 0;		
-		DFS(i, i);
-		ans = max(ans, cnt[i]);
-	}
-	for (int i = 1; i <= N; i++) if (cnt[i] == ans) cout << i << " ";
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
-void DFS(int vertex, int start) {
-	if (mark[vertex] != 0) return;
-	mark[vertex] = 1;
-	cnt[start]++;
-	for (int i = 0; i < graph[vertex].size(); i++) DFS(graph[vertex][i], start);
+void DFS(int node) {
+	if (mark[node]) return;
+	mark[node] = 1;
+	cnt++;
+	for (int i = 0; i < graph[node].size(); i++) DFS(graph[node][i]);
 	return;
+}
+
+bool cmp(pii a, pii b) {
+	if (a.first == b.first) return a.second < b.second;
+	return a.first > b.first;
 }
