c29b6699a59eabf1075449298c27a4604912ff7c
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-06 09:56:24 +0900

    [Gold III] Title: 레이저 통신, Time: 0 ms, Memory: 2492 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.cc
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.cc
@@ -1,67 +1,90 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int INF = 0x3f3f3f3f;
 
-int W, H, k;
-int map[100][100];
-int mark[100][100];
-pair <int, int> arr[2];
-queue <pair<pair<int, int>, pair<int,int>>> q;
+int W, H, cnt;
+int arr[101][101];
+pii pos[2];
+int mark[101][101][4];
+queue <pair<pii, pii>> q, temp;
 
 void BFS();
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void solve() {
 	cin >> W >> H;
 	for (int i = 0; i < H; i++) {
-		string input;
-		cin >> input;
+		string s;
+		cin >> s;
 		for (int j = 0; j < W; j++) {
-			if (input[j] == '*') map[i][j] = 1;
-			else if (input[j] == 'C') arr[k++] = { j,i };
+			if (s[j] == '*') arr[i][j] = 1;
+			else if (s[j] == 'C') pos[cnt++] = { j,i };
 		}
 	}
-	mark[arr[0].second][arr[0].first] = 1;
-	q.push({ {arr[0].first - 1,arr[0].second},{1, 0} });
-	q.push({ {arr[0].first + 1,arr[0].second},{1, 1} });
-	q.push({ {arr[0].first,arr[0].second - 1},{1, 2} });
-	q.push({ {arr[0].first,arr[0].second + 1},{1, 3} });
-	while (q.size()) BFS();
-	cout << mark[arr[1].second][arr[1].first] - 1;
+	memset(mark, -1, sizeof(mark));
+	q.push({ {pos[0].first - 1, pos[0].second}, {0, 0} });
+	q.push({ {pos[0].first, pos[0].second + 1}, {0, 1} });
+	q.push({ {pos[0].first + 1, pos[0].second}, {0, 2} });
+	q.push({ {pos[0].first, pos[0].second - 1}, {0, 3} });
+	while (temp.size() || q.size()) {
+		while (temp.size()) {
+			q.push(temp.front());
+			temp.pop();
+		}
+		while (q.size()) BFS();
+	}
+	int ans = INF;
+	for (int i = 0; i < 4; i++) {
+		int x = pos[1].first, y = pos[1].second;
+		if (mark[y][x][i] == -1) continue;
+		ans = min(ans, mark[y][x][i]);
+	}
+	cout << ans;
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
 void BFS() {
-	pair<pair<int, int>, pair<int, int>> f = q.front();
+	pair<pii, pii> f = q.front();
 	q.pop();
 	int x = f.first.first;
 	int y = f.first.second;
-	int cnt = f.second.first;
-	int d = f.second.second;
-	if (x < 0 || x > W - 1 || y < 0 || y > H - 1) return;
-	if (map[y][x] == 1) return;
-	if (mark[y][x] != 0 && mark[y][x] < cnt) return;
-	mark[y][x] = cnt;
-	if (d == 0) {
-		q.push({ {x - 1, y},{cnt, 0} });
-		q.push({ {x, y - 1},{cnt + 1, 2} });
-		q.push({ {x, y + 1},{cnt + 1, 3} });
+	int d = f.second.first;
+	int dir = f.second.second;
+	if (x < 0 || x >= W || y < 0 || y >= H) return;
+	if (arr[y][x] == 1) return;
+	if (mark[y][x][dir] != -1) return;
+	mark[y][x][dir] = d;
+	if (dir == 0) {
+		q.push({ {x - 1, y}, {d, dir} });
+		temp.push({ {x, y + 1}, {d + 1, 1} });
+		temp.push({ {x, y - 1}, {d + 1, 3} });
 	}
-	else if (d == 1) {
-		q.push({ {x + 1, y},{cnt, 1} });
-		q.push({ {x, y - 1},{cnt + 1, 2} });
-		q.push({ {x, y + 1},{cnt + 1, 3} });
+	else if (dir == 1) {
+		q.push({ {x, y + 1}, {d, dir} });
+		temp.push({ {x - 1, y}, {d + 1, 0} });
+		temp.push({ {x + 1, y}, {d + 1, 2} });
 	}
-	else if (d == 2) {
-		q.push({ {x - 1, y},{cnt + 1, 0} });
-		q.push({ {x + 1, y},{cnt + 1, 1} });
-		q.push({ {x, y - 1},{cnt, 2} });
+	else if (dir == 2) {
+		q.push({ {x + 1, y}, {d, dir} });
+		temp.push({ {x, y + 1}, {d + 1, 1} });
+		temp.push({ {x, y - 1}, {d + 1, 3} });
 	}
 	else {
-		q.push({ {x - 1, y},{cnt + 1, 0} });
-		q.push({ {x + 1, y},{cnt + 1, 1} });
-		q.push({ {x, y + 1},{cnt , 3} });
+		q.push({ {x, y - 1}, {d, dir} });
+		temp.push({ {x - 1, y}, {d + 1, 0} });
+		temp.push({ {x + 1, y}, {d + 1, 2} });
 	}
 }
