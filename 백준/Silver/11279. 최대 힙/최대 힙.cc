077469a27c534bb75aebf769ad121fac1a47f056
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-07 11:26:57 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int N;
+priority_queue <int> pq;
+
+void solve() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int x; cin >> x;
+		if (x) pq.push(x);
+		else {
+			if (pq.size() == 0) cout << 0 << '\n';
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+	}
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
