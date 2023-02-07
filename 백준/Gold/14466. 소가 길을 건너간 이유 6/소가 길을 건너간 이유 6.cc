01c15b20b3a6088ecdc6cc41c78d01a20718890a
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-07 10:17:47 +0900

    [Gold IV] Title: 소가 길을 건너간 이유 6, Time: 12 ms, Memory: 3580 KB -BaekjoonHub

--- 백준/Gold/14466. 소가 길을 건너간 이유 6/소가 길을 건너간 이유 6.cc
+++ 백준/Gold/14466. 소가 길을 건너간 이유 6/소가 길을 건너간 이유 6.cc
@@ -1,56 +1,70 @@
-#include <iostream>
-#include <vector>
-#include <set>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
 
-int N, K, R, ans;
+int N, K, R, cnt, id = 1;
+ll num[10001];
+ll psum[10001];
+int arr[101][101];
 int mark[101][101];
-set <pair<pair<int, int>, pair<int,int>>> road;
-vector <pair<int, int>> cow;
+set <pair<pii, pii>> road;
 
-void clearMark();
 void DFS(int x, int y);
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void solve() {
 	cin >> N >> K >> R;
 	for (int i = 0; i < R; i++) {
 		int a, b, c, d;
 		cin >> a >> b >> c >> d;
-		road.insert({ {b,a}, {d,c} });
-		road.insert({ {d,c}, {b,a} });
+		road.insert({ {b,a},{d,c} });
+		road.insert({ {d,c},{b,a} });
 	}
 	for (int i = 0; i < K; i++) {
-		int x, y;
-		cin >> y >> x;
-		cow.push_back({ x,y });
+		int a, b;
+		cin >> a >> b;
+		arr[a][b] = 1;
 	}
-	for (int i = 0; i < cow.size(); i++) {
-		clearMark();
-		DFS(cow[i].first, cow[i].second);
-		for (int j = i + 1; j < cow.size(); j++) {
-			if (mark[cow[j].second][cow[j].first] == 0) ans++;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (mark[i][j] == 0) {
+				cnt = 0;
+				DFS(j, i);
+				num[id++] = cnt;
+			}
 		}
 	}
+	for (int i = 1; i < id; i++) psum[i] = psum[i - 1] + num[i];
+	ll ans = 0;
+	for (int i = 1; i < id; i++) ans += num[i] * (psum[id - 1] - psum[i]);
 	cout << ans;
-	return 0;
+	return;
 }
 
-void clearMark() {
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) mark[i][j] = 0;
-	}
-	return;
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
 }
 
 void DFS(int x, int y) {
 	if (x < 1 || x > N || y < 1 || y > N) return;
-	if (mark[y][x] != 0) return;
+	if (mark[y][x]) return;
 	mark[y][x] = 1;
-	if (road.count({ {x,y},{x - 1,y} }) == 0) DFS(x - 1, y);
-	if (road.count({ {x,y},{x,y - 1} }) == 0) DFS(x, y - 1);
-	if (road.count({ {x,y},{x + 1,y} }) == 0) DFS(x + 1, y);
-	if (road.count({ {x,y},{x,y + 1} }) == 0) DFS(x, y + 1);
+	cnt += arr[y][x];
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if (road.count({ { x,y }, { nx,ny } })) continue;
+		DFS(nx, ny);
+	}
 	return;
 }
