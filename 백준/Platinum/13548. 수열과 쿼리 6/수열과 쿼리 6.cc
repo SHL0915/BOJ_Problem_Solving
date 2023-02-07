8fbcf98151e1eee6feaa5b08121a8b3b6504ad3c
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-07 23:29:08 +0900

    [Platinum I] Title: 수열과 쿼리 6, Time: 268 ms, Memory: 4760 KB -BaekjoonHub

--- 백준/Platinum/13548. 수열과 쿼리 6/수열과 쿼리 6.cc
+++ 백준/Platinum/13548. 수열과 쿼리 6/수열과 쿼리 6.cc
@@ -3,39 +3,39 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 
-int N, M, check, B;
+int N, M, res, B;
 int arr[100001];
-int tree[200005];
+int cnt[100001];
+int table[100001];
 pair<pii, int> query[100001];
 int ans[100001];
 
 bool cmp(pair<pii, int> a, pair<pii, int> b);
 void push(int a);
 void pop(int a);
-void update(int idx, int val);
-int qq(int left, int right);
 
 void solve() {
 	cin >> N;
 	for (int i = 1; i <= N; i++) cin >> arr[i];
 	cin >> M;
 	for (int i = 0; i < M; i++) {
-		cin >> query[i].first.first >> query[i].first.second;
-		query[i].second = i;
+		int l, r;
+		cin >> l >> r;
+		query[i] = { {l,r},i };
 	}
 	while (B * B <= N) B++;
 	B--;
 	sort(query, query + M, cmp);
 	int l = query[0].first.first, r = query[0].first.second;
 	for (int i = l; i <= r; i++) push(i);
-	ans[query[0].second] = qq(1, 100001);
+	ans[query[0].second] = res;
 	for (int i = 1; i < M; i++) {
 		int nl = query[i].first.first, nr = query[i].first.second, id = query[i].second;
 		while (l < nl) pop(l++);
 		while (l > nl) push(--l);
 		while (r < nr) push(++r);
-		while (nr < r) pop(r--);
-		ans[id] = qq(1, 100001);
+		while (r > nr) pop(r--);
+		ans[id] = res;
 	}
 	for (int i = 0; i < M; i++) cout << ans[i] << '\n';
 	return;
@@ -60,26 +60,18 @@ bool cmp(pair<pii, int> a, pair<pii, int> b) {
 
 void push(int a) {
 	int now = arr[a];
-	update(now, 1);
+	if (cnt[now] != 0) table[cnt[now]]--;
+	cnt[now]++;
+	table[cnt[now]]++;
+	res = max(res, cnt[now]);
 	return;
 }
 
 void pop(int a) {
 	int now = arr[a];
-	update(now, -1);
+	table[cnt[now]]--;
+	if (res == cnt[now] && table[cnt[now]] == 0) res--;
+	cnt[now]--;
+	table[cnt[now]]++;
 	return;
-}
-
-void update(int idx, int val) {
-	for (tree[idx += 100000] += val; idx > 1; idx >>= 1) tree[idx >> 1] = max(tree[idx], tree[idx ^ 1]);
-	return;
-}
-
-int qq(int left, int right) {
-	int ret = 0;
-	for (left += 100000, right += 100000; left < right; left >>= 1, right >>= 1) {
-		if (left & 1) ret = max(ret, tree[left++]);
-		if (right & 1) ret = max(ret, tree[--right]);
-	}
-	return ret;
 }
