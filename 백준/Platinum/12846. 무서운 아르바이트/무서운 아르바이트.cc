0ea156386683c5b482b6f8d237c658e2ad6be9b4
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-08 11:59:49 +0900

    [Platinum V] Title: 무서운 아르바이트, Time: 16 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.cc
+++ 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.cc
@@ -1,70 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-const long long INF = 0x3f3f3f3f3f3f3f3fLL;
+using ll = long long;
+using pii = pair<int, int>;
 
 int N;
-long long arr[100001];
-vector <int> tree;
+ll arr[100005];
 
-void update(int node, int start, int end, int idx);
-int query(int node, int start, int end, int left, int right);
-long long DNC(int left, int right);
+ll DnC(int left, int right);
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void solve() {
 	cin >> N;
-	arr[0] = INF;
 	for (int i = 1; i <= N; i++) cin >> arr[i];
-	tree.resize(4 * N);
-	for (int i = 1; i <= N; i++) update(1, 1, N, i);
-	cout << DNC(1, N);
-	return 0;
-}
-
-void update(int node, int start, int end, int idx) {
-	if (idx > end || idx < start) return;
-	if (start == end) {
-		tree[node] = idx;
-		return;
-	}
-	int mid = (start + end) / 2;
-	update(node * 2, start, mid, idx);
-	update(node * 2 + 1, mid + 1, end, idx);
-	if (arr[tree[node * 2]] < arr[tree[node * 2 + 1]]) tree[node] = tree[node * 2];
-	else tree[node] = tree[node * 2 + 1];
+	cout << DnC(1, N);
 	return;
 }
 
-int query(int node, int start, int end, int left, int right) {
-	if (left > end || right < start) return 0;
-	if (left <= start && end <= right) return tree[node];
-	int mid = (start + end) / 2;
-	int A = query(node * 2, start, mid, left, right);
-	int B = query(node * 2 + 1, mid + 1, end, left, right);
-	if (arr[A] < arr[B]) return A;
-	else return B;
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
 }
 
-long long DNC(int left, int right) {
+ll DnC(int left, int right) {
+	if (left > right) return 0;
+	if (left == right) return arr[left];
+	ll ret = 0;
 	int mid = (left + right) / 2;
-	if (left < right) {
-		long long A = DNC(left, mid);
-		long long B = DNC(mid + 1, right);
-		int lpos = mid, rpos = mid, h;
-		long long C = 0;
-		while (1) {
-			h = arr[query(1, 1, N, lpos, rpos)];
-			C = max(C, (long long)(rpos - lpos + 1) * h);
-			if (lpos == left && rpos == right) break;
-			if (lpos == left) rpos++;
-			else if (rpos == right) lpos--;
-			else {
-				if (arr[lpos - 1] > arr[rpos + 1]) lpos--;
-				else rpos++;
-			}
-		}		
-		return max({ A,B,C });
+	ret = max(ret, DnC(left, mid - 1));
+	ret = max(ret, DnC(mid + 1, right));
+	ll h = arr[mid], l = mid, r = mid, w = 1;
+	while (l >= left && r <= right) {
+		h = min({ h, arr[l], arr[r] });
+		ret = max(ret, h * w);
+		if (l == left) {
+			r++;
+			w++;
+		}
+		else if (r == right) {
+			l--;
+			w--;
+		}
+		else if (arr[l - 1] >= arr[r + 1]) {
+			l--;
+			w++;
+		}
+		else {
+			r++;
+			w++;
+		}
 	}
-	else return arr[mid];
+	return ret;
 }
