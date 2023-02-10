cdfc099e8d5b0531cf1d9d33e791fa3f29479e01
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-10 15:12:57 +0900

    [Platinum IV] Title: 2-SAT - 3, Time: 32 ms, Memory: 6168 KB -BaekjoonHub

--- 백준/Platinum/11280. 2－SAT － 3/2－SAT － 3.cc
+++ 백준/Platinum/11280. 2－SAT － 3/2－SAT － 3.cc
@@ -1,57 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
-int N, M, cnt;
+int N, M, cnt, id_cnt;
 vector <int> graph[20001];
-int par[20001];
-int mark[20001];
-int ID[20001];
-stack <int> s;
+int par[20001], id[20001];
 vector <vector<int>> SCC;
+stack <int> st;
 
-int convert(int node) {
-	if (node < 0) return -node * 2;
-	else return node * 2 - 1;
-}
+int convert(int node);
 int DFS(int node);
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void solve() {
 	cin >> N >> M;
 	for (int i = 0; i < M; i++) {
-		int A, B;
-		cin >> A >> B;
-		graph[convert(-A)].push_back(convert(B));
-		graph[convert(-B)].push_back(convert(A));
+		int a, b;
+		cin >> a >> b;
+		graph[convert(-a)].push_back(convert(b));
+		graph[convert(-b)].push_back(convert(a));
 	}
 	for (int i = 1; i <= 2 * N; i++) if (par[i] == 0) DFS(i);
 	for (int i = 1; i <= N; i++) {
-		if (ID[i * 2] == ID[i * 2 - 1]) {
+		if (id[convert(i)] == id[convert(-i)]) {
 			cout << 0;
-			return 0;
+			return;
 		}
 	}
 	cout << 1;
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
+}
+
+int convert(int node) {
+	if (node > 0) return node * 2 - 1;
+	else return -node * 2;
 }
 
 int DFS(int node) {
 	par[node] = ++cnt;
-	int ret = par[node];
-	s.push(node);
+	int ret = cnt;
+	st.push(node);
 	for (int i = 0; i < graph[node].size(); i++) {
 		int next = graph[node][i];
 		if (par[next] == 0) ret = min(ret, DFS(next));
-		else if (mark[next] == 0) ret = min(ret, par[next]);
+		else if (id[next] == 0) ret = min(ret, par[next]);
 	}
 	if (ret == par[node]) {
+		id_cnt++;
 		vector <int> v;
 		while (1) {
-			int t = s.top();
-			s.pop();
-			mark[t] = 1;
-			ID[t] = SCC.size();
+			int t = st.top();
+			st.pop();
+			id[t] = id_cnt;
 			v.push_back(t);
 			if (t == node) break;
 		}
