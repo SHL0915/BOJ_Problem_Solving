f63c6660fedf35036029be1e4c430d2589d59da2
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-10 09:38:00 +0900

    [Gold III] Title: 줄 세우기, Time: 28 ms, Memory: 4152 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,39 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
-int N , M;
+int N, M;
 vector <int> graph[32001];
-int arr[32001];
+int in_degree[32001];
 queue <int> q;
 vector <int> ans;
 
-void Topological();
+void Topology();
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void solve() {
 	cin >> N >> M;
 	for (int i = 0; i < M; i++) {
-		int A, B;
-		cin >> A >> B;
-		graph[A].push_back(B);
-		arr[B] ++;
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		in_degree[b]++;
 	}
-	Topological();
+	Topology();
 	for (int i = 0; i < ans.size(); i++) cout << ans[i] << " ";
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
-void Topological() {
-	for (int i = 1; i <= N; i++) if (arr[i] == 0) q.push(i);
+void Topology() {
+	for (int i = 1; i <= N; i++) if (in_degree[i] == 0) q.push(i);
 	for (int i = 1; i <= N; i++) {
 		int now = q.front();
-		ans.push_back(now);
 		q.pop();
+		ans.push_back(now);
 		for (int j = 0; j < graph[now].size(); j++) {
-			int next = graph[now][j];
-			arr[next]--;
-			if (arr[next] == 0) q.push(next);
+			if (--in_degree[graph[now][j]] == 0) q.push(graph[now][j]);
 		}
 	}
 	return;
