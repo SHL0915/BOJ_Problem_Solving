c19492874fe11d63e17b18aa3875b32441882827
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-10 15:40:18 +0900

    [Platinum III] Title: 아이돌, Time: 16 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Platinum/3648. 아이돌/아이돌.cc
+++ 백준/Platinum/3648. 아이돌/아이돌.cc
@@ -1,37 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 1001;
+using ll = long long;
+using pii = pair<int, int>;
 
-int N, M, cnt, sz;
-vector <int> graph[MAX * 2];
-int par[MAX * 2];
-int ID[MAX * 2];
-int mark[MAX * 2];
-stack <int> s;
+int N, M, cnt, id_cnt;
+vector <int> graph[2001];
+int par[2001], id[2001];
+stack <int> st;
 
+void init();
 int convert(int node);
 int DFS(int node);
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void solve() {
 	while (cin >> N >> M) {
-		for (int i = 1; i <= N * 2; i++) graph[i].clear();
-		memset(par, 0, sizeof(par));
-		memset(mark, 0, sizeof(mark));
-		memset(ID, 0, sizeof(ID));
-		cnt = 0, sz = 0;
+		init();
 		for (int i = 0; i < M; i++) {
-			int A, B;
-			cin >> A >> B;
-			graph[convert(-A)].push_back(convert(B));
-			graph[convert(-B)].push_back(convert(A));
+			int a, b;
+			cin >> a >> b;
+			graph[convert(-a)].push_back(convert(b));
+			graph[convert(-b)].push_back(convert(a));
 		}
-		graph[2].push_back(1);
-		for (int i = 1; i <= N * 2; i++) if (par[i] == 0) DFS(i);
+		graph[convert(-1)].push_back(convert(1));
+		for (int i = 1; i <= 2 * N; i++) if (par[i] == 0) DFS(i);
 		int flag = 0;
 		for (int i = 1; i <= N; i++) {
-			if (ID[i * 2] == ID[i * 2 - 1]) {
+			if (id[convert(i)] == id[convert(-i)]) {
 				flag = 1;
 				break;
 			}
@@ -39,30 +33,52 @@ int main(void) {
 		if (flag) cout << "no\n";
 		else cout << "yes\n";
 	}
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
+void init() {
+	cnt = 0;
+	id_cnt = 0;
+	for (int i = 1; i <= 2 * N; i++) {
+		graph[i].clear();
+		par[i] = 0;
+		id[i] = 0;
+	}
+	return;
+}
+
 int convert(int node) {
-	if (node < 0) return -node * 2;
-	else return node * 2 - 1;
+	if (node > 0) return node * 2 - 1;
+	else return -node * 2;
 }
 
 int DFS(int node) {
 	par[node] = ++cnt;
-	int ret = par[node];
-	s.push(node);
+	int ret = cnt;
+	st.push(node);
 	for (int i = 0; i < graph[node].size(); i++) {
 		int next = graph[node][i];
 		if (par[next] == 0) ret = min(ret, DFS(next));
-		else if (mark[next] == 0) ret = min(ret, par[next]);
+		else if (id[next] == 0) ret = min(ret, par[next]);
 	}
 	if (ret == par[node]) {
-		sz++;
+		id_cnt++;
 		while (1) {
-			int t = s.top();
-			s.pop();
-			mark[t] = 1;
-			ID[t] = sz;
+			int t = st.top();
+			st.pop();
+			id[t] = id_cnt;
 			if (t == node) break;
 		}
 	}
