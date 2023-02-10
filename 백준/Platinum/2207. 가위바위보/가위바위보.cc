ac7b5eef9d23ef7295861d2303faf9378c11b3d4
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-10 15:30:20 +0900

    [Platinum IV] Title: 가위바위보, Time: 4 ms, Memory: 3044 KB -BaekjoonHub

--- 백준/Platinum/2207. 가위바위보/가위바위보.cc
+++ 백준/Platinum/2207. 가위바위보/가위바위보.cc
@@ -1,59 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 20001;
+using ll = long long;
+using pii = pair<int, int>;
 
-int N, M, cnt, sz;
-vector <int> graph[MAX];
-int par[MAX];
-int ID[MAX];
-int mark[MAX];
-stack <int> s;
+int N, M, cnt, id_cnt;
+vector <int> graph[20001];
+int par[20001], id[20001];
+stack <int> st;
 
 int convert(int node);
 int DFS(int node);
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> M >> N;
-	for (int i = 0; i < M; i++) {
-		int A, B;
-		cin >> A >> B;
-		graph[convert(-A)].push_back(convert(B));
-		graph[convert(-B)].push_back(convert(A));
+void solve() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int a, b;
+		cin >> a >> b;
+		graph[convert(-a)].push_back(convert(b));
+		graph[convert(-b)].push_back(convert(a));
 	}
-	for (int i = 1; i <= 2 * N; i++) if (par[i] == 0) DFS(i);	
-	for (int i = 1; i <= N; i++) {
-		if (ID[i * 2] == ID[i * 2 - 1]) {
+	for (int i = 1; i <= 2 * M; i++) if (par[i] == 0) DFS(i);
+	for (int i = 1; i <= M; i++) {
+		if (id[convert(i)] == id[convert(-i)]) {
 			cout << "OTL";
-			return 0;
+			return;
 		}
 	}
 	cout << "^_^";
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
 int convert(int node) {
-	if (node < 0) return -node * 2;
-	else return node * 2 - 1;
+	if (node > 0) return node * 2 - 1;
+	else return -node * 2;
 }
 
 int DFS(int node) {
 	par[node] = ++cnt;
-	int ret = par[node];
-	s.push(node);
+	int ret = cnt;
+	st.push(node);
 	for (int i = 0; i < graph[node].size(); i++) {
 		int next = graph[node][i];
 		if (par[next] == 0) ret = min(ret, DFS(next));
-		else if (mark[next] == 0) ret = min(ret, par[next]);
+		else if (id[next] == 0) ret = min(ret, par[next]);
 	}
 	if (ret == par[node]) {
-		sz++;
+		id_cnt++;
 		while (1) {
-			int t = s.top();
-			s.pop();
-			mark[t] = 1;
-			ID[t] = sz;
+			int t = st.top();
+			st.pop();
+			id[t] = id_cnt;
 			if (t == node) break;
 		}
 	}
