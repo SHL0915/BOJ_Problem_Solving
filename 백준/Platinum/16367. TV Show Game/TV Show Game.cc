7a182119286d1560d10694b00cf6208737804617
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-13 09:30:34 +0900

    [Platinum II] Title: TV Show Game, Time: 8 ms, Memory: 3396 KB -BaekjoonHub

--- 백준/Platinum/16367. TV Show Game/TV Show Game.cc
+++ 백준/Platinum/16367. TV Show Game/TV Show Game.cc
@@ -1,88 +1,94 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 10005;
+using ll = long long;
+using pii = pair<int, int>;
 
-int K, N, cnt, sz;
-vector <int> graph[MAX];
-int par[MAX];
-int ID[MAX];
-int mark[MAX];
-stack <int> s;
+int K, N, cnt, id_cnt;
+vector <int> graph[10001];
+int parent[10001], id[10001], ans[10001];
+vector <vector<int>> SCC;
+stack <int> st;
 
-int convert(int node);
 int DFS(int node);
-bool cmp(int A, int B);
+int convert(int node);
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void solve() {
 	cin >> K >> N;
 	for (int i = 0; i < N; i++) {
-		int nodes[3];
-		for (int j = 0; j < 3; j++) {
-			cin >> nodes[j];
-			string str;
-			cin >> str;
-			if (str == "B") nodes[j] *= -1;
-		}		
-		for (int j = 0; j < 3; j++) {
-			for (int k = j + 1; k < 3; k++) {
-				graph[convert(-nodes[j])].push_back(convert(nodes[k]));
-				graph[convert(-nodes[k])].push_back(convert(nodes[j]));
-			}
-		}
+		int a, b, c;
+		char color;
+		cin >> a >> color;
+		if (color == 'B') a *= -1;
+		cin >> b >> color;
+		if (color == 'B') b *= -1;
+		cin >> c >> color;
+		if (color == 'B') c *= -1;
+		graph[convert(-a)].push_back(convert(b));
+		graph[convert(-a)].push_back(convert(c));
+		graph[convert(-b)].push_back(convert(a));
+		graph[convert(-b)].push_back(convert(c));
+		graph[convert(-c)].push_back(convert(a));
+		graph[convert(-c)].push_back(convert(b));
 	}
-	for (int i = 1; i <= 2 * K; i++) if (par[i] == 0) DFS(i);
+	for (int i = 1; i <= 2 * K; i++) if (parent[i] == 0) DFS(i);
 	for (int i = 1; i <= K; i++) {
-		if (ID[i * 2] == ID[i * 2 - 1]) {
+		if (id[convert(i)] == id[convert(-i)]) {
 			cout << -1;
-			return 0;
+			return;
 		}
 	}
-	vector <int> nodes;
-	int ans[MAX];
 	memset(ans, -1, sizeof(ans));
-	for (int i = 1; i <= 2 * K; i++) nodes.push_back(i);
-	sort(nodes.begin(), nodes.end(), cmp);
-	for (int i = 0; i < nodes.size(); i++) {
-		int now = (nodes[i] + 1) / 2;
-		if (ans[now] != -1) continue;
-		ans[now] = !(nodes[i] % 2);
+	for (int i = SCC.size() - 1; i >= 0; i--) {
+		for (int j = 0; j < SCC[i].size(); j++) {
+			int now = (SCC[i][j] + 1) / 2;
+			if (ans[now] != -1) continue;
+			ans[now] = (SCC[i][j] % 2) ^ 1;
+		}
 	}
 	for (int i = 1; i <= K; i++) {
 		if (ans[i]) cout << "R";
 		else cout << "B";
 	}
+
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
-int convert(int node) {
-	if (node < 0) return -node * 2;
-	else return node * 2 - 1;
-}
-
 int DFS(int node) {
-	par[node] = ++cnt;
-	int ret = par[node];
-	s.push(node);
+	parent[node] = ++cnt;
+	int ret = cnt;
+	st.push(node);
 	for (int i = 0; i < graph[node].size(); i++) {
 		int next = graph[node][i];
-		if (par[next] == 0) ret = min(ret, DFS(next));
-		else if (mark[next] == 0) ret = min(ret, par[next]);
+		if (parent[next] == 0) ret = min(ret, DFS(next));
+		else if (id[next] == 0) ret = min(ret, parent[next]);
 	}
-	if (ret == par[node]) {
-		sz++;
+	if (ret == parent[node]) {
+		id_cnt++;
+		vector <int> v;
 		while (1) {
-			int t = s.top();
-			s.pop();
-			mark[t] = 1;
-			ID[t] = sz;
+			int t = st.top();
+			st.pop();
+			id[t] = id_cnt;
+			v.push_back(t);
 			if (t == node) break;
 		}
+		SCC.push_back(v);
 	}
 	return ret;
 }
 
-bool cmp(int A, int B) {
-	return ID[A] > ID[B];
+int convert(int node) {
+	if (node > 0) return node * 2 - 1;
+	else return -node * 2;
 }
