7360dc9ccef2c544a3e4131bb2439e75e147d3b1
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-13 11:40:08 +0900

    [Gold V] Title: 돌 게임 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9660. 돌 게임 6/돌 게임 6.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+ll N;
+
+void solve() {
+	cin >> N;
+	N %= 7;
+	if (N == 0 || N == 2) cout << "CY";
+	else cout << "SK";
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
