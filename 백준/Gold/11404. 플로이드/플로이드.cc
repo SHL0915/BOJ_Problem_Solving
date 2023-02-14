b02b134dcdae4403df7875b0fbd7ca9fcfcf9075
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-14 11:54:17 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,46 +1,46 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-
-const long long INF = 1000000000000;
+using ll = long long;
+using pii = pair<int, int>;
+const int INF = 0x3f3f3f3f;
 
 int N, M;
-long long dist[101][101];
+int graph[101][101];
 
-void FW();
-
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void solve() {
 	cin >> N >> M;
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			if (i == j) dist[i][j] = 0;
-			else dist[i][j] = INF;
-		}
-	}
+	memset(graph, INF, sizeof(graph));
 	for (int i = 0; i < M; i++) {
-		long long A, B, C;
-		cin >> A >> B >> C;
-		dist[A][B] = min(dist[A][B], C);
+		int a, b, c;
+		cin >> a >> b >> c;
+		graph[a][b] = min(graph[a][b], c);
 	}
-	FW();
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			if (dist[i][j] == INF) cout << "0 ";
-			else cout << dist[i][j] << " ";
-		}
-		cout << '\n';
-	}
-	return 0;
-}
-
-void FW() {
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			for (int k = 1; k <= N; k++) {
-				if (dist[j][k] > dist[j][i] + dist[i][k]) dist[j][k] = dist[j][i] + dist[i][k];
+	for (int k = 1; k <= N; k++) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (i == j) continue;
+				graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
 			}
 		}
 	}
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (graph[i][j] == INF) cout << 0 << " ";
+			else cout << graph[i][j] << " ";
+		}
+		cout << '\n';
+	}
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
 }
