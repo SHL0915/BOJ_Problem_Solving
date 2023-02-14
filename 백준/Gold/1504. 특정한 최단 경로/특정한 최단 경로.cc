8edb3aebbef3f57372df788597970e8b3e088265
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-14 16:11:33 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 64 ms, Memory: 9452 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,91 +1,70 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<ll, ll>;
+const ll INF = 1000000000000000LL;
 
-const long long INF = 10000000000000LL;
+int N, M, u, v;
+vector <pii> graph[801];
+ll dist[801][2];
 
-struct cmp {
-	bool operator () (pair<long long, long long> A, pair<long long, long long> B) {
-		return A.second > B.second;
-	}
-};
+void Dijkstra(int start, int type);
 
-long long N, E, v1, v2, flag, V;
-vector <pair<long long, long long>> graph[801];
-long long val[801];
-
-void Dijkstra(long long start);
-
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> N >> E;
-	for (int i = 0; i < E; i++) {
+void solve() {
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
 		int a, b, c;
 		cin >> a >> b >> c;
-		graph[a].push_back({ b, c });
-		graph[b].push_back({ a, c });
+		graph[a].push_back({ b,c });
+		graph[b].push_back({ a,c });
 	}
-	cin >> v1 >> v2;
-	Dijkstra(v1);
-	if (val[v2] == INF) {
-		cout << -1;
-		return 0;
+	Dijkstra(1, 0);
+	Dijkstra(N, 1);
+	cin >> u >> v;
+	ll sum = INF;
+	for (int i = 0; i < graph[u].size(); i++) {
+		if (graph[u][i].first == v) sum = min(sum, graph[u][i].second);
 	}
-	V = val[v2];
-	Dijkstra(1);
-	long long A = val[v1];
-	long long B = val[v2];
-	if (A == INF && B == INF) {
-		cout << -1;
-		return 0;
-	}
-	else if (A == INF) flag = 1;
-	else if (B == INF) flag = 2;
-	if (flag == 0) {
-		Dijkstra(v2);
-		A += val[N];
-		long long temp = val[N];
-		Dijkstra(v1);
-		B += val[N];
-		if (temp == INF && val[N] == INF) {
-			cout << -1;
-			return 0;
-		}
-		else if (temp == INF) cout << B + V;
-		else if (val[N] == INF) cout << A + V;
-		else cout << min(B + V, A + V);
-	}
-	else if (flag == 1) {
-		Dijkstra(v1);
-		B += val[N];
-		if (val[N] == INF) cout << -1;
-		else cout << B + V;
-	}
-	else {
-		Dijkstra(v2);
-		A += val[N];
-		if (val[N] == INF) cout << -1;
-		else cout << A + V;
-	}
+	ll ans = INF;
+	ans = min(ans, dist[u][0] + dist[v][1]);
+	ans = min(ans, dist[v][0] + dist[u][1]);
+	Dijkstra(u, 0);
+	ans += dist[v][0];
+	if (ans >= INF) cout << -1;
+	else cout << ans;
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
-void Dijkstra(long long start) {
-	priority_queue <pair<long long, long long>, vector<pair<long long, long long>>, cmp> pq;
-	for (int i = 1; i <= N; i++) val[i] = INF;
-	pq.push({ start, 0 });
-	val[start] = 0;
+void Dijkstra(int start, int type) {
+	for (int i = 1; i <= N; i++) dist[i][type] = INF;
+	priority_queue <pii, vector<pii>, greater<>> pq;
+	dist[start][type] = 0;
+	pq.push({ 0, start });
 	while (pq.size()) {
-		long long idx = pq.top().first;
-		long long value = pq.top().second;
+		int now = pq.top().second;
+		ll val = pq.top().first;
 		pq.pop();
-		for (int i = 0; i < graph[idx].size(); i++) {
-			if (val[graph[idx][i].first] > val[idx] + graph[idx][i].second) {
-				val[graph[idx][i].first] = val[idx] + graph[idx][i].second;
-				pq.push({ graph[idx][i].first, val[graph[idx][i].first] });
+		if (dist[now][type] < val) continue;
+		for (int i = 0; i < graph[now].size(); i++) {
+			int next = graph[now][i].first;
+			ll next_val = graph[now][i].second + val;
+			if (dist[next][type] > next_val) {
+				dist[next][type] = next_val;
+				pq.push({ next_val, next });
 			}
 		}
 	}
+	return;
 }
