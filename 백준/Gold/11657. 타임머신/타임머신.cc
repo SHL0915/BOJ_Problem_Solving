ae5ed5ad4a256afec1d4e8b25f987b85795ac0e6
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-15 14:42:09 +0900

    [Gold IV] Title: 타임머신, Time: 8 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -1,50 +1,63 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-
-const long long INF = 1000000000000;
+using ll = long long;
+using pii = pair<int, int>;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 
 int N, M;
-vector <pair<pair<int, int>, int>> graph;
-long long dist[501];
+vector <pair<ll, pii>> graph;
+ll dist[501];
 
-void BF(int start);
+void Bellman_Ford(int start);
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+void solve() {
 	cin >> N >> M;
 	for (int i = 0; i < M; i++) {
-		int A, B, C;
-		cin >> A >> B >> C;
-		graph.push_back({ {A,B},C });
+		int a, b, c;
+		cin >> a >> b >> c;
+		graph.push_back({ c, {a,b} });
 	}
-	BF(1);
-	for (int i = 0; i < N - 1; i++) {
-		for (int i = 0; i < M; i++) {
-			if (dist[graph[i].first.first] == INF) continue;
-			else if (dist[graph[i].first.second] > dist[graph[i].first.first] + graph[i].second) {
-				cout << -1;
-				return 0;
-			}
+	Bellman_Ford(1);
+	for (int i = 0; i < M; i++) {
+		int u = graph[i].second.first;
+		int v = graph[i].second.second;
+		ll val = graph[i].first;
+		if (dist[u] == INF) continue;
+		if (dist[v] > dist[u] + val) {
+			cout << -1;
+			return;
 		}
 	}
 	for (int i = 2; i <= N; i++) {
 		if (dist[i] == INF) cout << -1 << '\n';
 		else cout << dist[i] << '\n';
 	}
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
 
-void BF(int start) {
-	for (int i = 1; i <= N; i++) dist[i] = INF;
+void Bellman_Ford(int start) {
+	memset(dist, INF, sizeof(dist));
+	dist[start] = 0;
 	for (int i = 0; i < N - 1; i++) {
-		dist[start] = 0;
-		for (int i = 0; i < M; i++) {
-			if (dist[graph[i].first.first] == INF) continue;
-			else if (dist[graph[i].first.second] > dist[graph[i].first.first] + graph[i].second) {
-				dist[graph[i].first.second] = dist[graph[i].first.first] + graph[i].second;
-			}
-		}
+		for (int j = 0; j < graph.size(); j++) {
+			int u = graph[j].second.first;
+			int v = graph[j].second.second;
+			ll val = graph[j].first;
+			if (dist[u] == INF) continue;
+			if (dist[v] > dist[u] + val) dist[v] = dist[u] + val;			
+ 		}
 	}
+	return;
 }
