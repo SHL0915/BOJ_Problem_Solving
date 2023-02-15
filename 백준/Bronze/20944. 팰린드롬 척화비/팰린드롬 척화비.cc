9da140b446f8fb0500016336ccadb13b426178ec
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-15 19:36:44 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int N;
+
+void solve() {
+	cin >> N;
+	for (int i = 0; i < N; i++) cout << "a";
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
