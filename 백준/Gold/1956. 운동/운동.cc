3b9433a72f079f87287a0c11edee0e4260b31c33
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-15 15:26:05 +0900

    [Gold IV] Title: 운동, Time: 100 ms, Memory: 2648 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.cc
+++ 백준/Gold/1956. 운동/운동.cc
@@ -1,45 +1,45 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int INF = 0x3f3f3f3f;
 
-const long long INF = 1000000000000;
+int N, M;
+int graph[401][401];
 
-long long V, E;
-long long dist[401][401];
-long long ans = INF;
-
-void FW();
-
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> V >> E;
-	for (int i = 1; i <= V; i++) {
-		for (int j = 1; j <= V; j++) dist[i][j] = INF;
+void solve() {
+	cin >> N >> M;
+	memset(graph, INF, sizeof(graph));
+	for (int i = 0; i < M; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		graph[a][b] = c;
 	}
-	for (int i = 0; i < E; i++) {
-		long long A, B, C;
-		cin >> A >> B >> C;
-		dist[A][B] = min(dist[A][B], C);
+	for (int k = 1; k <= N; k++) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+		}
 	}
-	FW();
-	for (int i = 1; i <= V; i++) {
-		for (int j = 1; j <= V; j++) {
-			if(i == j) ans = min(ans, dist[i][j]);
-			else ans = min(ans, dist[i][j] + dist[j][i]);
+	int ans = INF;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (j == i) continue;
+			ans = min(ans, graph[i][j] + graph[j][i]);
 		}
 	}
 	if (ans == INF) cout << -1;
 	else cout << ans;
-	return 0;
+	return;
 }
 
-void FW() {
-	for (int i = 1; i <= V; i++) {
-		for (int j = 1; j <= V; j++) {
-			for (int k = 1; k <= V; k++) {
-				dist[j][k] = min(dist[j][k], dist[j][i] + dist[i][k]);
-			}
-		}
-	}
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
 }
