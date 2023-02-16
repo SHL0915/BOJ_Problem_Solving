9e38b2fbf635573af545ab58c84443aab55082c8
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-16 14:23:12 +0900

    [Platinum IV] Title: 축사 배정, Time: 12 ms, Memory: 2692 KB -BaekjoonHub

--- 백준/Platinum/2188. 축사 배정/축사 배정.cc
+++ 백준/Platinum/2188. 축사 배정/축사 배정.cc
@@ -2,79 +2,78 @@
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
-const int INF = 0x3f3f3f3f;
+
+struct edge { int dist, cap, rev; };
+vector <edge> graph[405];
 
 int N, M;
-vector <int> graph[405];
-int cap[405][405];
-int flow[405][405];
-int mark[405];
 
-int maxFlow(int s, int e);
+void add_edge(int v, int u);
+int find_max(int s, int e);
 
 void solve() {
 	cin >> N >> M;
+	for (int i = 1; i <= N; i++) add_edge(0, i);
+	for (int i = 1; i <= M; i++) add_edge(i + N, N + M + 1);
 	for (int i = 1; i <= N; i++) {
 		int k; cin >> k;
 		for (int j = 0; j < k; j++) {
 			int a; cin >> a;
-			a += N;
-			graph[i].push_back(a);
-			graph[a].push_back(i);
-			cap[i][a] = 1;
+			add_edge(i, a + N);
 		}
 	}
-	for (int i = 1; i <= N; i++) {
-		graph[0].push_back(i);
-		graph[i].push_back(0);
-		cap[0][i] = 1;
-	}
-	for (int i = N + 1; i <= N + M; i++) {
-		graph[i].push_back(N + M + 1);
-		graph[N + M + 1].push_back(i);
-		cap[i][N + M + 1] = 1;
-	}
-	cout << maxFlow(0, N + M + 1);
+	cout << find_max(0, N + M + 1);
 	return;
 }
 
 int main(void) {
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-#endif
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int t = 1;
-	//cin >> t;
-	while (t--) solve();
-	return 0;
+		freopen("input.txt", "r", stdin);
+	#endif
+		ios::sync_with_stdio(false);
+		cin.tie(0);
+		int t = 1;
+		//cin >> t;
+		while (t--) solve();
+		return 0;
 }
 
-int maxFlow(int s, int e) {
+void add_edge(int v, int u) {
+	edge x;
+	x.dist = u; x.cap = 1; x.rev = graph[u].size();
+	graph[v].push_back(x);
+	x.dist = v; x.cap = 0; x.rev = graph[v].size() - 1;
+	graph[u].push_back(x);
+}
+
+int find_max(int s, int e) {
 	int ret = 0;
 	while (1) {
-		memset(mark, -1, sizeof(mark));
-		queue <int > q;
+		queue <int> q;
+		int v[405] = {}, pa[405] = {}, pe[405] = {};
 		q.push(s);
+		v[s] = 1;
 		while (q.size()) {
-			int now = q.front(); q.pop();
-			for (int i = 0; i < graph[now].size(); i++) {
-				int next = graph[now][i];
-				if (cap[now][next] - flow[now][next] > 0 && mark[next] == -1) {
-					q.push(next);
-					mark[next] = now;
-					if (next == e) break;
+			int f = q.front(); q.pop();
+			if (f == e) break;
+			for (int i = 0; i < graph[f].size(); i++) {
+				int now = graph[f][i].dist;
+				int cap = graph[f][i].cap;
+				if (v[now] == 0 && cap > 0) {
+					q.push(now);
+					pa[now] = f;
+					pe[now] = i;
+					v[now] = 1;
 				}
 			}
 		}
-		if (mark[e] == -1) break;
-		int val = INF;
-		for (int i = e; i != s; i = mark[i]) val = min(val, cap[mark[i]][i] - flow[mark[i]][i]);
-		for (int i = e; i != s; i = mark[i]) {
-			flow[mark[i]][i] += val;
-			flow[i][mark[i]] -= val;
+		if (v[e] == 0) break;
+		ret++;
+		for (int i = e; i != s; i = pa[i]) {
+			int rev = graph[pa[i]][pe[i]].rev;
+			graph[pa[i]][pe[i]].cap--;
+			graph[i][rev].cap++;
 		}
-		ret += val;
 	}
 	return ret;
 }
