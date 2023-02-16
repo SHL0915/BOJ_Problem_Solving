6f0da0a9ff90902cb07eb49366da048626175474
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-16 15:03:04 +0900

    [Platinum IV] Title: 도시 왕복하기 1, Time: 8 ms, Memory: 2560 KB -BaekjoonHub

---
+++ 백준/Platinum/17412. 도시 왕복하기 1/도시 왕복하기 1.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+struct edge { int dist, cap, rev; };
+
+int N, P;
+vector <edge> graph[405];
+
+void add_edge(int u, int v);
+int find_Max(int s, int e);
+
+void solve() {
+	cin >> N >> P;
+	for (int i = 0; i < P; i++) {
+		int a, b;
+		cin >> a >> b;
+		add_edge(a, b);
+	}
+	cout << find_Max(1, 2);
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
+
+void add_edge(int u, int v) {
+	edge x;
+	x.dist = v, x.cap = 1, x.rev = graph[v].size();
+	graph[u].push_back(x);
+	x.dist = u, x.cap = 0, x.rev = graph[u].size() - 1;
+	graph[v].push_back(x);
+	return;
+}
+
+int find_Max(int s, int e) {
+	int ret = 0;
+	while (1) {
+		queue <int> q;
+		int parent[405], pe[405];
+		memset(parent, -1, sizeof(parent));
+		memset(pe, 0, sizeof(pe));
+		q.push(s);
+		while (q.size()) {
+			int f = q.front(); q.pop();
+			if (f == e) break;
+			for (int i = 0; i < graph[f].size(); i++) {
+				int now = graph[f][i].dist;
+				int cap = graph[f][i].cap;
+				if (parent[now] == -1 && cap > 0) {
+					q.push(now);
+					parent[now] = f;
+					pe[now] = i;
+				}
+			}
+		}
+		if (parent[e] == -1) break;
+		int val = 0x3f3f3f3f;
+		for (int i = e; i != s; i = parent[i]) val = min(val, graph[parent[i]][pe[i]].cap);
+		for (int i = e; i != s; i = parent[i]) {
+			int rev = graph[parent[i]][pe[i]].rev;
+			graph[parent[i]][pe[i]].cap -= val;
+			graph[i][rev].cap += val;
+		}
+		ret += val;
+	}
+	return ret;
+}
