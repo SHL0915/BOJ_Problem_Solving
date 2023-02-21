d2723561953294c0bc686d857373d7d440f8f1cb
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-21 09:51:59 +0900

    [Gold III] Title: 줄 세우기, Time: 108 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/7570. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.cc
@@ -1,26 +1,32 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
-int N, maxCnt, cnt = 1;
-int children[1000000];
-int key[1000001];
+int N, ans;
+int arr[1000001];
+int table[1000001];
+
+void solve() {
+	cin >> N;
+	for (int i = 0; i < N; i++) cin >> arr[i];
+	for (int i = 0; i < N; i++) {
+		int now = arr[i];
+		table[now] = max(1, table[now - 1] + 1);
+		ans = max(ans, table[now]);
+	}
+	cout << N - ans;
+	return;
+}
 
 int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> children[i];
-		key[children[i]] = i;
-	}
-	for (int i = N; i >= 2; i--) {
-		if (key[i] > key[i - 1]) cnt++;
-		else {
-			maxCnt = max(maxCnt, cnt);
-			cnt = 1;
-		}
-	}
-	cout << N - maxCnt;
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
 	return 0;
 }
