fe56df89c98b4ab7034f1dd533f948b31c9e89be
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-21 09:14:04 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int N, M;
+int arr[101];
+
+void solve() {
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) arr[i] = i;
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+		swap(arr[a], arr[b]);
+	}
+	for (int i = 1; i <= N; i++) cout << arr[i] << " ";
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
