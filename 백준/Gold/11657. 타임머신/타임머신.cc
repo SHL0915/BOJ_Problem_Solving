7a1b0dfd63e9ee341a9b92f865d35fb42327ad6e
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-21 14:03:51 +0900

    [Gold IV] Title: 타임머신, Time: 8 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.cc
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -5,29 +5,50 @@ using pii = pair<int, int>;
 const ll INF = 0x3f3f3f3f3f3f3f3fLL;
 
 int N, M;
-vector <pair<ll, pii>> graph;
-ll dist[501];
-
-void Bellman_Ford(int start);
+vector <pii> graph[501];
+ll dist[501], mark[501], cycle[501];
 
 void solve() {
 	cin >> N >> M;
 	for (int i = 0; i < M; i++) {
 		int a, b, c;
 		cin >> a >> b >> c;
-		graph.push_back({ c, {a,b} });
+		graph[a].push_back({ b,c });
 	}
-	Bellman_Ford(1);
-	for (int i = 0; i < M; i++) {
-		int u = graph[i].second.first;
-		int v = graph[i].second.second;
-		ll val = graph[i].first;
-		if (dist[u] == INF) continue;
-		if (dist[v] > dist[u] + val) {
-			cout << -1;
-			return;
+
+	memset(dist, INF, sizeof(dist));
+	
+	queue <int> q;
+	dist[1] = 0;
+	mark[1] = 1;
+	cycle[1] = 1;
+
+	q.push(1);
+	while (q.size()) {
+		int now = q.front(); q.pop();
+
+		mark[now] = 0;
+		for (int i = 0; i < graph[now].size(); i++) {
+			int next = graph[now][i].first;
+			int val = graph[now][i].second;
+
+			if (dist[next] > dist[now] + val) {
+				dist[next] = dist[now] + val;
+				if (mark[next] == 0) {
+					cycle[next]++;
+					
+					if (cycle[next] >= N) {
+						cout << -1;
+						return;
+					}
+
+					q.push(next);
+					mark[next] = 1;
+				}
+			}
 		}
 	}
+
 	for (int i = 2; i <= N; i++) {
 		if (dist[i] == INF) cout << -1 << '\n';
 		else cout << dist[i] << '\n';
@@ -45,19 +66,4 @@ int main(void) {
 	//cin >> t;
 	while (t--) solve();
 	return 0;
-}
-
-void Bellman_Ford(int start) {
-	memset(dist, INF, sizeof(dist));
-	dist[start] = 0;
-	for (int i = 0; i < N - 1; i++) {
-		for (int j = 0; j < graph.size(); j++) {
-			int u = graph[j].second.first;
-			int v = graph[j].second.second;
-			ll val = graph[j].first;
-			if (dist[u] == INF) continue;
-			if (dist[v] > dist[u] + val) dist[v] = dist[u] + val;			
- 		}
-	}
-	return;
 }
