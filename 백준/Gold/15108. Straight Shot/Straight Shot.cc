28beb9f025cac477e1200fee713de5050867fa8c
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-02-24 18:20:12 +0900

    [Gold IV] Title: Straight Shot, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/15108. Straight Shot/Straight Shot.cc
+++ 백준/Gold/15108. Straight Shot/Straight Shot.cc
@@ -8,46 +8,46 @@ double X, V;
 pii arr[101];
 
 void solve() {
-	cin >> N >> X >> V;
-	for (int i = 0; i < N; i++) {
-		int l, r;
-		double v;
-		cin >> l >> r >> v;
-		arr[i] = { r - l, v };
-	}
-	double sum = 0;
-	for (int i = 0; i < N; i++) sum += (arr[i].first * arr[i].second);
-	
-	sum /= X;
-	sum *= sum;
+   cin >> N >> X >> V;
+   for (int i = 0; i < N; i++) {
+      int l, r;
+      double v;
+      cin >> l >> r >> v;
+      arr[i] = { r - l, v };
+   }
+   double sum = 0;
+   for (int i = 0; i < N; i++) sum += (arr[i].first * arr[i].second);
+   
+   sum /= X;
+   sum *= sum;
 
-	double dx2 = V * V - sum;
+   double dx2 = V * V - sum;
 
-	if (dx2 <= 0) {
-		cout << "Too hard";
-		return;
-	}
+   if (dx2 <= 0) {
+      cout << "Too hard";
+      return;
+   }
 
-	dx2 = sqrt(dx2);
+   dx2 = sqrt(dx2);
 
-	if (dx2 < V / 2) {
-		cout << "Too hard";
-		return;
-	}
+   if (dx2 < V / 2) {
+      cout << "Too hard";
+      return;
+   }
 
-	cout << X / dx2 << '\n';
+   cout << X / dx2 << '\n';
 }
 
 int main(void) {
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
+   freopen("input.txt", "r", stdin);
 #endif
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout << fixed;
-	cout.precision(3);
-	int t = 1;
-	//cin >> t;
-	while (t--) solve();
-	return 0;
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+   cout << fixed;
+   cout.precision(3);
+   int t = 1;
+   //cin >> t;
+   while (t--) solve();
+   return 0;
 }
