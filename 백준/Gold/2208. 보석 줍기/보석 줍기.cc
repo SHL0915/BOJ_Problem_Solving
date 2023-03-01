3cef4ab41ad21d08d3693b374c3cac4e56ca444f
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-03-01 17:03:56 +0900

    [Gold II] Title: 보석 줍기, Time: 8 ms, Memory: 3584 KB -BaekjoonHub

---
+++ 백준/Gold/2208. 보석 줍기/보석 줍기.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const ll INF = 0x3f3f3f3f3f3f3f3fLL;
+
+int N, M;
+ll arr[100001];
+ll psum[100001];
+
+void solve() {
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i];
+		psum[i] = psum[i - 1] + arr[i];
+	}
+
+	ll m = INF, ans = 0;
+	for (int i = M; i <= N; i++) {
+		m = min(m, psum[i - M]);
+		ans = max(ans, psum[i] - m);
+	}
+
+	cout << ans;
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
