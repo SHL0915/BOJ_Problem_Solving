15a1ad250e871984afd2600f2728fc1681d87faf
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-03-04 14:28:39 +0900

    [Silver I] Title: Angry Cows (Silver), Time: 8 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/11973. Angry Cows （Silver）/Angry Cows （Silver）.cc
+++ 백준/Silver/11973. Angry Cows （Silver）/Angry Cows （Silver）.cc
@@ -1,62 +1,51 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
+	#include <bits/stdc++.h>
+	using namespace std;
+	using ll = long long;
+	using pii = pair<int, int>;
 
-int N, K;
-int arr[50001];
+	int N, K;
+	int arr[50001];
 
-ll BinarySearch(ll left, ll right);
+	ll BinarySearch(ll left, ll right);
 
-void solve() {
-	cin >> N >> K;
-	for (int i = 0; i < N; i++) cin >> arr[i];
+	void solve() {
+		cin >> N >> K;
+		for (int i = 0; i < N; i++) cin >> arr[i];
 
-	sort(arr, arr + N);
+		sort(arr, arr + N);
 
-	ll ans = BinarySearch(0, 1000000005L);
+		ll ans = BinarySearch(0, 1000000005L);
 
-	int cnt = 1, prev = arr[0];
-
-	for (int i = 0; i < N; i++) {
-		if (arr[i] > prev + 2 * ans) {
-			cnt++;
-			prev = arr[i];
-		}
+		cout << ans;
+		return;
 	}
 
-	if (cnt > K) ans++;
+	int main(void) {
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+	#endif
+		ios::sync_with_stdio(false);
+		cin.tie(0);
+		int t = 1;
+		//cin >> t;
+		while (t--) solve();
+		return 0;
+	}
 
-	cout << ans;
-	return;
-}
+	ll BinarySearch(ll left, ll right) {
+		if (left <= right) {
+			ll mid = (left + right) / 2;
+			int cnt = 1, prev = arr[0];
 
-int main(void) {
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-#endif
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int t = 1;
-	//cin >> t;
-	while (t--) solve();
-	return 0;
-}
-
-ll BinarySearch(ll left, ll right) {
-	if (left < right) {
-		ll mid = (left + right) / 2;
-		int cnt = 1, prev = arr[0];
-
-		for (int i = 0; i < N; i++) {
-			if (arr[i] > prev + 2 * mid) {
-				cnt++;
-				prev = arr[i];
+			for (int i = 0; i < N; i++) {
+				if (arr[i] > prev + 2 * mid) {
+					cnt++;
+					prev = arr[i];
+				}
 			}
-		}
 
-		if (cnt > K) return BinarySearch(mid + 1, right);
-		else return BinarySearch(left, mid - 1);
+			if (cnt > K) return BinarySearch(mid + 1, right);
+			else return BinarySearch(left, mid - 1);
+		}
+		else return left;
 	}
-	else return left;
-}
