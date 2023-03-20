2cb67bd1d2ed77271c77928353ce4a40147778ad
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-03-20 14:12:21 +0900

    [Platinum III] Title: Xor Maximization, Time: 52 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Platinum/11191. Xor Maximization/Xor Maximization.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int N;
+ll arr[100001];
+ll basis[64];
+
+void solve() {
+	cin >> N;
+	for (int i = 1; i <= N; i++) cin >> arr[i];
+	for (int i = 1; i <= N; i++) {
+		for (int j = 63; j >= 0; j--) {
+			if (arr[i] & (1LL << j)) {
+				if (basis[j] == 0) {
+					basis[j] = arr[i];
+					break;
+				}
+				else arr[i] ^= basis[j];
+			}
+		}
+	}
+
+	ll ans = 0;
+
+	for (int i = 63; i >= 0; i--) ans = max(ans, ans ^ basis[i]);
+
+	cout << ans;
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
