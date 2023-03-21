a6303a1a333ba7c7fad956f483afb60e500ade5c
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-03-21 17:50:59 +0900

    [Bronze V] Title: 長方形 (Rectangle), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 長方形 （Rectangle）/長方形 （Rectangle）.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int A, B;
+
+void solve() {
+	cin >> A >> B;
+	cout << A * B;
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
