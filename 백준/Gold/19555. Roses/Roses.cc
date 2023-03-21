876b73dc63a7013f477b20af86871a4cc3a4bd6d
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-03-21 14:15:54 +0900

    [Gold V] Title: Roses, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/19555. Roses/Roses.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+ll N, A, B, C, D;
+
+ll GCD(ll a, ll b);
+
+void solve() {
+	cin >> N >> A >> B >> C >> D;
+	ll ans = 0;
+	if (B * C > A * D) {
+		swap(A, C);
+		swap(B, D);
+	}
+
+	ans += (N / A) * B;	
+	if (N % A) ans += B;
+
+	ll cmp = A / GCD(A, C);
+
+	for (ll i = 1; i < cmp; i++) {
+		ll val = i * D;
+		ll num = i * C;
+		if (N > num) {
+			val += (N - num) / A * B;
+			if ((N - num) % A) val += B;
+		}
+		ans = min(ans, val);
+	}	
+	
+	cout << ans;
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
+
+ll GCD(ll a, ll b) {
+	if (a > b) {
+		if (a % b) return GCD(a % b, b);
+		else return b;
+	}
+	else {
+		if (b % a) return GCD(b % a, a);
+		else return a;
+	}
+}
