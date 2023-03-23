248ddf5c92c2ae6111285c31f82eaf4bf6be4d9c
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-03-23 10:30:19 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 28 ms, Memory: 2236 KB -BaekjoonHub

--- 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.cc
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.cc
@@ -12,26 +12,19 @@ void solve() {
 	for (int i = 0; i < N; i++) {
 		ll v, c, k;
 		cin >> v >> c >> k;
-		while (1) {
-			ll B = 1;
-			ll bit = 1;
-			while (B <= k) B *= 2;
-			B /= 2; B--;
-
-			k -= B;
-			while (B) {
-				if (B & bit) {
-					arr.push_back({ v * bit, c * bit });
-					B -= bit;
-				}
-				bit <<= 1;
+		ll B = 1, bit = 1;
+		while (B <= k) B *= 2;
+		B /= 2; B--;
+		
+		while (B) {
+			if (B & bit) {
+				arr.push_back({ v * bit, c * bit });
+				k -= bit;
+				B -= bit;
 			}
-			
-			if (k == 1) {
-				arr.push_back({ v,c });
-				break;
-			}
+			bit <<= 1;
 		}
+		arr.push_back({ v * k, c * k });
 	}
 
 	ll ans = 0;
