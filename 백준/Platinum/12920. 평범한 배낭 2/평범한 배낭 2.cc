7f463e37399228d9f3f42615934b9a4faad5b392
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-03-23 10:27:15 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 96 ms, Memory: 2368 KB -BaekjoonHub

---
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<ll, ll>;
+
+int N, M;
+vector <pii> arr;
+ll table[10001];
+
+void solve() {
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		ll v, c, k;
+		cin >> v >> c >> k;
+		while (1) {
+			ll B = 1;
+			ll bit = 1;
+			while (B <= k) B *= 2;
+			B /= 2; B--;
+
+			k -= B;
+			while (B) {
+				if (B & bit) {
+					arr.push_back({ v * bit, c * bit });
+					B -= bit;
+				}
+				bit <<= 1;
+			}
+			
+			if (k == 1) {
+				arr.push_back({ v,c });
+				break;
+			}
+		}
+	}
+
+	ll ans = 0;
+
+	for (int i = 0; i < arr.size(); i++) {
+		for (int j = M; j >= 0; j--) {
+			if (j < arr[i].first) continue;
+			table[j] = max(table[j], arr[i].second + table[j - arr[i].first]);
+			ans = max(ans, table[j]);
+		}
+	}
+
+	cout << ans;
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
