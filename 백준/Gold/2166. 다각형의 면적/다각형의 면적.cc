99c6444297fa1ccd50e586fc4a9c5e3dcd98350a
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-04-06 14:47:46 +0900

    [Gold V] Title: 다각형의 면적, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -1,33 +1,32 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<ll, ll>;
 
 int N;
-double ans;
-pair<long long,long long> arr[10000];
+pii arr[10001];
 
-double absol(double A){
-  if(A >= 0) return A;
-  else return -1 * A;
+void solve() {
+	cin >> N;
+	for (int i = 0; i < N; i++) cin >> arr[i].first >> arr[i].second;
+	arr[N] = arr[0];
+	double ans = 0;
+
+	for (int i = 1; i <= N; i++) ans += (arr[i].first - arr[i - 1].first) * (arr[i].second + arr[i - 1].second);
+	cout << abs(ans) / 2;
+	return;
 }
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  cout << fixed;
-  cout.precision(1);
-  cin >> N;
-  for(int i = 0; i < N; i++){
-    long long x, y;
-    cin >> x >> y;
-    arr[i] = {x,y};
-  }
-  long long temp = 0;
-  for(int i = 0; i < N - 1; i++) temp += (arr[i].first * arr[i+1].second);
-  temp += (arr[N-1].first * arr[0].second);
-  for(int i = 0; i < N - 1; i++) temp -= (arr[i].second * arr[i+1].first);
-  temp-= (arr[N-1].second * arr[0].first);
-  ans = absol((double)temp / 2);
-  cout << ans;  
-  return 0;
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout << fixed;
+	cout.precision(1);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
 }
