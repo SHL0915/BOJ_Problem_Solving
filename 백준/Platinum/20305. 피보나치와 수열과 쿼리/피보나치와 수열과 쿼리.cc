fa4856df84caf7cacd6562bed7e0a6da0758075c
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-04-18 10:52:38 +0900

    [Platinum IV] Title: 피보나치와 수열과 쿼리, Time: 148 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Platinum/20305. 피보나치와 수열과 쿼리/피보나치와 수열과 쿼리.cc
+++ 백준/Platinum/20305. 피보나치와 수열과 쿼리/피보나치와 수열과 쿼리.cc
@@ -2,23 +2,35 @@
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
-const ll mod = 1000000007;
+const int mod = 1000000007;
 
 int N, Q;
-ll fibo[1000001];
-ll ans[1000005];
+int fibo[1000005];
+int ans[1000005];
 
 void solve() {
 	cin >> N >> Q;
 	for (int i = 0; i < Q; i++) {
 		int l, r;
 		cin >> l >> r;
-		ans[l] += fibo[1]; ans[l] %= mod;
-		ans[r + 1] -= fibo[r - l + 2]; ans[r + 1] += mod; ans[r + 1] %= mod;
-		ans[r + 2] -= fibo[r - l + 1]; ans[r + 2] += mod; ans[r + 2] %= mod;
+		ans[l]++;
+		if (ans[l] >= mod) ans[l] -= mod;
+
+		ans[r + 1] -= fibo[r - l + 2];
+		if (ans[r + 1] < 0) ans[r + 1] += mod;
+		else if (ans[r + 1] >= mod) ans[r + 1] -= mod;
+
+		ans[r + 2] -= fibo[r - l + 1];
+		if (ans[r + 2] < 0) ans[r + 2] += mod;
+		else if (ans[r + 2] >= mod) ans[r + 2] -= mod;
 	}
 
-	for (int i = 2; i <= N; i++) ans[i] = (ans[i] + ans[i - 1] + ans[i - 2]) % mod;
+	for (int i = 2; i <= N; i++) {
+		ans[i] += ans[i - 1];
+		if (ans[i] >= mod) ans[i] -= mod;
+		ans[i] += ans[i - 2];
+		if (ans[i] >= mod) ans[i] -= mod;
+	}
 
 	for (int i = 1; i <= N; i++) cout << ans[i] << " ";
 	return;
@@ -31,7 +43,10 @@ int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	fibo[0] = 0; fibo[1] = 1;
-	for (int i = 2; i <= 1000000; i++) fibo[i] = (fibo[i - 1] + fibo[i - 2]) % mod;
+	for (int i = 2; i < 1000005; i++) {
+		fibo[i] = (fibo[i - 1] + fibo[i - 2]);
+		if (fibo[i] >= mod) fibo[i] -= mod;
+	}
 	int t = 1;
 	//cin >> t;
 	while (t--) solve();
