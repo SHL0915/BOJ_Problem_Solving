84d14943b16e3d493800caa296557dee77c31e41
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-04-18 09:14:17 +0900

    [Platinum IV] Title: 피보나치와 수열과 쿼리, Time: 156 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Platinum/20305. 피보나치와 수열과 쿼리/피보나치와 수열과 쿼리.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const ll mod = 1000000007;
+
+int N, Q;
+ll fibo[1000001];
+ll ans[1000005];
+
+void solve() {
+	cin >> N >> Q;
+	for (int i = 0; i < Q; i++) {
+		int l, r;
+		cin >> l >> r;
+		ans[l] += fibo[1]; ans[l] %= mod;
+		ans[r + 1] -= fibo[r - l + 2]; ans[r + 1] += mod; ans[r + 1] %= mod;
+		ans[r + 2] -= fibo[r - l + 1]; ans[r + 2] += mod; ans[r + 2] %= mod;
+	}
+
+	for (int i = 2; i <= N; i++) ans[i] = (ans[i] + ans[i - 1] + ans[i - 2]) % mod;
+
+	for (int i = 1; i <= N; i++) cout << ans[i] << " ";
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	fibo[0] = 0; fibo[1] = 1;
+	for (int i = 2; i <= 1000000; i++) fibo[i] = (fibo[i - 1] + fibo[i - 2]) % mod;
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
