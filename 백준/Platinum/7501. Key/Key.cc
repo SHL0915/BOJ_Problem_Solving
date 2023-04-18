18099d6bc697d3dee1baa61e7e9674c183c05b7b
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-04-18 17:48:18 +0900

    [Platinum I] Title: Key, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Platinum/7501. Key/Key.cc
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef __int128 lli;
+using ll = unsigned long long;
+using pii = pair<int, int>;
+
+ll A, B;
+int sieve[100];
+vector <ll> check;
+
+ll power(ll a, ll k, ll mod);
+int miller(ll n, ll a);
+
+void solve() {
+	cin >> A >> B;
+	for (ll i = A; i <= B; i++) {
+		if (i % 2 == 0) continue;
+
+		int flag = 0;
+		for (int j = 0; j < check.size(); j++) {
+			if (miller(i, check[j]) == 0) flag = 1;			
+		}
+
+		if (flag == 0 || i == 9) cout << i << " ";
+	}
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	for (int i = 2; i <= 37; i++) {
+		if (sieve[i] == 0) {
+			check.push_back(i);
+			for (int j = i * i; j <= 37; j += i) sieve[j] = 1;
+		}
+	}
+
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
+
+ll power(ll a, ll k, ll mod) {
+	ll ret = 1;
+	a %= mod;
+	while (k) {
+		if (k & 1) ret = ((lli)ret * (lli)a) % mod;
+		a = ((lli)a * (lli)a) % mod;
+		k >>= 1;
+	}
+	return ret;
+}
+
+int miller(ll n, ll a) {
+	if (a % n == 0) return 1;
+	ll k = n - 1;
+	while (1) {
+		ll temp = power(a, k, n);
+		if (temp == n - 1) return 1;
+		else {
+			if (k % 2) return (temp == 1);
+			k >>= 1;
+		}
+	}
+}
