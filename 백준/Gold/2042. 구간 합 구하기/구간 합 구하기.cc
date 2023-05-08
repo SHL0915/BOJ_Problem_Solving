d80e4def64deb205834374c9f3802ef917390b8d
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-05-08 10:07:45 +0900

    [Gold I] Title: 구간 합 구하기, Time: 236 ms, Memory: 33272 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,65 +1,49 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
 
-long long N, M, K;
-vector <long long> arr;
-vector <long long> tree;
+int N, M, K;
+vector <ll> tree;
 
-void CreateSegTree(int node, int start, int end);
-long long Calculate(int node, int start, int end, int left, int right);
-void Update(int node, int start, int end, int idx, long long val);
+void update(int pos, ll val);
+ll query(int left, int right);
+
+void solve() {
+	cin >> N >> M >> K;
+	tree.resize(4 * N);
+	for (int i = 1; i <= N; i++) {
+		ll a; cin >> a;
+		update(i, a);
+	}
+	for (int i = 0; i < M + K; i++) {
+		ll a, b, c;
+		cin >> a >> b >> c;
+		if (a == 1) update(b, c);
+		else cout << query(b, c + 1) << '\n';
+	}
+	return;
+} 
 
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N >> M >> K;
-	for (int i = 0; i < N; i++) {
-		long long input;
-		cin >> input;
-		arr.push_back(input);
-	}
-	tree.resize(4 * N);
-	CreateSegTree(1, 0, N - 1);
-	for (int i = 0; i < M + K; i++) {
-		long long a, b, c;
-		cin >> a >> b >> c;
-		if (a == 1) Update(1, 0, N - 1, b - 1, c);
-		else cout << Calculate(1, 0, N - 1, b - 1, c - 1) << '\n';
-		
-	}
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
 }
 
-void CreateSegTree(int node, int start, int end) {
-	if (start == end) {
-		tree[node] = arr[start];
-		return;
-	}
-	CreateSegTree(node * 2, start, (start + end) / 2);
-	CreateSegTree(node * 2 + 1, (start + end) / 2 + 1, end);
-	tree[node] = tree[node * 2] + tree[node * 2 + 1];
-	return;
+void update(int pos, ll val) {
+	for(tree[pos += N] = val; pos > 0; pos >>= 1) tree[pos >> 1] = tree[pos] + tree[pos ^ 1];
+    return;
 }
 
-long long Calculate(int node, int start, int end, int left, int right) {
-	if (start > right || end < left) return 0;
-	else if (left <= start && end <= right) return tree[node];
-	else {
-		long long lsum = Calculate(node * 2, start, (start + end) / 2, left, right);
-		long long rsum = Calculate(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
-		return lsum + rsum;
-	}
-}
-
-void Update(int node, int start, int end, int idx, long long val) {
-	if (idx < start || idx > end) return;
-	if (start == end) {
-		arr[idx] = val;
-		tree[node] = val;
-		return;
-	}
-	Update(node * 2, start, (start + end) / 2, idx, val);
-	Update(node * 2 + 1, (start + end) / 2 + 1, end, idx, val);
-	tree[node] = tree[node * 2] + tree[node * 2 + 1];
-	return;
+ll query(int left, int right) {
+	ll ret = 0;
+    for(left += N, right += N; left < right; left >>= 1, right >>= 1){
+        if(left & 1) ret += tree[left++];
+        if(right & 1) ret += tree[--right];
+    }
+    return ret;
 }
