97e25bbfe837108ce2fdc0b26ce4ab8d6988c11a
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-05-09 17:20:36 +0900

    [Platinum II] Title: Saint John Festival, Time: 20 ms, Memory: 2748 KB -BaekjoonHub

--- 백준/Platinum/11686. Saint John Festival/Saint John Festival.cc
+++ 백준/Platinum/11686. Saint John Festival/Saint John Festival.cc
@@ -1,126 +1,70 @@
-#define _GLIBCXX_FILESYSTEM
-
 #include <bits/stdc++.h>
-
 using namespace std;
-
-#define fastio ios_base::sync_with_stdio(false); cin.tie(0)
-#define endl '\n'
-
-// def
-
-using lli = long long int;
-
-
-using pii = pair<int, int>;
-using pll = pair<lli, lli>;
-
+ 
+using ll = long long;
+using pll = pair<ll, ll>;
+ 
+#define all(v) (v).begin(), (v).end()
 #define X first
 #define Y second
-
-#define rep(I, S, E) for(int I = (S); I < (E); I++)
-#define repq(I, S, E) for(int I = (S); I <= (E); I++)
-
-#define pb push_back
-#define epb emplace_back
-
-#define ALL(V) V.begin(), V.end()
-
-pll operator+(pll a, pll b) {
-    return { a.X + b.X, a.Y + b.Y };
+ 
+ 
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr)
+ 
+// end of def
+ 
+ 
+ll ccw(pll a, pll b, pll c){
+    return a.X * b.Y + b.X * c.Y + c.X * a.Y - a.Y * b.X - b.Y * c.X - c.Y * a.X;
 }
-pll operator-(pll a, pll b) {
-    return { a.X - b.X, a.Y - b.Y };
-}
-
-lli operator*(pll a, pll b) {
-    return a.X * b.Y - a.Y * b.X;
-}
-lli CCW(pll a, pll b, pll c) {
-    return a * b + b * c + c * a;
-}
-
-ostream& operator<<(ostream& out, pii x) {
-    out << "(" << x.X << ", " << x.Y << ")";
-    return out;
-}
-
-// def end
-
-vector<pll> p;
-vector<pll> cvx;
-
-bool Comp(pll a, pll b) {
-    if (a * b == 0)
-        return a < b;
-    return a * b > 0;
-}
-
-void ConvexHull() {
-    int n = p.size();
-    sort(ALL(p));
-
-    rep(i, 1, n)
-        p[i] = p[i] - p[0];
-
-    sort(p.begin() + 1, p.end(), Comp);
-    rep(i, 1, n)
-        p[i] = p[i] + p[0];
-
-    cvx.pb(p[0]);
-    rep(i, 1, n) {
-        while (cvx.size() >= 2 && CCW(cvx[cvx.size() - 2], cvx.back(), p[i]) <= 0)
-            cvx.pop_back();
-        cvx.pb(p[i]);
+ 
+void CH(vector<pll> &v){
+    vector<pll> up, dw;
+    sort(all(v));
+    for (pll p: v){
+        while (up.size() >= 2 && ccw(up[up.size() - 2], up[up.size() - 1], p) <= 0) up.pop_back();
+        while (dw.size() >= 2 && ccw(dw[dw.size() - 2], dw[dw.size() - 1], p) >= 0) dw.pop_back();
+        up.emplace_back(p);
+        dw.emplace_back(p);
+    }
+    v.clear();
+    for (pll p: up){
+        v.emplace_back(p);
+    }
+    for (int i=dw.size() - 2;i >= 1; i--){
+        v.emplace_back(dw[i]);
     }
 }
-
+ 
+bool pt_in_ch(vector<pll> &p, pll q){
+    int n = p.size();
+    if (ccw(p[0], p[n - 1], q) > 0) return false;
+    if (ccw(p[0], p[1], q) < 0) return false;
+    int s = 1, e = n - 1;
+    while (s < e){
+        int m = (s + e) >> 1;
+        if (ccw(p[0], p[m], q) >= 0) s = m + 1;
+        else e = m;
+    }
+    return ccw(p[s - 1], p[s], q) >= 0;
+}
+ 
 int main() {
     fastio;
-
-    int l;
-    cin >> l;
-    p.resize(l);
-    rep(i, 0, l) {
-        cin >> p[i].X >> p[i].Y;
+    int n; cin >> n;
+    vector<pll> v;
+    for (int i=1;i<=n;i++){
+        ll x, y; cin >> x >> y;
+        v.emplace_back(x, y);
     }
-
-    ConvexHull();
-    rep(i, 1, cvx.size())
-        cvx[i] = cvx[i] - cvx[0];
-    sort(cvx.begin() + 1, cvx.end(), Comp);
-    rep(i, 1, cvx.size())
-        cvx[i] = cvx[i] + cvx[0];
-
-    int res = 0;
-
-    int q;
-    cin >> q;
-    while (q--) {
-        pll k;
-        cin >> k.X >> k.Y;
-
-        int lo = 1, hi = (int)cvx.size() - 2;
-        while (lo <= hi) {
-            int mid = (lo + hi) / 2;
-
-            if (CCW(cvx[mid], cvx[0], k) <= 0)
-                lo = mid + 1;
-            else
-                hi = mid - 1;
-        }
-        if (hi == 0)
-            continue;
-
-        vector<pll> r = { cvx[0], cvx[hi], cvx[hi + 1] };
-
-        bool f = true;
-        rep(i, 0, 3) {
-            f &= (CCW(r[i], r[(i + 1) % 3], k) >= 0);
-        }
-        res += f;
+    CH(v);
+    int m; cin >> m;
+    int ans = 0;
+    for (int i=0;i<m;i++){
+        ll x, y; cin >> x >> y;
+        if (pt_in_ch(v, {x, y})) ans++;
     }
-    cout << res;
-
+    cout << ans << endl;
     return 0;
 }
