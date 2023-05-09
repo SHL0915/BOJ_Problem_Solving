74284b27eb713512a658054b0850dad245283285
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-05-09 16:22:22 +0900

    [Platinum IV] Title: 단순 다각형, Time: 8 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Platinum/3679. 단순 다각형/단순 다각형.cc
+++ 백준/Platinum/3679. 단순 다각형/단순 다각형.cc
@@ -4,12 +4,10 @@ using ll = long long;
 using pii = pair<ll, ll>;
 
 int N;
-pair<pii, int> arr[2001];
+pair <pii, int> arr[2005];
 
-bool cmp(pair<pii, int> a, pair<pii, int> b);
-ll CCW(pii a, pii b, pii c);
+ll ccw(pii a, pii b, pii c);
 pii sub(pii a, pii b);
-ll dist(pii a, pii b);
 
 void solve() {
 	cin >> N;
@@ -17,13 +15,16 @@ void solve() {
 		cin >> arr[i].first.first >> arr[i].first.second;
 		arr[i].second = i;
 	}
-
 	sort(arr, arr + N);
-	sort(arr + 1, arr + N, cmp);
-	
+	sort(arr + 1, arr + N, [&](pair <pii, int> a, pair <pii, int> b) {
+		ll res = ccw(arr[0].first, a.first, b.first);
+		if (res) return res > 0;
+		else return a < b;
+		});
+
 	int ptr = N - 1;
-	while (1) {
-		if (CCW(arr[0].first, arr[ptr].first, arr[ptr - 1].first) == 0) ptr--;
+	while (ptr) {
+		if (ccw(arr[0].first, arr[ptr].first, arr[ptr - 1].first) == 0) ptr--;
 		else break;
 	}
 
@@ -34,6 +35,9 @@ void solve() {
 }
 
 int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	int t = 1;
@@ -42,15 +46,8 @@ int main(void) {
 	return 0;
 }
 
-bool cmp(pair<pii, int> a, pair<pii, int> b) {
-	ll ccw = CCW(arr[0].first, a.first, b.first);
-	if (ccw) return ccw > 0;
-	else return dist(arr[0].first, a.first) < dist(arr[0].first, b.first);
-}
-
-ll CCW(pii a, pii b, pii c) {
-	pii u = sub(b, a);
-	pii v = sub(c, b);
+ll ccw(pii a, pii b, pii c) {
+	pii u = sub(b, a), v = sub(c, b);
 	ll ret = u.first * v.second - u.second * v.first;
 	if (ret) ret /= abs(ret);
 	return ret;
@@ -58,10 +55,4 @@ ll CCW(pii a, pii b, pii c) {
 
 pii sub(pii a, pii b) {
 	return { a.first - b.first, a.second - b.second };
-}
-
-ll dist(pii a, pii b) {
-	ll dx = a.first - b.first;
-	ll dy = a.second - b.second;
-	return dx * dx + dy * dy;
 }
