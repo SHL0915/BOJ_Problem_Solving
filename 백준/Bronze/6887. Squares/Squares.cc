b88b361540758ec880fc8307cdbc68369ca91e5c
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-05-19 11:35:25 +0900

    [Bronze IV] Title: Squares, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/6887. Squares/Squares.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int N;
+
+void solve() {
+	cin >> N;
+	int ans = sqrt(N);
+
+	cout << "The largest square has side length " << ans << ".";
+
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
