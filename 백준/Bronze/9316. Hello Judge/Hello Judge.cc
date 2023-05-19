37a1fdb31068ced73fb6fc3e9bf8e9db3b0db7e5
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-05-19 11:41:33 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int N;
+
+void solve() {
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cout << "Hello World, Judge " << i << "!\n";
+	}
+
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
