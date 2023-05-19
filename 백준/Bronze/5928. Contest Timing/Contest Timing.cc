b231d99dbc3713544ad1c321896939cb9bccfd54
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-05-19 11:06:23 +0900

    [Bronze IV] Title: Contest Timing, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5928. Contest Timing/Contest Timing.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int A, B, C;
+
+void solve() {
+	cin >> A >> B >> C;
+	A -= 11;
+	B -= 11;
+	C -= 11;
+
+	B += A * 24;
+	C += B * 60;
+
+	if (C < 0) cout << -1;
+	else cout << C;
+	return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
