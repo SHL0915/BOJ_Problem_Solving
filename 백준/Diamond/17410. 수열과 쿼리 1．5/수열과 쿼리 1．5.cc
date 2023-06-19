ce1aa025ac6a8acb712ebcec2907e2412376974e
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-06-19 10:50:18 +0900

    [Diamond V] Title: 수열과 쿼리 1.5, Time: 816 ms, Memory: 41700 KB -BaekjoonHub

---
+++ 백준/Diamond/17410. 수열과 쿼리 1．5/수열과 쿼리 1．5.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+const int SZ = 10000;
+
+int N, M, sqrtN;
+int arr[100005];
+int bucket[1005][10005];
+
+void update(int pos, int k);
+int query(int l, int r, int k);
+
+void solve() {
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> arr[i];
+    sqrtN = sqrt(N);
+    
+    for (int i = 1; i <= N; i++) bucket[i / sqrtN][arr[i]]++;
+    for (int i = 0; i <= N / sqrtN; i++) {
+        for (int j = 1; j <= SZ; j++) bucket[i][j] += bucket[i][j - 1];
+    }
+    
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        int q;
+        cin >> q;
+        if (q == 1) {
+            int pos, k;
+            cin >> pos >> k;
+            update(pos, k);
+        } else {
+            int l, r, k;
+            cin >> l >> r >> k;
+            cout << query(l, r, k) << '\n';
+        }
+    }
+    
+    return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
+    return 0;
+}
+
+void update(int pos, int k) {
+    int idx = pos / sqrtN;
+    int org = arr[pos];
+    
+    arr[pos] = k;
+    if (org > k) for (int i = k; i < org; i++) bucket[idx][i]++;
+    else for (int i = org; i < k; i++) bucket[idx][i]--;
+    
+    return;
+}
+
+int query(int l, int r, int k) {
+    int ret = 0;
+    while (l % sqrtN && l <= r) {
+        if (arr[l] > k) ret++;
+        l++;
+    }
+    while ((r + 1) % sqrtN && l <= r) {
+        if (arr[r] > k) ret++;
+        r--;
+    }
+    while (l <= r) {
+        ret += bucket[l / sqrtN][SZ] - bucket[l / sqrtN][k];
+        l += sqrtN;
+    }
+    
+    return ret;
+}
