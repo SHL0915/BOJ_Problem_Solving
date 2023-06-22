eea8e26a193154e832161db6e2c49666eb020eed
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-06-22 11:22:16 +0900

    [Silver II] Title: 터렛, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.cc
+++ 백준/Silver/1002. 터렛/터렛.cc
@@ -1,36 +1,53 @@
-#include <iostream>
-#include <cstdlib>
-#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
+using ll = long long;
+using pii = pair<ll, ll>;
 
-int T;
+ll sqrdist(pii a, pii b);
 
-long long disSqr(int x1, int y1, int x2, int y2) {
-	return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+void solve() {
+    pii a, b;
+    ll r1, r2;
+    cin >> a.first >> a.second >> r1;
+    cin >> b.first >> b.second >> r2;
+    
+    if (a == b && r1 == r2) {
+        if (r1 == 0) cout << 1 << '\n';
+        else cout << "-1\n";
+        return;
+    }
+    
+    ll d = sqrdist(a, b);
+    if (d <= max(r1 * r1, r2 * r2)) {
+        ll v = abs(r1 - r2) * abs(r1 - r2);
+        if (v > d) cout << 0 << '\n';
+        else if (v == d) cout << 1 << '\n';
+        else cout << 2 << '\n';
+    } else {
+        ll v = (r1 + r2) * (r1 + r2);
+        if (v > d) cout << 2 << '\n';
+        else if (v == d) cout << 1 << '\n';
+        else cout << 0 << '\n';
+    }
+    
+    return;
 }
 
 int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> T;
-	while (T--) {
-		long long x1, y1, r1, x2, y2, r2;
-		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
-		if (x1 == x2 && y1 == y2 && r1 == r2) {
-			cout << -1 << '\n';
-			continue;
-		}
-		long long dis = disSqr(x1, y1, x2, y2);
-		if (dis >= max(r1 * r1, r2 * r2)) {
-			if (dis > r1 * r1 + 2 * r1 * r2 + r2 * r2) cout << 0 << '\n';
-			else if (dis == r1 * r1 + 2 * r1 * r2 + r2 * r2) cout << 1 << '\n';
-			else cout << 2 << '\n';
-		}
-		else {
-			if (dis < r1 * r1 - 2 * r1 * r2 + r2 * r2) cout << 0 << '\n';
-			else if (dis == r1 * r1 - 2 * r1 * r2 + r2 * r2) cout << 1 << '\n';
-			else cout << 2 << '\n';
-		}
-	}
-	return 0;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
+
+ll sqrdist(pii a, pii b) {
+    ll dx = a.first - b.first;
+    ll dy = a.second - b.second;
+    return dx * dx + dy * dy;
 }
