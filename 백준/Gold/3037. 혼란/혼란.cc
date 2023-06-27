6cc5c6d63e408e573b36452a83b85539b22f1de9
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-06-27 14:23:41 +0900

    [Gold I] Title: 혼란, Time: 56 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/3037. 혼란/혼란.cc
+++ 백준/Gold/3037. 혼란/혼란.cc
@@ -5,25 +5,24 @@ using pii = pair<int, int>;
 const ll mod = 1000000007;
 
 int N, C;
-ll table[1005][10005];
-ll psum[10005];
+ll A[10005], B[10005];
 
 void solve() {
     cin >> N >> C;
 
-    table[0][0] = 1;
-    psum[0] = 1;
+    A[0] = 1;
+    B[0] = 1;
     for(int i = 1; i <= N; i++) {
         for(int j = 0; j <= C; j++) {
-            if(j - (N - i) <= 0) table[i][j] = psum[j];
-            else table[i][j] = (psum[j] - psum[j - (N - i) - 1] + mod) % mod;
+            if(j - (N - i) <= 0) A[j] = B[j];
+            else A[j] = (B[j] - B[j - (N - i) - 1] + mod) % mod;
         }
 
-        psum[0] = table[i][0];
-        for(int j = 1; j <= C; j++) psum[j] = (table[i][j] + psum[j - 1]) % mod;
+        B[0] = A[0];
+        for(int j = 1; j <= C; j++) B[j] = (A[j] + B[j - 1]) % mod;
     }
 
-    cout << psum[C];
+    cout << B[C];
     return;
 }
 
