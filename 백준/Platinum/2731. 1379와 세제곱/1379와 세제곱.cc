7fdbd92044e3eec1a218601605f4683a180e8f26
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-07-02 22:21:05 +0900

    [Platinum V] Title: 1379와 세제곱, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Platinum/2731. 1379와 세제곱/1379와 세제곱.cc
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int flag = 0;
+string S;
+ll pow_10[15];
+
+void BackTracking(int idx, string s);
+
+ll StoI(string s);
+
+string ItoS(ll n, int len);
+
+void solve() {
+    cin >> S;
+    flag = 0;
+    string s = "";
+    BackTracking(0, s);
+    cout << '\n';
+    return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    pow_10[0] = 1;
+    for (int i = 1; i < 15; i++) pow_10[i] = pow_10[i - 1] * 10;
+
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
+
+void BackTracking(int idx, string s) {
+    if (flag) return;
+    if (idx == S.length()) {
+        cout << StoI(s);
+        flag = 1;
+        return;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        string next = (char) ('0' + i) + s;
+        __int128 Inext = StoI(next);
+
+        __int128 res = Inext;
+        res *= (__int128)Inext;
+        res %= (__int128)pow_10[idx + 1];
+        res *= (__int128)Inext;
+        res %= (__int128)pow_10[idx + 1];
+
+        string convert = ItoS(res, next.length());
+        int a = 0;
+        for (int i = 0; i <= idx; i++) {
+            if (convert[i] != S[S.length() - 1 - idx + i]) a = 1;
+        }
+
+        if (a == 0) BackTracking(idx + 1, next);
+    }
+
+}
+
+ll StoI(string s) {
+    ll ret = 0;
+    for (int i = 0; i < s.length(); i++) {
+        ret *= 10;
+        ret += s[i] - '0';
+    }
+    return ret;
+}
+
+string ItoS(ll n, int len) {
+    string ret = "";
+    while (n) {
+        ret = (char) ('0' + n % 10) + ret;
+        n /= 10;
+    }
+
+    while(ret.length() < len) ret = '0' + ret;
+
+    return ret;
+}
---
+++ 백준/Platinum/2731. 1379와 세제곱/README.md
@@ -0,0 +1,26 @@
+# [Platinum V] 1379와 세제곱 - 2731 
+
+[문제 링크](https://www.acmicpc.net/problem/2731) 
+
+### 성능 요약
+
+메모리: 2028 KB, 시간: 0 ms
+
+### 분류
+
+백트래킹, 수학, 정수론
+
+### 문제 설명
+
+<p>Given any string of decimal digits, ending in 1, 3, 7 or 9, there is always a decimal number, which when cubed has a decimal expansion ending in the original given digit string. The number need never have more digits than the given digit string. </p>
+
+<p>Write a program, which takes as input a string of decimal digits ending in 1, 3, 7 or 9 and finds a number of at most the same number of digits, which when cubed, ends in the given digit string. </p>
+
+### 입력 
+
+ <p>The input begins with a line containing only the count of problem instances, nProb, as a decimal integer, 1 <= nProb <= 1000. This is followed by nProb lines, each of which contains a string of between 1 and 10 decimal digits ending in 1, 3, 7 or 9.</p>
+
+### 출력 
+
+ <p>For each problem instance, there should be one line of output consisting of the number, which when cubed, ends in the given digit string. The number should be output as a decimal integer with no leading spaces and no leading zeroes. </p>
+
