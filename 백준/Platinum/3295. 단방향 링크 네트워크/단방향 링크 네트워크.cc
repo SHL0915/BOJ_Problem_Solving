715237ecda3b7e491d380547ef9c2ca7de528870
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-07-06 15:50:32 +0900

    [Platinum II] Title: 단방향 링크 네트워크, Time: 36 ms, Memory: 2452 KB -BaekjoonHub

--- 백준/Platinum/3295. 단방향 링크 네트워크/단방향 링크 네트워크.cc
+++ 백준/Platinum/3295. 단방향 링크 네트워크/단방향 링크 네트워크.cc
@@ -3,35 +3,58 @@
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
+const int SZ = 1005;
 
 int N, M;
-vector<int> graph[1005];
-int A[1005], B[1005], mark[1005];
+vector<int> graph[SZ];
+int A[SZ], B[SZ], mark[SZ];
 
-void init();
+int DFS(int node) {
+    mark[node] = 1;
+    for (int next: graph[node]) {
+        if (B[next] == -1) {
+            A[node] = next;
+            B[next] = node;
+            return 1;
+        }
+    }
 
-void clearMark();
+    for (int next: graph[node]) {
+        if (mark[B[next]] == 0 && DFS(B[next])) {
+            A[node] = next;
+            B[next] = node;
+            return 1;
+        }
+    }
 
-int DFS(int node);
+    return 0;
+}
+
+int Bipartite_Matching() {
+    int ret = 0;
+    memset(A, -1, sizeof(A));
+    memset(B, -1, sizeof(B));
+    for (int i = 0; i < N; i++) {
+        if (A[i] == -1) {
+            memset(mark, 0, sizeof(mark));
+            ret += DFS(i);
+        }
+    }
+    return ret;
+}
 
 void solve() {
     cin >> N >> M;
-    init();
+
+    for (int i = 0; i < N; i++) graph[i].clear();
+
     for (int i = 0; i < M; i++) {
         int a, b;
         cin >> a >> b;
         graph[a].push_back(b);
     }
 
-    int ans = 0;
-    for (int i = 0; i < N; i++) {
-        if (A[i] == -1) {
-            clearMark();
-            ans += DFS(i);
-        }
-    }
-
-    cout << ans << '\n';
+    cout << Bipartite_Matching() << '\n';
     return;
 }
 
@@ -44,34 +67,5 @@ int main(void) {
     int t = 1;
     cin >> t;
     while (t--) solve();
-    return 0;
-}
-
-void init() {
-    for (int i = 0; i < N; i++) {
-        graph[i].clear();
-        A[i] = -1;
-        B[i] = -1;
-    }
-    return;
-}
-
-void clearMark() {
-    for (int i = 0; i < N; i++) mark[i] = 0;
-    return;
-}
-
-int DFS(int node) {
-    mark[node] = 1;
-
-    for (int i = 0; i < graph[node].size(); i++) {
-        int next = graph[node][i];
-        if (B[next] == -1 || mark[B[next]] == 0 && DFS(B[next])) {
-            A[node] = next;
-            B[next] = node;
-            return 1;
-        }
-    }
-
     return 0;
 }
