c20dce23c420bd6ad1bd54aabaf2d39d98810682
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-07-06 16:55:31 +0900

    [Platinum III] Title: 용량 부족, Time: 72 ms, Memory: 7444 KB -BaekjoonHub

---
+++ 백준/Platinum/5446. 용량 부족/용량 부족.cc
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+struct Trie {
+    map<char, Trie *> ch;
+    int a, b, acnt, bcnt;
+
+    Trie() {
+        acnt = 0;
+        bcnt = 0;
+    }
+
+    ~Trie() {
+        for (auto i = ch.begin(); i != ch.end(); i++) delete (*i).second;
+    }
+
+    void insert(const char *s, int type) {
+        if (!*s) {
+            if (type == 0) acnt++;
+            else bcnt++;
+        } else {
+            char next = *s;
+            if (ch.count(next) == 0) ch[next] = new Trie;
+            ch[next]->insert(s + 1, type);
+        }
+    }
+
+    void psum() {
+        a = acnt;
+        b = bcnt;
+        for (auto i = ch.begin(); i != ch.end(); i++) {
+            (*i).second->psum();
+            a += (*i).second->a;
+            b += (*i).second->b;
+        }
+        return;
+    }
+
+    int query() {
+        int ret = 0;
+        if (a != 0 && b == 0) {
+            a = 0;
+            return 1;
+        }
+
+        a = acnt;
+        for (auto i = ch.begin(); i != ch.end(); i++) {
+            ret += (*i).second->query();
+            a += (*i).second->a;
+        }
+
+        return ret;
+    }
+};
+
+int A, B;
+
+void solve() {
+    cin >> A;
+
+    Trie *root = new Trie;
+
+    for (int i = 0; i < A; i++) {
+        string s;
+        cin >> s;
+        root->insert(s.c_str(), 0);
+    }
+
+    cin >> B;
+
+    for (int i = 0; i < B; i++) {
+        string s;
+        cin >> s;
+        root->insert(s.c_str(), 1);
+    }
+
+    root->psum();
+
+    int ans = root->query();
+    ans += root->a;
+
+    cout << ans << '\n';
+
+    delete root;
+    return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
