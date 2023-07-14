a4965e33d511363dbf148b03f13ebc24d93ccda5
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-07-14 17:20:15 +0900

    [Platinum II] Title: Triumphal arch, Time: 980 ms, Memory: 42908 KB -BaekjoonHub

---
+++ 백준/Platinum/8243. Triumphal arch/Triumphal arch.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int N;
+vector<int> tree[300005];
+int arr[300005];
+
+void dp(int node, int par, int k) {
+    int cnt = 0;
+    for (int next: tree[node]) {
+        if (next == par) continue;
+        cnt++;
+        dp(next, node, k);
+        arr[node] += arr[next];
+    }
+
+    arr[node] += k - cnt;
+    arr[node] = min(arr[node], 0);
+
+    return;
+}
+
+int chk(int k) {
+    memset(arr, 0, sizeof(arr));
+    dp(1, 1, k);
+    return arr[1] < 0;
+}
+
+void solve() {
+    cin >> N;
+    for (int i = 0; i < N - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        tree[a].push_back(b);
+        tree[b].push_back(a);
+    }
+
+    int l = 0, r = N;
+    int ans = -1;
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (chk(mid) == 0) {
+            ans = mid;
+            r = mid - 1;
+        } else l = mid + 1;
+    }
+
+    cout << ans;
+    return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
+    return 0;
+}
