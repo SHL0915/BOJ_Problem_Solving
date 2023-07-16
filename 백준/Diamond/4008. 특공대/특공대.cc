b698ff0b3dec46053a2e32e9a963018435a0c30f
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-07-16 16:42:15 +0900

    [Diamond V] Title: 특공대, Time: 116 ms, Memory: 62452 KB -BaekjoonHub

--- 백준/Diamond/4008. 특공대/특공대.cc
+++ 백준/Diamond/4008. 특공대/특공대.cc
@@ -3,66 +3,66 @@
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
-const ll INF = 1e10;
+const ll INF = 1e15;
 
 struct line {
     ll a, b;
-    double s;
+    double x;
 
-    line(ll a = 0, ll b = 0, double c = 0) : a(a), b(b), s(c) {};
+    line(ll a, ll b, double x) : a(a), b(b), x(x) {};
 
     ll f(ll x) {
         return a * x + b;
     }
 };
 
-ll N, A, B, C, K;
+ll N, A, B, C;
 int ptr;
-ll arr[1000005], psum[1000005];
-ll dp[1000005];
-vector<line> l;
+ll arr[1000005], psum[1000005], dp[1000005];
+vector<line> L;
+
+ll slope(int i) {
+    return -2 * A * psum[i];
+}
+
+ll intercept(int i) {
+    return A * psum[i] * psum[i] - B * psum[i] + dp[i];
+}
+
+ll f(int i) {
+    return A * psum[i] * psum[i] + B * psum[i] + C;
+}
 
 double intersect(line &a, line &b) {
     return (double) (a.b - b.b) / (b.a - a.a);
 }
 
 void add_line(ll a, ll b) {
-    line L(a, b, -INF);
-
-    if (l.size() == 0) {
-        l.push_back(L);
+    line l(a, b, -INF);
+    if (L.size() == 0) {
+        L.push_back(l);
         return;
     }
 
-    while (l.size()) {
-        line t = l.back();
-        double x = intersect(t, L);
-        if (x <= t.s) l.pop_back();
+    while(L.size()) {
+        line t = L.back();
+        double x = intersect(t, l);
+        if(x <= t.x) L.pop_back();
         else break;
     }
 
-    L.s = intersect(l.back(), L);
-    l.push_back(L);
+    l.x = intersect(l, L.back());
+    L.push_back(l);
 
     return;
 }
 
-ll k_i(int i) {
-    return -2 * A * psum[i];
-}
-
-ll m_i(int i) {
-    return A * psum[i] * psum[i] - B * psum[i] + dp[i];
-}
-
 ll query(ll x) {
-    while (ptr < l.size() - 1 && l[ptr + 1].s < x) ptr++;
-    return l[ptr].f(x);
+    while (ptr < L.size() - 1 && L[ptr + 1].x <= x) ptr++;
+
+    return L[ptr].f(x);
 }
 
-ll f(int i) {
-    return A * psum[i] * psum[i] + B * psum[i] + C;
-}
 
 void solve() {
     cin >> N >> A >> B >> C;
@@ -71,12 +71,12 @@ void solve() {
         psum[i] = arr[i] + psum[i - 1];
     }
 
-    dp[1] = A * psum[1] * psum[1] + B * psum[1] + C;
-    add_line(k_i(1), m_i(1));
+    dp[1] = f(1);
+    add_line(slope(1), intercept(1));
 
     for (int i = 2; i <= N; i++) {
         dp[i] = max(f(i), query(psum[i]) + f(i));
-        add_line(k_i(i), m_i(i));
+        add_line(slope(i), intercept(i));
     }
 
     cout << dp[N];
