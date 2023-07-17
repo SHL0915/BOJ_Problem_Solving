545d092ef5ca993511ba2b9384ef2afa024eed77
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-07-18 02:18:39 +0900

    [Diamond V] Title: 반평면 땅따먹기, Time: 124 ms, Memory: 8228 KB -BaekjoonHub

---
+++ 백준/Diamond/12795. 반평면 땅따먹기/반평면 땅따먹기.cc
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+struct line {
+    mutable ll a, b, x;
+
+    bool operator<(const line &cmp) const {
+        return a < cmp.a;
+    }
+
+    bool operator<(const ll cmp) const {
+        return x < cmp;
+    }
+};
+
+struct LineContainer : multiset<line, less<>> { // 기울기가 증가하는 순으로 정렬
+    const ll INF = 1e15;
+
+    ll div(ll a, ll b) {
+        return a / b - ((a ^ b) < 0 && a % b);
+    }
+
+    bool intersect(iterator x, iterator y) {
+        if (y == end()) return x->x = INF, 0;
+        if (x->a == y->a) x->x = x->b > y->b ? INF : -INF;
+        else x->x = div(y->b - x->b, x->a - y->a);
+        return x->x >= y->x;
+    }
+
+    void add(ll a, ll b) {
+        auto z = insert({a, b, 0}), y = z++, x = y;
+        while (intersect(y, z)) z = erase(z);
+        if (x != begin() && intersect(--x, y)) intersect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->x >= y->x) intersect(x, erase(y));
+    }
+
+    ll query(ll x) {
+        auto l = *lower_bound(x);
+        return l.a * x + l.b;
+    }
+};
+
+int Q;
+
+void solve() {
+    cin >> Q;
+    LineContainer A, B;
+    int acnt = 0, bcnt = 0;
+
+    for (int i = 0; i < Q; i++) {
+        int a;
+        cin >> a;
+        if (a == 1) {
+            ll x, y;
+            cin >> x >> y;
+            if (x >= 0) {
+                acnt++;
+                A.add(x, y);
+            } else {
+                bcnt++;
+                B.add(x, y);
+            }
+        } else {
+            ll x;
+            cin >> x;
+            ll ans = -0x3f3f3f3f3f3f3f3fLL;
+            if (acnt) ans = max(ans, A.query(x));
+            if (bcnt) ans = max(ans, B.query(x));
+
+            cout << ans << '\n';
+        }
+    }
+
+    return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
+    return 0;
+}
