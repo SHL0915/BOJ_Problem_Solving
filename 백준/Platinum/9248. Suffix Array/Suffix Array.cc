28c7ee34c5800f1860c19972a34345c2b72be673
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-08-18 17:00:45 +0900

    [Platinum III] Title: Suffix Array, Time: 676 ms, Memory: 30256 KB -BaekjoonHub

--- 백준/Platinum/9248. Suffix Array/Suffix Array.cc
+++ 백준/Platinum/9248. Suffix Array/Suffix Array.cc
@@ -5,44 +5,43 @@ using ll = long long;
 using pii = pair<int, int>;
 
 string S;
-int N;
-int sa[500005], now[500005], temp[500005], isa[500005], lcp[500005];
-int d;
+int N, d;
+int suffix[500005], lcp[5000005], group[500005], temp[500005], rev[500005];
 
-bool cmp(int p, int q) {
-    if (now[p] != now[q]) return now[p] < now[q];
-    else return now[min(p + d, N)] < now[min(q + d, N)];
+bool cmp(int a, int b) {
+    if (group[a] != group[b]) return group[a] < group[b];
+    return group[min(a + d, N)] < group[min(b + d, N)];
 }
 
 void solve() {
     cin >> S;
     N = S.length();
     for (int i = 0; i < N; i++) {
-        sa[i] = i;
-        now[i] = S[i];
+        suffix[i] = i;
+        group[i] = S[i];
     }
 
     for (d = 1; d < N; d <<= 1) {
-        sort(sa, sa + N, cmp);
-        temp[sa[0]] = 1;
-        for (int i = 1; i < N; i++) temp[sa[i]] = temp[sa[i - 1]] + cmp(sa[i - 1], sa[i]);
-        for (int i = 0; i < N; i++) now[i] = temp[i];
-        if (now[sa[N - 1]] == N) break;
+        sort(suffix, suffix + N, cmp);
+        temp[suffix[0]] = 1;
+        for (int i = 1; i < N; i++) temp[suffix[i]] = temp[suffix[i - 1]] + cmp(suffix[i - 1], suffix[i]);
+        for (int i = 0; i < N; i++) group[i] = temp[i];
+        if (group[suffix[N - 1]] == N) break;
     }
 
-    for(int i = 0; i < N; i++) isa[sa[i]] = i;
+    for (int i = 0; i < N; i++) rev[suffix[i]] = i;
 
     int l = 0;
-    for(int i = 0; i < N; i++, l = max(l - 1, 0)) {
-        if(isa[i] == 0) continue;
-        for(int j = sa[isa[i] - 1]; S[i + l] == S[j + l]; l++);
-        lcp[isa[i]] = l;
+    for (int i = 0; i < N; i++) {
+        l = max(l - 1, 0);
+        if (rev[i] == 0) continue;
+        for (int j = suffix[rev[i] - 1]; S[i + l] == S[j + l]; l++);
+        lcp[rev[i]] = l;
     }
 
-    for (int i = 0; i < N; i++) cout << sa[i] + 1 << " ";
-    cout << endl << "x ";
-    for(int i = 1; i < N; i++) cout << lcp[i] << " ";
-
+    for (int i = 0; i < N; i++) cout << suffix[i] + 1 << " ";
+    cout << '\n' << "x ";
+    for (int i = 1; i < N; i++) cout << lcp[i] << " ";
     return;
 }
 
