97dd41342540b99fa0fd4c948bbe5ce957018a44
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-08-18 17:06:10 +0900

    [Platinum III] Title: Suffix Array, Time: 672 ms, Memory: 12680 KB -BaekjoonHub

--- 백준/Platinum/9248. Suffix Array/Suffix Array.cc
+++ 백준/Platinum/9248. Suffix Array/Suffix Array.cc
@@ -6,40 +6,40 @@ using pii = pair<int, int>;
 
 string S;
 int N, d;
-int suffix[500005], lcp[5000005], group[500005], temp[500005], rev[500005];
+int sa[500005], lcp[500005], now[500005], nxt[500005], rev[500005];
 
 bool cmp(int a, int b) {
-    if (group[a] != group[b]) return group[a] < group[b];
-    return group[min(a + d, N)] < group[min(b + d, N)];
+    if (now[a] != now[b]) return now[a] < now[b];
+    else return now[min(a + d, N)] < now[min(b + d, N)];
 }
 
 void solve() {
     cin >> S;
     N = S.length();
     for (int i = 0; i < N; i++) {
-        suffix[i] = i;
-        group[i] = S[i];
+        sa[i] = i;
+        now[i] = S[i];
     }
 
     for (d = 1; d < N; d <<= 1) {
-        sort(suffix, suffix + N, cmp);
-        temp[suffix[0]] = 1;
-        for (int i = 1; i < N; i++) temp[suffix[i]] = temp[suffix[i - 1]] + cmp(suffix[i - 1], suffix[i]);
-        for (int i = 0; i < N; i++) group[i] = temp[i];
-        if (group[suffix[N - 1]] == N) break;
+        sort(sa, sa + N, cmp);
+        nxt[sa[0]] = 1;
+        for (int i = 1; i < N; i++) nxt[sa[i]] = nxt[sa[i - 1]] + cmp(sa[i - 1], sa[i]);
+        for (int i = 0; i < N; i++) now[i] = nxt[i];
+        if (now[sa[N - 1]] == N) break;
     }
 
-    for (int i = 0; i < N; i++) rev[suffix[i]] = i;
+    for (int i = 0; i < N; i++) rev[sa[i]] = i;
 
     int l = 0;
     for (int i = 0; i < N; i++) {
         l = max(l - 1, 0);
         if (rev[i] == 0) continue;
-        for (int j = suffix[rev[i] - 1]; S[i + l] == S[j + l]; l++);
+        for (int j = sa[rev[i] - 1]; S[i + l] == S[j + l]; l++);
         lcp[rev[i]] = l;
     }
 
-    for (int i = 0; i < N; i++) cout << suffix[i] + 1 << " ";
+    for (int i = 0; i < N; i++) cout << sa[i] + 1 << " ";
     cout << '\n' << "x ";
     for (int i = 1; i < N; i++) cout << lcp[i] << " ";
     return;
