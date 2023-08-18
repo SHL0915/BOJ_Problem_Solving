83cdc169a5bc88de578df978a78f4808f0439f3f
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-08-18 15:13:38 +0900

    [Platinum II] Title: 문자열 찾기, Time: 16 ms, Memory: 17832 KB -BaekjoonHub

--- 백준/Platinum/25008. 문자열 찾기/문자열 찾기.cc
+++ 백준/Platinum/25008. 문자열 찾기/문자열 찾기.cc
@@ -6,27 +6,41 @@ using pii = pair<int, int>;
 const int INF = 0x3f3f3f3f;
 int len;
 int pi[1000005];
-int org[1000005];
+int idx[26], A[1000005], B[1000005];
+
+bool f(int i, int j) {
+    int a, b;
+    if (A[i] == -1) a = INF;
+    else a = i - A[i];
+    if (A[j] == -1 || A[j] < j - i) b = INF;
+    else b = j - A[j];
+
+    return a == b;
+}
+
+bool g(int i, int j) {
+    int a, b;
+    if (A[i] == -1) a = INF;
+    else a = i - A[i];
+    if (B[j] == -1 || B[j] < j - i) b = INF;
+    else b = j - B[j];
+
+    return a == b;
+}
 
 void makePi(char P[]) {
-    vector<int> A[26], B[26];
+    memset(idx, -1, sizeof(idx));
+    memset(A, -1, sizeof(A));
 
     for (int i = 0; i < len; i++) {
-        int c = P[i] - 'a';
-        if (A[c].size() == 0) org[i] = INF;
-        else org[i] = i - A[c].back();
-        A[c].push_back(i);
+        A[i] = idx[P[i] - 'a'];
+        idx[P[i] - 'a'] = i;
     }
 
     int pos = 1, k = 0;
 
     while (pos + k < len) {
-        int c = P[pos + k] - 'a';
-        int now;
-        if (B[c].size() == 0 || B[c].back() < pos) now = INF;
-        else now = pos + k - B[c].back();
-        if (org[k] == now) {
-            B[c].push_back(pos + k);
+        if (f(k, pos + k)) {
             k++;
             pi[pos + k - 1] = k;
         } else {
@@ -47,17 +61,16 @@ int findP(char T[], char P[], int N, int M) {
     len = M;
     makePi(P);
 
-    vector<int> A[26];
+    memset(idx, -1, sizeof(idx));
+    memset(B, -1, sizeof(B));
     int pos = 0, k = 0;
+    for (int i = 0; i < N; i++) {
+        B[i] = idx[T[i] - 'a'];
+        idx[T[i] - 'a'] = i;
+    }
 
     while (pos + M <= N) {
-        int c = T[pos + k] - 'a';
-        int now;
-        if (A[c].size() == 0 || A[c].back() < pos) now = INF;
-        else now = pos + k - A[c].back();
-
-        if (k < M && org[k] == now) {
-            A[c].push_back(pos + k);
+        if (k < M && g(k, pos + k)) {
             k++;
             if (k == M) ret++;
         } else {
