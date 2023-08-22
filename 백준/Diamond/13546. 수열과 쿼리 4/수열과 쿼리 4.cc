640adbb641c6d33e6269e6c2d67abb0df9e354ee
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-08-22 16:39:20 +0900

    [Diamond IV] Title: 수열과 쿼리 4, Time: 2140 ms, Memory: 84536 KB -BaekjoonHub

--- 백준/Diamond/13546. 수열과 쿼리 4/수열과 쿼리 4.cc
+++ 백준/Diamond/13546. 수열과 쿼리 4/수열과 쿼리 4.cc
@@ -4,56 +4,111 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 
-int N, K, M, B;
-ll arr[100005];
+int N, K, Q, B, sz;
+int arr[100005];
 deque<int> cnt[100005];
-int seg[200005];
-pair<pii, int> q[100005];
+int ccnt[100505];
+int bucket[100505];
+pair<int, pii> q[100005];
 int ans[100005];
 
-void push(int a);
+void push(int a) {
+    int num = arr[a];
+    if (cnt[num].size() >= 2) {
+        int val = cnt[num].back() - cnt[num].front();
+        ccnt[val]--;
+        bucket[val / sz]--;
+    }
 
-void pop(int a);
+    if (cnt[num].size() == 0) cnt[num].push_back(a);
+    else if (a < cnt[num].front()) cnt[num].push_front(a);
+    else cnt[num].push_back(a);
 
-void update(int pos, int val);
+    if (cnt[num].size() >= 2) {
+        int val = cnt[num].back() - cnt[num].front();
+        ccnt[val]++;
+        bucket[val / sz]++;
+    }
+    return;
+}
 
-int query(int l, int r);
+void pop(int a) {
+    int num = arr[a];
+    if (cnt[num].size() >= 2) {
+        int val = cnt[num].back() - cnt[num].front();
+        ccnt[val]--;
+        bucket[val / sz]--;
+    }
+
+    if (cnt[num].front() == a) cnt[num].pop_front();
+    else cnt[num].pop_back();
+
+    if (cnt[num].size() >= 2) {
+        int val = cnt[num].back() - cnt[num].front();
+        ccnt[val]++;
+        bucket[val / sz]++;
+    }
+
+    return;
+}
+
+int find_ans() {
+    int ret = 0;
+    int idx = N / sz + 5;
+    for (int i = idx; i >= 0; i--) {
+        if (bucket[i] > 0) {
+            int pos = (i + 1) * sz + 5;
+            for (int j = pos;; j--) {
+                if (ccnt[j] > 0) {
+                    ret = j;
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    return ret;
+}
 
 void solve() {
     cin >> N >> K;
     for (int i = 1; i <= N; i++) cin >> arr[i];
-    cin >> M;
-    while (B * B <= N) B++;
-    B--;
-    for (int i = 0; i < M; i++) {
+    cin >> Q;
+    for (int i = 0; i < Q; i++) {
         int l, r;
         cin >> l >> r;
-        q[i] = {{l, r}, i};
+        q[i] = {i, {l, r}};
     }
 
-    sort(q, q + M, [&](pair<pii, int> a, pair<pii, int> b) {
-        if (a.first.first / B == b.first.first / B) return a.first.second < b.first.second;
-        return a.first.first / B < b.first.first / B;
+    while (B * B <= Q) B++;
+    B--;
+
+    sz = sqrt(N) * 2;
+
+    sort(q, q + Q, [&](pair<int, pii> a, pair<int, pii> b) {
+        if (a.second.first / B == b.second.first / B) return a.second.second < b.second.second;
+        return a.second.first / B < b.second.first / B;
     });
 
-    int l = q[0].first.first, r = q[0].first.second;
+    int l = q[0].second.first, r = q[0].second.second, id = q[0].first;
     for (int i = l; i <= r; i++) push(i);
+    ans[id] = find_ans();
 
-    ans[q[0].second] = query(1, K);
-
-    for (int i = 1; i < M; i++) {
-        int nl = q[i].first.first, nr = q[i].first.second;
-        int idx = q[i].second;
+    for (int i = 1; i < Q; i++) {
+        int nl = q[i].second.first, nr = q[i].second.second;
+        id = q[i].first;
 
         while (l > nl) push(--l);
         while (r < nr) push(++r);
         while (l < nl) pop(l++);
         while (r > nr) pop(r--);
 
-        ans[idx] = query(1, K);
+        ans[id] = find_ans();
     }
 
-    for (int i = 0; i < M; i++) cout << ans[i] << '\n';
+    for (int i = 0; i < Q; i++) cout << ans[i] << '\n';
+
     return;
 }
 
@@ -67,38 +122,4 @@ int main(void) {
     //cin >> t;
     while (t--) solve();
     return 0;
-}
-
-void push(int a) {
-    int now = arr[a];
-    if (cnt[now].size() == 0) cnt[now].push_back(a);
-    else if (a < cnt[now].front()) cnt[now].push_front(a);
-    else cnt[now].push_back(a);
-    if (cnt[now].size() >= 2) update(now, cnt[now].back() - cnt[now].front());
-    return;
-}
-
-void pop(int a) {
-    int now = arr[a];
-    if (a == cnt[now].front()) cnt[now].pop_front();
-    else cnt[now].pop_back();
-
-    if (cnt[now].size() < 2) update(now, 0);
-    else update(now, cnt[now].back() - cnt[now].front());
-    return;
-}
-
-void update(int pos, int val) {
-    for (seg[pos += K] = val; pos > 0; pos >>= 1) seg[pos >> 1] = max(seg[pos], seg[pos ^ 1]);
-    return;
-}
-
-int query(int l, int r) {
-    r++;
-    int ret = 0;
-    for (l += K, r += K; l < r; l >>= 1, r >>= 1) {
-        if (l & 1) ret = max(ret, seg[l++]);
-        if (r & 1) ret = max(ret, seg[--r]);
-    }
-    return ret;
 }
