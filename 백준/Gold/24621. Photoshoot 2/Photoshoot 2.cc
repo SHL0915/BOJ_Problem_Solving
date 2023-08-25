b51ed3597ec3058fe21f994578c1ad2f74fc0410
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-08-25 17:16:38 +0900

    [Gold IV] Title: Photoshoot 2, Time: 20 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Gold/24621. Photoshoot 2/Photoshoot 2.cc
+++ 백준/Gold/24621. Photoshoot 2/Photoshoot 2.cc
@@ -1,35 +1,41 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 
-int N, cnt;
-int A[100001], B[100001], mark[100001];
-set <int> s;
+int N;
+int A[100005], B[100005], rev[100005];
 
 void solve() {
-	cin >> N;
-	for (int i = 0; i < N; i++) cin >> A[i];
-	for (int i = 0; i < N; i++) {
-		cin >> B[i];
-		mark[B[i]] = i;
-	}
-	for (int i = 0; i < N; i++) {
-		if (s.size() == 0) s.insert(mark[A[i]]);
-		else {
-			if ((*s.rbegin()) > mark[A[i]]) cnt++;
-			s.insert(mark[A[i]]);
-		}
-	}
-	cout << cnt;
-	return;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+        rev[A[i]] = i;
+    }
+    for (int i = 1; i <= N; i++) {
+        cin >> B[i];
+        B[i] = rev[B[i]];
+    }
+
+    int ans = 0, m = 0x3f3f3f3f;
+    for (int i = N; i >= 1; i--) {
+        if (B[i] > m) ans++;
+        m = min(B[i], m);
+    }
+
+    cout << ans;
+    return;
 }
 
 int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int t = 1;
-	//cin >> t;
-	while (t--) solve();
-	return 0;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
+    return 0;
 }
