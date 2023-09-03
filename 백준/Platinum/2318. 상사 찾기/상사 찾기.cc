67a8715a68905fae3b074f5f349cda80be638952
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-09-03 16:54:05 +0900

    [Platinum IV] Title: 상사 찾기, Time: 60 ms, Memory: 87044 KB -BaekjoonHub

--- 백준/Platinum/2318. 상사 찾기/상사 찾기.cc
+++ 백준/Platinum/2318. 상사 찾기/상사 찾기.cc
@@ -3,7 +3,6 @@
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
-const int SAL = 10000000;
 
 int N, M;
 map<int, int> id, sal, rev;
@@ -12,14 +11,14 @@ int sz[30005], seg[20000005], parent[30005];
 pair<pii, int> arr[30005];
 
 void update(int pos, int val) {
-    for (seg[pos += SAL] = val; pos > 0; pos >>= 1) seg[pos >> 1] = seg[pos] + seg[pos ^ 1];
+    for (seg[pos += N] = val; pos > 0; pos >>= 1) seg[pos >> 1] = seg[pos] + seg[pos ^ 1];
     return;
 }
 
 int query(int l, int r) {
     r++;
     int ret = 0;
-    for (l += SAL, r += SAL; l < r; l >>= 1, r >>= 1) {
+    for (l += N, r += N; l < r; l >>= 1, r >>= 1) {
         if (l & 1) ret += seg[l++];
         if (r & 1) ret += seg[--r];
     }
@@ -27,7 +26,7 @@ int query(int l, int r) {
 }
 
 int find_id(int val) {
-    int l = val, r = SAL;
+    int l = val, r = N;
     int ans = -1;
 
     while (l <= r) {
@@ -53,24 +52,30 @@ void tree_DP(int node) {
 
 void solve() {
     cin >> N >> M;
+    vector<int> v;
     for (int i = 1; i <= N; i++) {
         int a, b, c;
         cin >> a >> b >> c;
+        v.push_back(b);
         id[a] = i;
-        sal[b] = i;
         rev[i] = a;
         arr[i] = {{c, b}, a};
     }
     sort(arr + 1, arr + N + 1, greater<>());
+    sort(v.begin(), v.end());
 
     int boss = arr[1].second;
-    update(arr[1].first.second, 1);
+    int idx = lower_bound(v.begin(), v.end(), arr[1].first.second) - v.begin();
+    update(idx, 1);
+    sal[idx] = id[boss];
     for (int i = 2; i <= N; i++) {
         int b = arr[i].first.second, c = arr[i].second;
-        int bo = find_id(b + 1);
+        idx = lower_bound(v.begin(), v.end(), b) - v.begin();
+        sal[idx] = id[c];
+        int bo = find_id(idx + 1);
         parent[id[c]] = sal[bo];
         tree[parent[id[c]]].push_back(id[c]);
-        update(b, 1);
+        update(idx, 1);
     }
 
     tree_DP(id[boss]);
