67d0d4f30909965eb198c31ae13712a32e6da022
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-09-09 14:48:28 +0900

    [Platinum III] Title: 어지러운 트리, Time: 284 ms, Memory: 38360 KB -BaekjoonHub

--- 백준/Platinum/25973. 어지러운 트리/어지러운 트리.cc
+++ 백준/Platinum/25973. 어지러운 트리/어지러운 트리.cc
@@ -4,42 +4,48 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 
-ll N, Q;
-vector<int> tree[200005];
-ll arr[200005], sz[200005], level[200005], par[200005][25];
+ll N, Q, root = 1;
+vector<int> graph[200005];
+ll arr[200005], level[200005], sz[200005];
+int parent[200005][25];
 
-void DFS(int node, int p, int lv) {
+void DFS(int node, int par, int lv) {
     level[node] = lv;
-    par[node][0] = p;
     sz[node] = 1;
-    arr[node] = N - 1;
+    parent[node][0] = par;
+
+    for (int i = 1; i <= 20; i++) parent[node][i] = parent[parent[node][i - 1]][i - 1];
+
     ll sum = 0;
-    for (int next: tree[node]) {
-        if (next == p) continue;
+    for (int next: graph[node]) {
+        if (next == par) continue;
         DFS(next, node, lv + 1);
         sz[node] += sz[next];
+        arr[node] += sz[next];
         arr[node] += sum * sz[next];
         sum += sz[next];
     }
+
+    arr[node] += N - sz[node];
     arr[node] += sum * (N - sz[node]);
     return;
 }
 
-int LCA(int a, int b) {
-    if (level[a] < level[b]) swap(a, b);
-    if (level[a] != level[b]) {
+int LCA(int A, int B) {
+    if (level[A] < level[B]) swap(A, B);
+    if (level[A] != level[B]) {
         for (int i = 20; i >= 0; i--) {
-            if (level[par[a][i]] >= level[b]) a = par[a][i];
+            if (level[parent[A][i]] >= level[B]) A = parent[A][i];
         }
     }
-    int ret = a;
-    if (a != b) {
+    int ret = A;
+    if (A != B) {
         for (int i = 20; i >= 0; i--) {
-            if (par[a][i] != par[b][i]) {
-                a = par[a][i];
-                b = par[b][i];
+            if (parent[A][i] != parent[B][i]) {
+                A = parent[A][i];
+                B = parent[B][i];
             }
-            ret = par[a][i];
+            ret = parent[A][i];
         }
     }
     return ret;
@@ -50,15 +56,11 @@ void solve() {
     for (int i = 0; i < N - 1; i++) {
         int a, b;
         cin >> a >> b;
-        tree[a].push_back(b);
-        tree[b].push_back(a);
+        graph[a].push_back(b);
+        graph[b].push_back(a);
     }
-    int root = 1;
-    DFS(1, 1, 1);
 
-    for (int i = 1; i <= 20; i++) {
-        for (int j = 1; j <= N; j++) par[j][i] = par[par[j][i - 1]][i - 1];
-    }
+    DFS(1, 1, 1);
 
     while (Q--) {
         ll a, b;
@@ -67,23 +69,25 @@ void solve() {
         else {
             int lca = LCA(root, b);
             if (root == b) cout << arr[b] << '\n';
-            else if (lca == b) {
+            else if (lca != b) cout << arr[b] - sz[b] * (N - sz[b]) << '\n';
+            else {
                 int now = root;
                 for (int i = 20; i >= 0; i--) {
-                    if (level[par[now][i]] > level[b]) now = par[now][i];
+                    if (level[parent[now][i]] > level[b]) now = parent[now][i];
                 }
                 cout << arr[b] - (N - sz[now]) * sz[now] << '\n';
-            } else cout << arr[b] - sz[b] * (N - sz[b]) << '\n';
+            }
         }
     }
 
-}
+    return;
+};
 
-int main(void) {
+int main() {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
 #endif
-    ios::sync_with_stdio(false);
+    ios::sync_with_stdio(0);
     cin.tie(0);
     int t = 1;
     //cin >> t;
