e6dc326853ebe9376e955f68e20d62c80af49c72
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-09-18 16:13:22 +0900

    [Gold II] Title: K번째 수 찾는 함수, Time: 104 ms, Memory: 21556 KB -BaekjoonHub

--- 백준/Gold/16455. K번째 수 찾는 함수/K번째 수 찾는 함수.cc
+++ 백준/Gold/16455. K번째 수 찾는 함수/K번째 수 찾는 함수.cc
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int kth(vector<int> &a, int k) {
-	nth_element(a.begin(), a.begin() + k - 1, a.end());
-	return a[k - 1];
+    int n = a.size() / 5;
+
+    for (int i = 0; i < 5; i++) {
+        if (i % 2) continue;
+        shuffle(a.begin() + i * n, min(a.begin() + (i + 1) * n, a.end()), default_random_engine());
+    }
+
+    nth_element(a.begin(), a.begin() + k - 1, a.end());
+    return a[k - 1];
 }
