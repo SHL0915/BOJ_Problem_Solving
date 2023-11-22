d8617520f576a35e666f35b7fba22418f4b80bd2
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-11-22 21:55:49 +0900

    [Bronze II] Title: Copying Homework, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19155. Copying Homework/Copying Homework.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int N;
+
+void solve() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		cout << N - a + 1 << " ";
+	}
+	return;
+};
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
