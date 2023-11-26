c7068d033770443ec552beb179668c2bad70365b
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-11-26 22:16:05 +0900

    [Gold III] Title: 투스타 춘배, Time: 76 ms, Memory: 12872 KB -BaekjoonHub

--- 백준/Gold/30414. 투스타 춘배/투스타 춘배.cc
+++ 백준/Gold/30414. 투스타 춘배/투스타 춘배.cc
@@ -8,15 +8,15 @@ int N, P;
 vector<int> tree[100005];
 int A[100005], B[100005];
 
-ll DFS(int node, int par) {
-    ll ret = A[node] - B[node];
+int DFS(int node, int par) {
+    int ret = A[node] - B[node];
 
     for (int next: tree[node]) {
         if (next == par) continue;
         ret += DFS(next, node);
     }
 
-    return min(0LL, ret);
+    return min(0, ret);
 }
 
 void solve() {
