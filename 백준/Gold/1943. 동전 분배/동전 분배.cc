68fc9c94a672cd5774b8bbe6eda5400f3a9f7325
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2023-12-06 04:15:20 +0900

    [Gold III] Title: 동전 분배, Time: 20 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.cc
+++ 백준/Gold/1943. 동전 분배/동전 분배.cc
@@ -1,53 +1,43 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
 
-int N;
-pii arr[105];
 
-void solve() {
-    cin >> N;
-    ll tot = 0;
-    for (int i = 1; i <= N; i++) cin >> arr[i].first >> arr[i].second, tot += (arr[i].first * arr[i].second);
+int n;
+int x[101];
+int c[101];
+int dp[101010];
 
-    if (tot % 2) {
-        cout << 0 << '\n';
-        return;
-    }
 
-    vector <int> dp(tot + 5, 0);
-    dp[0] = 1;
-
-    for (int i = 1; i <= N; i++) {
-        for (int j = tot; j >= 0; j--) {
-            if (!dp[j]) continue;
-            for (int k = 1; k <= arr[i].second; k++) {
-                if (j + arr[i].first * k > 100000) break;
-                dp[j + arr[i].first * k] = 1;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;tc=3;
+    while(tc--){
+        for (auto &i: dp) i = false;
+        cin>>n;
+        int s=0;
+        for (int i=1;i<=n;i++){
+            cin>>x[i]>>c[i];
+            s+=x[i]*c[i];
+        }
+        if (s&1){
+            cout<<"0\n";
+            continue;
+        }
+        dp[0] = 0;
+        fill(dp+1,dp+100000+1,1000000);
+        for (int i=1;i<=n;i++){
+            for (int j=0;j<=100000;j++){
+                if (dp[j]==1000000) continue;
+                if (dp[j] < c[i] && j + x[i] <= 100000 && dp[j + x[i]] == 1000000){
+                    dp[j + x[i]] = dp[j] + 1;
+                }
+                dp[j] = 0;
             }
         }
-        if (dp[tot / 2]) {
-            cout << 1 << '\n';
-            return;
-        }
+        cout<<(dp[s>>1]<1000000)<<'\n';
     }
 
-    if (!dp[tot / 2]) cout << 0 << '\n';
-    else cout << 1 << '\n';
-
-    return;
-}
-
-int main(void) {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-#endif
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int t = 3;
-    //cin >> t;
-    while (t--) solve();
     return 0;
 }
