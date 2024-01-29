88c221d577e688ec1fa6f545c62247e8fa669309
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2024-01-29 18:15:29 +0900

    [Platinum V] Title: 이항계수, Time: 12 ms, Memory: 6264 KB -BaekjoonHub

---
+++ 백준/Platinum/3651. 이항계수/README.md
@@ -0,0 +1,45 @@
+# [Platinum V] 이항계수 - 3651 
+
+[문제 링크](https://www.acmicpc.net/problem/3651) 
+
+### 성능 요약
+
+메모리: 6264 KB, 시간: 12 ms
+
+### 분류
+
+이분 탐색, 조합론, 수학
+
+### 제출 일자
+
+2024년 1월 29일 18:15:19
+
+### 문제 설명
+
+<p>Gunnar is quite an old and forgetful researcher. Right now he is writing a paper on security in social networks and it actually involves some combinatorics. He wrote a program for calculating binomial coefﬁcients to help him check some of his calculations.</p>
+
+<p>A binomial coefﬁcient is a number</p>
+
+<p><img alt="" src="https://www.acmicpc.net/upload/images/daum_equation_1358305217093.png" style="height:60px; opacity:0.9; width:177px"></p>
+
+<p>where n and k are non-negative integers.</p>
+
+<p>Gunnar used his program to calculate c(n, k) and got a number m as a result. Unfortunately, since he is forgetful, he forgot the numbers n and k he used as input. These two numbers were a result of a long calculation and they are written on one of many papers lying on his desk. Instead of trying to search for the papers, he tried to reconstruct the numbers n, k from the output he got. Can you help him and ﬁnd all possible candidates?</p>
+
+### 입력 
+
+ <p>On the first line a positive integer: the number of test cases, at most 100. After that per test case:</p>
+
+<ul>
+	<li>one line with an integer m (2 ≤ m ≤ 10<sup>15</sup>): the output of Gunnar’s program.</li>
+</ul>
+
+### 출력 
+
+ <p>Per test case:</p>
+
+<ul>
+	<li>one line with an integer: the number of ways of expressing m as a binomial coefﬁcient.</li>
+	<li>one line with all pairs (n, k) that satisfy c(n, k) = m. Order them in increasing order of n and, in case of a tie, order them in increasing order of k. Format them as in the sample output.</li>
+</ul>
+
---
+++ 백준/Platinum/3651. 이항계수/이항계수.cc
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<ll, ll>;
+
+ll M;
+vector<pii> ans;
+vector<ll> nCr[40005];
+
+void find_nC1() {
+    ans.push_back({M, 1});
+    ans.push_back({M, M - 1});
+    return;
+}
+
+void find_nC2() {
+    ll l = 2, r = 1e9;
+    while (l <= r) {
+        ll m = (l + r) / 2;
+        ll v = m * (m - 1) / 2;
+        if (v > M) r = m - 1;
+        else if (v == M) {
+            ans.push_back({m, 2});
+            ans.push_back({m, m - 2});
+            break;
+        } else l = m + 1;
+    }
+    return;
+}
+
+void find_nC3() {
+    ll l = 3, r = 1e6;
+    while (l <= r) {
+        ll m = (l + r) / 2;
+        ll v = m * (m - 1) * (m - 2) / 6;
+        if (v > M) r = m - 1;
+        else if (v == M) {
+            ans.push_back({m, 3});
+            ans.push_back({m, m - 3});
+            break;
+        } else l = m + 1;
+    }
+    return;
+}
+
+
+void solve() {
+    cin >> M;
+
+    find_nC1();
+    find_nC2();
+    find_nC3();
+
+    nCr[1].push_back(1);
+    nCr[1].push_back(1);
+    for (int i = 2; i <= 40000; i++) {
+        nCr[i].push_back(1);
+        for (int j = 1; j <= i; j++) {
+            if (j == i) nCr[i].push_back(1);
+            else {
+                if (j >= nCr[i - 1].size()) break;
+                else {
+                    ll v = nCr[i - 1][j - 1] + nCr[i - 1][j];
+                    if (v > 1e18) break;
+                    else nCr[i].push_back(v);
+                }
+            }
+        }
+    }
+
+
+    for (int i = 4; i <= 40000; i++) {
+        ll l = 0, r = min(i / 2, (int) nCr[i].size() - 1);
+        while (l <= r) {
+            ll m = (l + r) / 2;
+            ll v = nCr[i][m];
+            if (v > M) r = m - 1;
+            else if (v == M) {
+                ans.push_back({i, m});
+                ans.push_back({i, i - m});
+                break;
+            } else l = m + 1;
+        }
+    }
+
+    sort(ans.begin(), ans.end());
+    ans.erase(unique(ans.begin(), ans.end()), ans.end());
+
+    cout << ans.size() << '\n';
+    for (pii a: ans) cout << a.first << " " << a.second << '\n';
+
+
+    return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
+    return 0;
+}
