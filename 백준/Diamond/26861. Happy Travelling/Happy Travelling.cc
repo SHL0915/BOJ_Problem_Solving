2b3704e06fd34e3e1d38d87deaf3138250cd4612
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2024-03-06 18:30:39 +0900

    [Diamond V] Title: Happy Travelling, Time: 252 ms, Memory: 4888 KB -BaekjoonHub

--- 백준/Diamond/26861. Happy Travelling/Happy Travelling.cc
+++ 백준/Diamond/26861. Happy Travelling/Happy Travelling.cc
@@ -8,46 +8,27 @@ const int INF = 2e9;
 int N, K, D;
 int arr[100005], T[100005];
 int ans[100005];
-int seg[4 * 100005], lazy[4 * 100005];
+int seg[2 * 100005];
 deque<pair<int, pii>> dq[1205];
 
-void update_lazy(int n, int s, int e) {
-    if (lazy[n] != -INF) {
-        seg[n] = max(seg[n], lazy[n]);
-        if (s != e) {
-            lazy[n * 2] = max(lazy[n * 2], lazy[n]);
-            lazy[n * 2 + 1] = max(lazy[n * 2 + 1], lazy[n]);
-        }
-        lazy[n] = -INF;
-    }
+void update(int pos, int v) {
+    pos += 100005;
+    for (seg[pos] = max(seg[pos], v); pos > 0; pos >>= 1) seg[pos >> 1] = max(seg[pos], seg[pos ^ 1]);
     return;
 }
 
-void update(int n, int s, int e, int l, int r, int v) {
-    update_lazy(n, s, e);
-    if (l > e || r < s) return;
-    if (l <= s && e <= r) {
-        lazy[n] = max(lazy[n], v);
-        update_lazy(n, s, e);
-        return;
+int query(int l, int r) {
+    r++;
+    int ret = -INF;
+    for (l += 100005, r += 100005; l < r; l >>= 1, r >>= 1) {
+        if (l & 1) ret = max(ret, seg[l++]);
+        if (r & 1) ret = max(ret, seg[--r]);
     }
-    int m = (s + e) / 2;
-    update(n * 2, s, m, l, r, v);
-    update(n * 2 + 1, m + 1, e, l, r, v);
-    seg[n] = max(seg[n * 2], seg[n * 2 + 1]);
-    return;
-}
-
-int query(int n, int s, int e, int l, int r) {
-    update_lazy(n, s, e);
-    if (l > e || r < s) return -INF;
-    if (l <= s && e <= r) return seg[n];
-    int m = (s + e) / 2;
-    return max(query(n * 2, s, m, l, r), query(n * 2 + 1, m + 1, e, l, r));
+    return ret;
 }
 
 int find_val(int idx) {
-    if (K >= 1105) return query(1, 1, N, idx, idx) + arr[idx];
+    if (K >= 805) return query(idx, N) + arr[idx];
     else {
         int ret = -INF;
         for (int i = 0; i < K; i++) {
@@ -68,11 +49,11 @@ int find_val(int idx) {
 }
 
 void update_val(int idx) {
-    if (K >= 1105) {
+    if (K >= 805) {
         int pos = idx, cnt = 0;
         while (pos <= idx + T[idx]) {
-            int l = pos, r = min(idx + T[idx], pos + K - 1);
-            update(1, 1, N, l, r, ans[idx] - cnt * D);
+            int r = min(idx + T[idx], pos + K - 1);
+            update(r, ans[idx] - cnt * D);
             cnt++;
             pos += K;
         }
@@ -95,10 +76,9 @@ void solve() {
     for (int i = 1; i <= N; i++) cin >> arr[i];
     for (int i = 1; i < N; i++) cin >> T[i];
 
-    fill(seg, seg + 4 * 100005, -INF);
-    fill(lazy, lazy + 4 * 100005, -INF);
+    fill(seg, seg + 2 * 100005, -INF);
 
-    update(1, 1, N, 1, 1, arr[1]);
+    update(1, arr[1]);
     ans[1] = arr[1];
     update_val(1);
 
