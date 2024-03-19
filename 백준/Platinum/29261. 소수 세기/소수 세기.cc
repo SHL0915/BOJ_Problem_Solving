d664358936b8699b68234876b5b5fca6ca75cede
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2024-03-19 14:40:20 +0900

    [Platinum IV] Title: 소수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/29261. 소수 세기/소수 세기.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int P;
+
+void solve() {
+    cin >> P;
+    cout << ((P + 1) / 3) * 2 - 1;
+    return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
+    return 0;
+}
