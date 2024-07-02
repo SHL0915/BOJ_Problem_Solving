312be1a5f358e120160453917293d3f7204b9fc6
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2024-07-02 14:37:50 +0900

    [Gold V] Title: 수, Time: 24 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/22943. 수/수.cc
+++ 백준/Gold/22943. 수/수.cc
@@ -1,38 +1,78 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 
 int K, M, ans;
-int sieve[1000001];
-int isProduct[1000001];
+int sieve[1000005];
 int mark[10];
-vector <ll> prime;
 
-void BackTracking(int k, string s);
+void BruteForce(int k, int now) {
+    if (k == K) {
+        if (now % 2) {
+            if (now == 1) return;
+            if (sieve[now - 2]) return;
+            
+            ll a = now;
+            while (1) {
+                if (a % M) break;
+                else a /= M;
+            }
+            
+            int flag = 0;
+            for (ll i = 1; i * i <= a; i++) {
+                if (sieve[i]) continue;
+                if (a % i) continue;
+                if (sieve[a / i]) continue;
+                flag = 1;
+            }
+            
+            if (flag) ans++;
+        } else {
+            int f = 0;
+            for (ll i = 2; i <= now / 2; i++) {
+                if (!sieve[i] && !sieve[now - i] && i != now - i) {
+                    f = 1;
+                    break;
+                }
+            }
+            if (!f) return;
+            
+            ll a = now;
+            while (1) {
+                if (a % M) break;
+                else a /= M;
+            }
+            
+            int flag = 0;
+            for (ll i = 1; i * i <= a; i++) {
+                if (sieve[i]) continue;
+                if (a % i) continue;
+                if (sieve[a / i]) continue;
+                flag = 1;
+            }
+            
+            if (flag) ans++;
+        }
+        
+    } else {
+        for (int i = 0; i < 10; i++) {
+            if (!k && !i) continue;
+            if (mark[i]) continue;
+            mark[i] = 1;
+            BruteForce(k + 1, now * 10 + i);
+            mark[i] = 0;
+        }
+    }
+}
 
 void solve() {
     cin >> K >> M;
-
-    sieve[0] = 1; sieve[1] = 1;
-    for (ll i = 2; i <= 1000000; i++) {
-        if (sieve[i] == 0) {
-            prime.push_back(i);
-            for (ll j = i * i; j <= 1000000; j += i) sieve[j] = 1;
-        }
-    }
-
-    for (int i = 0; i < prime.size(); i++) {
-        for (int j = i; j < prime.size(); j++) {
-            ll now = prime[i] * prime[j];
-            if (now > 1000000) break;
-            isProduct[now] = 1;
-        }
-    }
-
-    BackTracking(0, "");
-
+    
+    BruteForce(0, 0);
     cout << ans;
+    
     return;
 }
 
@@ -42,44 +82,16 @@ int main(void) {
 #endif
     ios::sync_with_stdio(false);
     cin.tie(0);
+    sieve[0] = 1;
+    sieve[1] = 1;
+    for (ll i = 2; i <= 1000000; i++) {
+        if (sieve[i] == 0) {
+            for (ll j = i * i; j <= 1000000; j += i) sieve[j] = 1;
+        }
+    }
+    
     int t = 1;
     //cin >> t;
     while (t--) solve();
     return 0;
-}
-
-void BackTracking(int k, string s) {
-    if (k == K) {
-        ll now = 0;
-        for (int i = 0; i < s.length(); i++) {
-            now *= 10;
-            now += (s[i] - '0');
-        }
-
-        int flag = 0;
-        for (int i = 0; i < prime.size(); i++) {
-            ll p = prime[i];
-            if (now - p < 0) continue;
-            if (now - p != p && sieve[now - p] == 0) flag = 1;
-        }
-
-        if (flag == 0) return;
-
-        while (1) {
-            if (now % M) break;
-            else now /= M;
-        }
-
-        if (isProduct[now] == 1) ans++;        
-        return;
-    }
-    else {
-        for (int i = 0; i < 10; i++) {
-            if (mark[i]) continue;
-            if (k == 0 && i == 0) continue;
-            mark[i] = 1;
-            BackTracking(k + 1, s + (char)('0' + i));
-            mark[i] = 0;
-        }
-    }
 }
