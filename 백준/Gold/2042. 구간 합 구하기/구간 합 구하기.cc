c579ed9f00391f0a4a7848f7d591a6e2fd139722
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2024-08-18 18:19:30 +0900

    [Gold I] Title: 구간 합 구하기, Time: 188 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,49 +1,58 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 
 int N, M, K;
-vector <ll> tree;
+ll arr[1000005];
+ll fenwick[1000005];
 
-void update(int pos, ll val);
-ll query(int left, int right);
-
-void solve() {
-	cin >> N >> M >> K;
-	tree.resize(4 * N);
-	for (int i = 1; i <= N; i++) {
-		ll a; cin >> a;
-		update(i, a);
-	}
-	for (int i = 0; i < M + K; i++) {
-		ll a, b, c;
-		cin >> a >> b >> c;
-		if (a == 1) update(b, c);
-		else cout << query(b, c + 1) << '\n';
-	}
-	return;
-} 
-
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int t = 1;
-	//cin >> t;
-	while (t--) solve();
-	return 0;
-}
-
-void update(int pos, ll val) {
-	for(tree[pos += N] = val; pos > 0; pos >>= 1) tree[pos >> 1] = tree[pos] + tree[pos ^ 1];
+void update(int pos, ll diff) {
+    while (pos <= N) {
+        fenwick[pos] += diff;
+        pos += (pos & -pos);
+    }
     return;
 }
 
-ll query(int left, int right) {
-	ll ret = 0;
-    for(left += N, right += N; left < right; left >>= 1, right >>= 1){
-        if(left & 1) ret += tree[left++];
-        if(right & 1) ret += tree[--right];
+ll sum(int pos) {
+    ll ret = 0;
+    while (pos > 0) {
+        ret += fenwick[pos];
+        pos -= (pos & -pos);
     }
     return ret;
+}
+
+void solve() {
+    cin >> N >> M >> K;
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
+        update(i, arr[i]);
+    }
+
+    for (int i = 0; i < M + K; i++) {
+        ll a, b, c;
+        cin >> a >> b >> c;
+        if (a == 1) {
+            ll diff = c - arr[b];
+            update(b, diff);
+            arr[b] = c;
+        } else cout << sum(c) - sum(b - 1) << '\n';
+    }
+
+    return;
+}
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
+    return 0;
 }
