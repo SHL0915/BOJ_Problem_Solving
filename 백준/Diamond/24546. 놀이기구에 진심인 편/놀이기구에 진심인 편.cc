16c933975319facab2e7a9dfd5d1410b51f57b8f
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2024-08-20 13:46:17 +0900

    [Diamond IV] Title: 놀이기구에 진심인 편, Time: 460 ms, Memory: 574004 KB -BaekjoonHub

--- 백준/Diamond/24546. 놀이기구에 진심인 편/놀이기구에 진심인 편.cc
+++ 백준/Diamond/24546. 놀이기구에 진심인 편/놀이기구에 진심인 편.cc
@@ -3,62 +3,44 @@
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
-const int sz = 100000;
+const int sz = 100001;
 const int sqrtN = 316;
 
 int N, K, H, W, D;
+ll now;
 vector<pii> add[200005], del[200005];
-ll cnt[200005], ccnt[355], offset[355], bucket[355][200005];
+ll cnt[200005], ccnt[355], bucket[355][200005];
 
 void update(int l, int r, int k) {
+    l = max(l, W - D), r = min(r, W + D);
+
     while ((l % sqrtN) && l <= r) {
         int b = l / sqrtN;
-        if (k == -1 && cnt[l] + offset[b] == K) ccnt[b]--;
         bucket[b][cnt[l] + sz]--;
         cnt[l] += k;
         bucket[b][cnt[l] + sz]++;
-        if (k == 1 && cnt[l] + offset[b] == K) ccnt[b]++;
+        if (k == 1 && cnt[l] + ccnt[b] == K) now++;
+        if (k == -1 && cnt[l] + ccnt[b] == K - 1) now--;
         l++;
     }
 
     while (((r + 1) % sqrtN) && l <= r) {
         int b = r / sqrtN;
-        if (k == -1 && cnt[r] + offset[b] == K) ccnt[b]--;
         bucket[b][cnt[r] + sz]--;
         cnt[r] += k;
         bucket[b][cnt[r] + sz]++;
-        if (k == 1 && cnt[r] + offset[b] == K) ccnt[b]++;
+        if (k == 1 && cnt[r] + ccnt[b] == K) now++;
+        if (k == -1 && cnt[r] + ccnt[b] == K - 1) now--;
         r--;
     }
 
     while (l <= r) {
         int b = l / sqrtN;
-        if (k == -1) ccnt[b] -= bucket[b][K - offset[b] + sz];
-        offset[b] += k;
-        if (k == 1) ccnt[b] += bucket[b][K - offset[b] + sz];
+        ccnt[b] += k;
+        if (k == 1) now += bucket[b][K - ccnt[b] + sz];
+        if (k == -1) now -= bucket[b][K - 1 - ccnt[b] + sz];
         l += sqrtN;
     }
-}
-
-ll query(int l, int r) {
-    ll ret = 0;
-    while ((l % sqrtN) && l <= r) {
-        int b = l / sqrtN;
-        if (cnt[l++] + offset[b] >= K) ret++;
-    }
-
-    while (((r + 1) % sqrtN) && l <= r) {
-        int b = r / sqrtN;
-        if (cnt[r--] + offset[b] >= K) ret++;
-    }
-
-    while (l <= r) {
-        int b = l / sqrtN;
-        ret += ccnt[b];
-        l += sqrtN;
-    }
-
-    return ret;
 }
 
 void solve() {
@@ -77,7 +59,7 @@ void solve() {
 
     for (int i = 1; i <= sz; i++) {
         for (auto &[l, r]: add[i]) update(l, r, 1);
-        if (i >= max(1, H - D) && i <= min(sz, H + D)) ans += query(max(1, W - D), min(sz, W + D));
+        if (i >= max(1, H - D) && i <= min(sz, H + D)) ans += now;
         for (auto &[l, r]: del[i]) update(l, r, -1);
     }
 
