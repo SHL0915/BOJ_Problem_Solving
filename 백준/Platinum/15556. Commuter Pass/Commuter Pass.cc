192b8c3dd794ee0f74756b6bd076bebf40e5aa67
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2024-09-19 23:47:04 +0900

    [Platinum I] Title: Commuter Pass, Time: 284 ms, Memory: 41172 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/15556. Commuter Pass/Commuter Pass.cc
+++ 백준/Platinum/15556. Commuter Pass/Commuter Pass.cc
@@ -5,37 +5,58 @@ using ll = long long;
 using pii = pair<ll, ll>;
 
 int N, M, S, T, U, V;
+ll ans, dp1[200005], dp2[200005];
+vector<ll> A, B, C;
+int in_degree[200005], vst[200005];
 vector<pii> graph[200005];
-ll dist[200005], vst[200005];
-vector<int> path[200005];
-set<pii> mark;
+vector<int> path[200005], crit[200005];
 
-void DFS(int now, int E) {
+void DFS(int now) {
     if (vst[now]) return;
     else vst[now] = 1;
-    
-    if (now == E) return;
+
     for (int next: path[now]) {
-        mark.insert({now, next});
-        DFS(next, E);
+        crit[now].push_back(next);
+        DFS(next);
     }
 }
 
-void Dijkstra(int start) {
-    memset(dist, 0x3f, sizeof(dist));
+void toposort() {
+    queue<int> q;
+    for (int i = 1; i <= N; i++) {
+        dp1[i] = B[i];
+        dp2[i] = C[i];
+        if (!in_degree[i]) q.push(i);
+    }
+
+    for (int i = 1; i <= N; i++) {
+        int now = q.front();
+        q.pop();
+        for (int next: crit[now]) {
+            if (--in_degree[next] == 0) q.push(next);
+            dp1[next] = min(dp1[next], dp1[now]);
+            dp2[next] = min(dp2[next], dp2[now]);
+        }
+    }
+
+    ans = 1e18;
+    for (int i = 1; i <= N; i++) ans = min({ans, C[i] + dp1[i], B[i] + dp2[i]});
+}
+
+void Dijkstra(int start, vector<ll> &dist) {
+    fill(dist.begin(), dist.end(), 1e18);
     priority_queue<pii, vector<pii>, greater<>> pq;
     for (int i = 1; i <= N; i++) path[i].clear();
-    
+
     dist[start] = 0;
     pq.push({0, start});
-    
+
     while (pq.size()) {
         auto t = pq.top();
         pq.pop();
         ll now = t.second, v = t.first;
         if (dist[now] < v) continue;
-        for (auto [next, cost]: graph[now]) {
-            if (mark.count({now, next})) cost = 0;
+        for (auto &[next, cost]: graph[now]) {
             ll ncost = v + cost;
             if (dist[next] > ncost) {
                 path[next].clear();
@@ -55,15 +76,21 @@ void solve() {
         graph[a].push_back({b, c});
         graph[b].push_back({a, c});
     }
-    
-    Dijkstra(S);
-    DFS(T, S);
-    Dijkstra(U);
-    ll ans = dist[V];
-    Dijkstra(V);
-    ans = min(ans, dist[U]);
+
+    A.resize(N + 5), B.resize(N + 5), C.resize(N + 5);
+
+    Dijkstra(U, B);
+    Dijkstra(V, C);
+    Dijkstra(S, A);
+    DFS(T);
+    for (int i = 1; i <= N; i++) {
+        for (int next: crit[i]) in_degree[next]++;
+    }
+
+    toposort();
+
     cout << ans;
-    
+
     return;
 }
 
