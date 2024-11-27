9a43948beecdfab38475d45f9fedc69d480283e1
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2024-11-27 17:53:21 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.cc
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -1,31 +1,39 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int H, W, left_wall, right_wall, temp, ans;
-int height[500];
+int maxWater(vector<int>& arr) {
+    int res = 0;
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> H >> W;
-	for (int i = 0; i < W; i++) cin >> height[i];	
-	for(int i = 0; i < W; i++){
-		if (height[i] >= left_wall) {
-			left_wall = height[i];
-			ans += temp;
-			temp = 0;
-		}
-		else temp += (left_wall - height[i]);
-	}
-	temp = 0;
-	for (int i = W - 1; i >= 0; i--) {
-		if (height[i] > right_wall) {
-			right_wall = height[i];
-			ans += temp;
-			temp = 0;
-		}
-		else temp += (right_wall - height[i]);
-	}
-	cout << ans;
-	return 0;
+    int n = arr.size();
+    vector <int> prefix_max(n + 1, 0), suffix_max(n + 1, 0);
+    prefix_max[0] = arr[0];
+    suffix_max[n - 1] = arr[n - 1];
+    for(int i = 1; i < n; i++) prefix_max[i] = max(prefix_max[i - 1], arr[i]);
+    for(int i = n - 2; i >= 0; i--) suffix_max[i] = max(suffix_max[i + 1], arr[i]);
+
+    for(int i = 0; i < n; i++) {
+        res += min(suffix_max[i], prefix_max[i]) - arr[i];
+    }
+
+    return res;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    for (int t = 0; t < 1; ++t) {
+        int B, H;
+        cin >> H >> B;
+
+        vector<int> arr(B);
+        for (int i = 0; i < B; ++i) {
+            cin >> arr[i];
+        }
+
+        cout << maxWater(arr) << endl;
+    }
+
+    return 0;
 }
