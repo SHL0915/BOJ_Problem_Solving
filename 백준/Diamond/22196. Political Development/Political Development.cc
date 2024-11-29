4b2cbefb478362cd6c422d9b24bfd4eb8d6434ce
SHL0915 <95903235+SHL0915@users.noreply.github.com>
2024-11-29 15:01:16 +0900

    [Diamond V] Title: Political Development, Time: 320 ms, Memory: 28064 KB, Score: 100 point -BaekjoonHub

--- 백준/Diamond/22196. Political Development/Political Development.cc
+++ 백준/Diamond/22196. Political Development/Political Development.cc
@@ -5,24 +5,35 @@ using ll = long long;
 using pii = pair<int, int>;
 
 int N, K, ans;
-vector<int> graph[50005];
-bitset<50005> edge[50005];
+set<int> graph[50005];
+int ok[50005];
 
 void DFS(int root, int now, int state, int k) {
     ans = max(ans, k);
     if (now == graph[root].size()) return;
-    int nxt = graph[root][now];
+    int idx = 0, nxt = -1;
+    
+    for (auto i: graph[root]) {
+        if (idx == now) {
+            nxt = i;
+            break;
+        }
+        idx++;
+    }
     
     DFS(root, now + 1, state, k);
     int flag = 0;
-    for (int i = 0; i < now; i++) {
-        if (!(state & (1 << i))) continue;
-        if (!edge[nxt][graph[root][i]]) {
-            flag = 1;
-            break;
+    idx = 0;
+    for (auto i: graph[root]) {
+        if (state & (1 << idx)) {
+            if (!graph[nxt].count(i)) {
+                flag = 1;
+                break;
+            }
         }
+        idx++;
     }
-    if (!flag) DFS(root, now + 1, state + (1 << now), k + 1);
+    if (!flag) DFS(root, now + 1, state | (1 << now), k + 1);
     return;
 }
 
@@ -34,12 +45,28 @@ void solve() {
         for (int j = 0; j < k; j++) {
             int a;
             cin >> a;
-            graph[i].push_back(a), edge[i][a] = 1;
+            graph[i].insert(a);
         }
     }
     
+    set<pii> s;
     for (int i = 0; i < N; i++) {
-        if (graph[i].size() < K) DFS(i, 0, 0, 1);
+        s.insert({graph[i].size(), i});
+    }
+    
+    while (s.size()) {
+        pii now = *s.begin();
+        if (ok[now.second]) s.erase(now);
+        else {
+            s.erase(now);
+            DFS(now.second, 0, 0, 1);
+            for (auto i: graph[now.second]) {
+                s.erase({graph[i].size(), i});
+                graph[i].erase(now.second);
+            }
+            for (auto i: graph[now.second]) s.insert({graph[i].size(), i});
+            ok[now.second] = 1;
+        }
     }
     
     cout << ans;
